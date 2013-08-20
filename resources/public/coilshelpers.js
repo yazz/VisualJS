7a4c0bb330e7dbf0bcb88a9a4d4d2c93c6dd0b81
Zubair Quraishi <zubairq@gmail.com>
2013-08-20 07:17:50 +0200

    Investigating IE8 bug

--- resources/public/coilshelpers.js
+++ resources/public/coilshelpers.js
@@ -1,15 +1,3 @@
-showHelp = function() {
+function showHelp() {
     $('#myModal').modal();
-}
-
-updateScrollSpy = function() {
-    //$('#bs-sidebar').on('activate.bs.scrollspy', function () {
-
-
-         $('[data-spy="scroll"]').each(function () {
-          var $spy = $(this).scrollspy('refresh')
-        });
-
-    //});
-
 }
