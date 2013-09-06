b3eef27eff5a819c9e33ce20f4d0dbc1ab63d0b5
Zubair Quraishi <zubairq@gmail.com>
2013-09-06 14:39:58 +0200

    Removed unused code

--- src/webapp/framework/client/coreclient.cljs
+++ src/webapp/framework/client/coreclient.cljs
@@ -434,13 +434,13 @@
 
 
 
-(log "df")
+(comment log "df")
 
 (defn from-server []
   (GET "http://127.0.0.1:3000/main.html")
 )
 
-(go
+(comment go
   (log (count (<! (from-server))))
   (log (count (<! (GET "http://127.0.0.1:3000/main.html"))))
  )
