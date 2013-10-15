08e3ed5db1571fb616bbb4a61bc739224639c7bf
Zubair Quraishi <zubairq@gmail.com>
2013-10-15 07:17:22 +0200

    Moved Neo4j helpers into their own class

---
+++ src/webapp/framework/server/neo4j_helper.clj
@@ -0,0 +1,113 @@
+(ns webapp.framework.server.neo4j-helper
+
+  [:require [clojure.string :as str]]
+  [:use [korma.db]]
+  [:use [webapp.framework.server.systemfns]]
+  [:use [webapp.framework.server.email-service]]
+  [:use [webapp.framework.server.encrypt]]
+  [:use [korma.core]]
+  [:use [clojure.core.async]]
+  [:use [clojure.repl]]
+  [:use [webapp.framework.server.db-helper]]
+
+  (:require [clojurewerkz.neocons.rest :as nr])
+  (:require [clojurewerkz.neocons.rest.nodes :as nn])
+  (:require [clojurewerkz.neocons.rest.relationships :as nrl])
+  (:require [clojurewerkz.neocons.rest.cypher :as cy])
+  (:use [webapp-config.settings])
+  (:import [java.util.UUID])
+)
+
+
+(try
+     (nr/connect! "http://localhost:7474/db/data/")
+         (catch Exception e (str "caught exception: " (.getMessage e))))
+
+
+
+(defn get-from-neo [params]
+    {:value (into []
+         (let [amy (nn/create {:username "amy" :age 27})
+                bob (nn/create {:username "bob" :age 28})
+                _   (nrl/create amy bob :friend {:source "college"})
+                res (cy/tquery "START x = node({ids}) RETURN x.username, x.age" {:ids (map :id [amy bob])})]
+            res)
+          )})
+
+
+;(get-from-neo nil)
+
+
+
+
+
+
+
+
+
+
+
+
+   (defonce neo4j-type-index
+     ( try
+     (nn/create-index "types")
+         (catch Exception e (str "caught exception: " (.getMessage e))))
+ )
+
+
+
+   (defn to-nodes4j [results]
+        (into []
+           (map
+             #(get %1 "resultset")
+             results)))
+
+
+   (defn to-maps4j [results]
+        (into []
+           (map
+             #(:data (get %1 "resultset"))
+             results)))
+
+
+   (defn table4j [table-name]
+     (to-maps4j
+         (cy/tquery "START resultset=node:types('type:*') where resultset.type={type} RETURN resultset"
+                    {:type table-name})))
+
+
+
+   (defn nodes4j [table-name]
+     (to-nodes4j
+         (cy/tquery "START resultset=node:types('type:*') where resultset.type={type} RETURN resultset"
+                    {:type table-name})))
+
+
+
+
+   (defn make4j [node]
+      (if (:type node)
+         (let [
+                 node     (nn/create node)
+              ]
+              (nn/add-to-index (:id node)  (:name neo4j-type-index) "type" (:type node))
+         )
+         (throw (new java.lang.Exception "needs a type"))
+      )
+   )
+
+
+
+   ;(make4j {:type "place" :name "Moscow"})
+
+
+
+   ;(table4j "place")
+
+   ;(nodes4j "place")
+(comment let [amy (nn/create {:username "amy"})
+        bob (nn/create {:username "bob"})
+        _   (nrl/create amy bob :friend {:source "college"})]
+    (println (nrl/incoming-for bob :types [:friend])))
+
+
--- src/webapp/server/fns.clj
+++ src/webapp/server/fns.clj
@@ -8,18 +8,11 @@
   [:use [clojure.core.async]]
   [:use [clojure.repl]]
   [:use [webapp.framework.server.db-helper]]
+  [:use [webapp.framework.server.neo4j-helper]]
 
-  (:require [clojurewerkz.neocons.rest :as nr])
-  (:require [clojurewerkz.neocons.rest.nodes :as nn])
-  (:require [clojurewerkz.neocons.rest.relationships :as nrl])
-  (:require [clojurewerkz.neocons.rest.cypher :as cy])
   (:use [webapp-config.settings])
   (:import [java.util.UUID])
 )
-
-( try
-     (nr/connect! "http://localhost:7474/db/data/")
-         (catch Exception e (str "caught exception: " (.getMessage e))))
 
 
 
@@ -80,86 +73,6 @@
 
 ;(add-user {:user-name "zubair"})
 
-(defn get-from-neo [params]
-    {:value (into []
-         (let [amy (nn/create {:username "amy" :age 27})
-                bob (nn/create {:username "bob" :age 28})
-                _   (nrl/create amy bob :friend {:source "college"})
-                res (cy/tquery "START x = node({ids}) RETURN x.username, x.age" {:ids (map :id [amy bob])})]
-            res)
-          )})
-
-
-;(get-from-neo nil)
-
-
-
-
-
-
-
-
-
-
-
-
-   (defonce neo4j-type-index
-     ( try
-     (nn/create-index "types")
-         (catch Exception e (str "caught exception: " (.getMessage e))))
- )
-
-
-
-   (defn to-nodes4j [results]
-        (into []
-           (map
-             #(get %1 "resultset")
-             results)))
-
-
-   (defn to-maps4j [results]
-        (into []
-           (map
-             #(:data (get %1 "resultset"))
-             results)))
-
-
-   (defn table4j [table-name]
-     (to-maps4j
-         (cy/tquery "START resultset=node:types('type:*') where resultset.type={type} RETURN resultset"
-                    {:type table-name})))
-
-
-
-   (defn nodes4j [table-name]
-     (to-nodes4j
-         (cy/tquery "START resultset=node:types('type:*') where resultset.type={type} RETURN resultset"
-                    {:type table-name})))
-
-
-
-
-   (defn make4j [node]
-      (if (:type node)
-         (let [
-                 node     (nn/create node)
-              ]
-              (nn/add-to-index (:id node)  (:name neo4j-type-index) "type" (:type node))
-         )
-         (throw (new java.lang.Exception "needs a type"))
-      )
-   )
-
-
-
-   ;(make4j {:type "place" :name "Moscow"})
-
-
-
-   ;(table4j "place")
-
-   ;(nodes4j "place")
 
 
 (defn find-user-by-username [{username :username}]
