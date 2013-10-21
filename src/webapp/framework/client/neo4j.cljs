6b5bfe1a53b0c9fc337e285dee7f4daf96ae4e9c
Zubair Quraishi <zubairq@gmail.com>
2013-10-21 07:27:49 +0200

    Added delete all for Neo4j

--- src/webapp/framework/client/neo4j.cljs
+++ src/webapp/framework/client/neo4j.cljs
@@ -117,6 +117,11 @@
   )
 )
 
+(defn delete-all-neo-4j-nodes [ & {:keys [  are-you-sure?  ]}]
+    (go
+        (<! (neo4j "START n = node(*) MATCH n-[r?]-() WHERE ID(n)>0 DELETE n, r;"{}))
+     )
+)
 
 
 (comment go
@@ -127,7 +132,7 @@
     (log (<! (count-all-neo4j-records-with-field :type)))
 )
 
-
+(delete-all-neo-4j-nodes :are-you-sure? "yes")
 
 (comment go
     (log (<! (count-all-neo4j-records)))
