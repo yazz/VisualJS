867ac50e310d53455f4ece7918cfa7add342c0a5
Zubair Quraishi <zubairq@gmail.com>
2013-11-14 05:45:24 +0000

    Moved to latest clojurescript version

--- src/webapp/framework/client/neo4j.cljs
+++ src/webapp/framework/client/neo4j.cljs
@@ -1,7 +1,6 @@
 (ns webapp.framework.client.neo4j
     (:refer-clojure :exclude [val empty remove find next parents])
     (:require
-        [cljs.reader :as reader]
         [crate.core :as crate]
         [cljs.core.async :as async :refer         [chan close!]]
         [clojure.string]
@@ -9,19 +8,19 @@
     (:use
         [webapp.framework.client.coreclient :only  [body-html new-dom-id debug popup hide-popovers
                                                     show-popover set-text value-of find-el sql-fn neo4j-fn
-                                                    swap-section el clear remote  add-to on-mouseover-fn on-click-fn]]
+                                                    swap-section el clear remote add-to
+                                                    on-mouseover-fn on-click-fn]]
         [jayq.core                          :only  [$ css  append fade-out fade-in empty attr bind]]
         [webapp.framework.client.help       :only  [help]]
         [webapp.framework.client.eventbus   :only  [do-action esb undefine-action]]
-        [domina                             :only  [ by-id value destroy! ]]
+        [domina                             :only  [by-id value destroy! ]]
   )
   (:require-macros
-    [cljs.core.async.macros :refer                 [go alt!]])
+    [cljs.core.async.macros :refer                 [go alt!]]
+   )
   (:use-macros
         [webapp.framework.client.eventbus :only    [redefine-action define-action]]
-        [webapp.framework.client.coreclient :only  [ns-coils makeit defn-html on-click on-mouseover sql defn-html
-                                                    defn-html2 neo4j log log-async]]
-        [webapp.framework.client.interpreter :only [! !! !!!]]
+        [webapp.framework.client.coreclient :only  [ns-coils neo4j log]]
      )
 )
 (ns-coils 'webapp.framework.client.neo4j)
