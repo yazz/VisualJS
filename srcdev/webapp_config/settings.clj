3c784934c2e333bf148afd120517d54a3a28182a
Zubair Quraishi <zubairq@gmail.com>
2013-11-17 07:55:52 +0000

    Fixed coils demo

---
+++ srcdev/webapp_config/settings.clj
@@ -0,0 +1,26 @@
+(ns webapp-config.settings)
+
+
+
+
+
+
+(defonce ^:dynamic *mandrill-api-key* "enter_api_key_for_mandrill_here")
+
+(defonce ^:dynamic *environment* "dev")
+
+(defonce ^:dynamic *web-server* "127.0.0.1:3000")
+
+(defonce ^:dynamic *database-server* "127.0.0.1")
+
+(defonce ^:dynamic *database-user* "postgres")
+
+(defonce ^:dynamic *database-password* "manager")
+
+(defonce ^:dynamic *database-name* "webdb")
+
+(defonce ^:dynamic *sql-encryption-password* "animal")
+
+(defonce ^:dynamic *show-code* true)
+
+(defonce ^:dynamic *main-page* "coils.html")
