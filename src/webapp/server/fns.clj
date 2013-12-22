f474b90d234b842ec520bbef31871d948c1615b6
Zubair Quraishi <zq@nemcv.com>
2013-12-22 13:30:07 +0100

    Empty server functions file

---
+++ src/webapp/server/fns.clj
@@ -0,0 +1,15 @@
+(ns webapp.server.fns
+  [:require [clojure.string :as str]]
+  [:use [korma.db]]
+  [:use [webapp.framework.server.systemfns]]
+  [:use [webapp.framework.server.email-service]]
+  [:use [webapp.framework.server.encrypt]]
+  [:use [korma.core]]
+  [:use [clojure.repl]]
+  [:use [webapp.framework.server.db-helper]]
+  [:use [webapp.framework.server.neo4j-helper]]
+
+  (:use [webapp-config.settings])
+  (:import [java.util.UUID])
+)
+
