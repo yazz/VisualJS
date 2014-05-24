4660d5d25d26b73ed90cfdd5b619aec4cd2a777e
Zubair Quraishi <zq@nemcv.com>
2014-05-24 18:49:03 +0200

    moved to om 0.6.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                    [org.clojure/google-closure-library-third-party "0.0-2029"]
                    [org.clojure/tools.reader "0.7.10"]
                    [org.clojure/clojurescript "0.0-2173"]
-                   [om "0.6.2"]
+                   [om "0.6.3"]
                    [om-sync "0.1.1"]
                    [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
 
