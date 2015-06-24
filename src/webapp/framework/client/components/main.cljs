f88d1fc61437957646cbb07976e92020643d965e
Zubair Quraishi <zq@nemcv.com>
2015-06-24 21:52:30 +0200

    Cleaned up demo a bit
    Cleaned up demo a bit

--- src/webapp/framework/client/components/main.cljs
+++ src/webapp/framework/client/components/main.cljs
@@ -2,27 +2,11 @@
   (:require
    [webapp.framework.client.coreclient   :as c ])
   (:use-macros
-    [webapp.framework.client.coreclient  :only [ns-coils sql log neo4j neo4j-1 sql-1 log
-                                                watch-data  -->ui  <--data <--ui
-                                                watch-ui
-                                               remote  defn-ui-component
+    [webapp.framework.client.coreclient  :only [ns-coils defn-ui-component def-coils-app
                                                 container  map-many  inline  text
-                                                div   img pre
-                                                component h2 input
-                                                write-ui read-ui container
-                                                inline text admin
-                                                ==data
-                                                ==ui  -->ui  watch-ui <--ui
-                                                <--data -->data
-                                                remote
-                                                input
-                                                component <--
-                                                h1 h2 h3 h4 h5 h6
-                                                span  data-view-v2
-                                                watch-data map-many inline text
-                                                container <--pos <--id
-                                                session-user-id select select-debug
-                                                def-coils-app
+                                                div img pre component h2 input
+                                                write-ui read-ui container input component <--
+                                                h1 h2 h3 h4 h5 h6 span  data-view-v2 select
                                                 ]]))
 
 
@@ -37,11 +21,9 @@
                               (h2 nil "Coils")
                                 (select id, item from todo_items where item like '%'
                                           {}
-                                        (div {:style {:paddingBottom "20px"}}
-                                            (div {} (str (<-- :id)))
-                                             (div {} (str (<-- :item)))
-                                             )
-                                        )
+                                        (container
+                                            (inline  "10%" (str (<-- :id)))
+                                            (inline  "90%" (str (<-- :item)))))
                                 "Build database webapps with Clojure"))
 
 
