5ab211bef7614b89dbeb27bb1673616cdf29553e
Zubair Quraishi <zubairq@gmail.com>
2015-07-17 19:28:37 +0100

    Force all d fields to lower case in Clojure
    Force all d fields to lower case in Clojure

--- src/webapp/framework/server/db_helper.clj
+++ src/webapp/framework/server/db_helper.clj
@@ -11,17 +11,30 @@
 )
 
 
+
+
+
+
+
+(defn to-lower-case-keys [m]
+      (into (empty m) (for [[k v] m] [(keyword (.toLowerCase (name k))) v])))
+
+
+
+
 (defn db-count-records [table-name]
     (:cnt
+      (to-lower-case-keys
      (first
      (korma.core/exec-raw
                    [(str "SELECT COUNT(*) as CNT FROM " table-name)
                     []]
-                    :results)
+                    :results))
     )))
 
 (defn db-table-fields [table-name]
     (keys
+      (to-lower-case-keys
      (first
      (korma.core/exec-raw
                    [(str "SELECT * FROM " table-name
@@ -37,7 +50,7 @@
                              )                           )
                          )
                     []]
-                    :results)
+                    :results))
     )))
 
 
@@ -49,14 +62,16 @@
   ([sql-in params]
   (do
     (let [
-          lower           (.toLowerCase sql-in)
+          lower (.toLowerCase sql-in)
           ]
-      ;(println "SQL from client: " sql-in)
-      (cond
-       (.startsWith lower "select")  (do (comment println "SELECT") (korma.core/exec-raw [sql-in params] :results))
-       :else                         (do (comment println "INSERT") (korma.core/exec-raw [sql-in params]) [])
-   ; []
-    )))
+         ;(println "SQL from client: " sql-in)
+         ;
+           (cond
+             (.startsWith lower "select") (do (comment println "SELECT") (into [] (map   (fn [r] (to-lower-case-keys r));
+                                                                                      (korma.core/exec-raw [sql-in params] :results))))
+             :else (do (comment println "INSERT") (korma.core/exec-raw [sql-in params]) [])
+             ; []
+             )))
   )
 )
 
