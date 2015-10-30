c1ce2ea11a194e6daddce4b79638946484589fce
Zubair Quraishi <zq@nemcv.com>
2015-10-30 06:54:23 +0100

    Cleaned up demo code
    Just with easier to understand SPACING

--- src/webapp/framework/client/components/main_view.cljs
+++ src/webapp/framework/client/components/main_view.cljs
@@ -23,64 +23,65 @@
 
 (defn-ui-component     main-to-do-component   [app]
   {}
-
-  (section {:className "todoapp"}
-
-           (header {} (h2 nil "Todo app"))
-
-
-           (div {:className "main_div"}
+  (section
+   {:className "todoapp"}
+     (header {} (h2 nil "Todo app"))
+     (div {:className "main_div"}
 
 
-                (input-field {}
-                 app
-                 (fn [new-value]
-                   (go
-                    (sql "insert into  coils_todo_items   (item, item_status) values (?,?)"
-                        [new-value  "ACTIVE"]  ))))
+       (input-field {:placeholder  "Enter a new todo here"} app
+         (fn [new-todo-item-text] (go
+            (sql "insert into coils_todo_items
+                            (item, item_status)
+                  values
+                            (?,                  ?)"
+                            [new-todo-item-text  "ACTIVE"]  ))))
 
 
-                (div {:className "smallGap"})
 
-                (div {:className "todo-list"}
-                     (realtime select
-                               id, item, item_status
-                               from
-                               coils_todo_items
-                               where
-                               item_status = ?
-                               OR
-                               item_status = ?
-                               order
-                               by id desc
+       (div {:className "smallGap"})
 
-                               {:params [(if (read-ui app [:show]) (read-ui app [:show]) "ACTIVE")
-                                         (if (read-ui app [:show]) (read-ui app [:show]) "COMPLETED")
-                                         ]}
-                               (form {}
-                                     (div {:className "view" }
 
-                                          (div {:className  "checkbox"  :style { :width "20%" :display "inline-block;"} }
-                                               (input {:type  "checkbox"
-                                                       :checked   (if (= (<-- :item_status) "COMPLETED") "T" "")
-                                                       :onChange  (fn [event]
-                                                                    (let [newtext   (.. event -target -checked  )
-                                                                          item-id   (<-- :id)]
-                                                                      (if newtext
-                                                                        (go (sql "update  coils_todo_items   set item_status = 'COMPLETED' where id = ?" [item-id]  ))
-                                                                        (go (sql "update  coils_todo_items   set item_status = 'ACTIVE' where id = ?" [item-id]  ))
-                                                                        )))}))
 
-                                          (label {:style {:width   "50%"} :className (if (= (<-- :item_status) "COMPLETED") "completed" "item")} (str (<-- :item)))
-                                          (button {:className   "destroy"
-                                                   :style {:width   "10%"}
-                                                   :onClick
-                                                   (fn [e]
-                                                     (go
-                                                       (sql "delete from  coils_todo_items  where id = ?"
-                                                            [(<-- :id)]  ))
-                                                     false)})
-                                          ))))
+
+
+       (realtime select
+                      id, item, item_status
+                 from
+                      coils_todo_items
+                 where
+                      item_status = ? OR item_status = ?
+                 order
+                      by id desc
+
+                 {:params [(if (read-ui app [:show]) (read-ui app [:show]) "ACTIVE")
+                           (if (read-ui app [:show]) (read-ui app [:show]) "COMPLETED")]}
+
+                 (container
+
+                  (div {:className  "checkbox"  :style { :width "20%" :display "inline-block;"} }
+                       (input {:type  "checkbox"
+                               :checked   (if (= (<-- :item_status) "COMPLETED") "T" "")
+                               :onChange  (fn [event]
+                                            (let [newtext   (.. event -target -checked  )
+                                                  item-id   (<-- :id)]
+                                              (if newtext
+                                                (go (sql "update  coils_todo_items   set item_status = 'COMPLETED' where id = ?" [item-id]  ))
+                                                (go (sql "update  coils_todo_items   set item_status = 'ACTIVE' where id = ?" [item-id]  ))
+                                                )))}))
+
+                  (label {:style {:width   "50%"}
+                          :className (if (= (<-- :item_status) "COMPLETED") "completed" "item")}
+                                (str (<-- :item)))
+
+                  (button {:className   "destroy"
+                           :style {:width   "10%"}
+                           :onClick
+                           (fn [e]
+                             (go
+                              (sql "delete from  coils_todo_items  where id = ?"
+                                   [(<-- :id)]  ))
+                                          false)})))
 
 
 
