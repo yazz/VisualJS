decd6da8abcf185e7d339685bff7d90adbd1510e
Zubair Quraishi <zubairq@gmail.com>
2016-03-18 20:58:43 +0100

    Database Explorer can now add tables
    Still a bit buggy but can name a table and add it

--- resources/public/init/dbexplorer.cljs
+++ resources/public/init/dbexplorer.cljs
@@ -1,8 +1,11 @@
+
+(defn get-tables []
 (remote "!get-list-of-tables"
         {:session-id  (:session-id @webapp.framework.client.system-globals.client-session-atom)}
         (fn [result2]
             (reset! webapp.framework.client.system-globals/app-state (assoc-in @webapp.framework.client.system-globals/app-state [:ui :table-list] result2))))
-
+)
+(get-tables)
 
 
 (comment sql "select * from newtable" [] (fn [rr] (js/alert (pr-str rr))))
@@ -16,6 +19,26 @@
          (fn [table-name]
            (div nil table-name))
          (get-in @webapp.framework.client.system-globals/app-state [:ui :table-list])
+         )
+
+       (div {:className "input-group"}
+            (input {:id          "tablename"
+                    :type        "text"
+                    :className   "form-control"
+                    :placeholder "New table name" } )
+
+            (span {:className "input-group-btn"}
+                  (button {:className "btn btn-default" :type "button"
+                           :onClick #(let [table-name    (.. (js/document.getElementById "tablename") -value  )]
+                                       (sql  (str "create table " table-name " (did     integer)") {}
+                                             (fn [ss]
+                                               (do
+                                                 (set! (js/document.getElementById "tablename") -value  "")
+                                                 (get-tables))))
+
+                                       )
+                           } "Add!")
+                  )
+            )
        )
-   )
   )
