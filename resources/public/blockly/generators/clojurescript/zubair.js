4e3ca3af0ec8d559e56cb1af631feacda6d6bf42
Zubair Quraishi <ZQ@corp.novocorp.net>
2016-05-07 21:39:24 +0200

    Made basic Clojurescript Blockly generator
    Still not working but starting to understand how Blockly fits together

---
+++ resources/public/blockly/generators/clojurescript/zubair.js
@@ -0,0 +1,12 @@
+'use strict';
+
+goog.provide('Blockly.ClojureScript.zubair');
+
+goog.require('Blockly.ClojureScript');
+
+
+Blockly.ClojureScript['zubair'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '.hkjhkhjk..;\n';
+  return code;
+};
