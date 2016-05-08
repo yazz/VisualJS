06216c3687ca6914e8d3f8bcf9227667aec0dbc1
Zubair Quraishi <ZQ@corp.novocorp.net>
2016-05-08 12:46:29 +0200

    Added app and div blockly elements
    So we can finally see the AppShare blocks in development now! :)

--- resources/public/blockly/blocks/appshare.js
+++ resources/public/blockly/blocks/appshare.js
@@ -6,12 +6,24 @@ goog.require('Blockly.Blocks');
 
 
 
-
-
-
-Blockly.Blocks['appshare'] = {
+//https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#3ggmju
+Blockly.Blocks['appshare_app'] = {
   init: function() {
-    this.setColour(160);
+    this.appendDummyInput()
+        .appendField("Applicaiton");
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+
+
+
+//https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#wphz2c
+Blockly.Blocks['appshare_div'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("<div>");
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
--- resources/public/blockly/generators/clojurescript/appshare.js
+++ resources/public/blockly/generators/clojurescript/appshare.js
@@ -5,7 +5,15 @@ goog.provide('Blockly.ClojureScript.appshare');
 goog.require('Blockly.ClojureScript');
 
 
-Blockly.ClojureScript['appshare'] = function(block) {
+
+Blockly.ClojureScript['appshare_app'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '(app nil)\n';
+  return code;
+};
+
+
+Blockly.ClojureScript['appshare_div'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
   var code = '(div nil)\n';
   return code;
