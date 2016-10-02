9a441a24df043a5d981af7bd6a59ce16955b2a0c
Zubair Quraishi <ZQ@corp.novocorp.net>
2016-10-02 10:43:46 +0200

    Clearing up Blockly JS code
    Adding comments and stuff

--- resources/public/blockly_helper.js
+++ resources/public/blockly_helper.js
@@ -10,6 +10,9 @@ var toolbox = '<xml id="toolbox"  style="display: none"></xml>';
 // --------------------------------------------------------------------
 var component_list = [];
 
+
+
+
 // --------------------------------------------------------------------
 //   NAMES OF ALL THE FUNCTIONS IN A BLOCKLY PROGRAM ARE STORED HERE
 //
@@ -19,6 +22,17 @@ var component_list = [];
 // --------------------------------------------------------------------
 var component_names = [];
 
+
+
+
+// --------------------------------------------------------------------
+//       USED TO FIGURE OUT WHEN TEXT IN A BLOCKLY FIELD HAS CHANGED
+// --------------------------------------------------------------------
+var lastkeypresstime = -1;
+var updateblockly = false;
+var keypressed = false;
+var savelagtime = 3000;
+var lastEval = -1;
 
 
 // --------------------------------------------------------------------
@@ -87,272 +101,316 @@ function findFirstNextElement(el)
 
 
 
-      //
-      // this function rearranges the XML for blockly so that it can be parsed by
-      // the blockly compiler to make valid clojure code
-      //
-      function rearrangeDom(dom)
-      {
-        //
-        // Save the names of all the custom components
-        //
-        var blocks = dom.children;
-        //console.log("Block count: " + blocks.length);
-        bc = blocks.length;
-        var foundFirstMainUiBlock = false;
-        component_list = [];
-        component_names = [];
-        for (i = 0; i  < bc; i++) {
-          if (blocks[i].getAttribute('type') == 'appshare_custom_component') {
-            var tt = blocks[i].children[0].textContent;
-            component_list.push([tt,tt]);
-            component_names.push(tt);
-          }
-        };
+
+// --------------------------------------------------------------------
+//  function rearrangeDom
+//  input:        dom
+//  output:       none
+//  side effects: Rearranges the dom passed in
+//
+//
+// this function rearranges the XML for blockly so that it can be parsed by
+// the blockly compiler to make valid clojure code
+//
+// --------------------------------------------------------------------
+function rearrangeDom(dom)
+{
+  //
+  // Save the names of all the custom components
+  //
+  var blocks = dom.children;
+  //console.log("Block count: " + blocks.length);
+  bc = blocks.length;
+  var foundFirstMainUiBlock = false;
+  component_list = [];
+  component_names = [];
+  for (i = 0; i  < bc; i++) {
+    if (blocks[i].getAttribute('type') == 'appshare_custom_component') {
+      var tt = blocks[i].children[0].textContent;
+      component_list.push([tt,tt]);
+      component_names.push(tt);
+    }
+  };
 
 
 
 
 
-        //
-        // make a main element for the program, which contains a main div statement.
-        // Inside this div statement place all the blocks which
-        // are just 'hanging around' so that we can show something on
-        // the screen
-        //
-        // keep the custom components at the top
-        var mainCustomComponents = document.createElement("custcomponents");
-        var defineDbComponents = document.createElement("definedbcomponents");
+  //
+  // make a main element for the program, which contains a main div statement.
+  // Inside this div statement place all the blocks which
+  // are just 'hanging around' so that we can show something on
+  // the screen
+  //
+  // keep the custom components at the top
+  var mainCustomComponents = document.createElement("custcomponents");
+  var defineDbComponents = document.createElement("definedbcomponents");
 
-        var mainProgramBlocklycomponent = document.createElement("block");
-        mainProgramBlocklycomponent.setAttribute("type","appshare_ui_component");
+  var mainProgramBlocklycomponent = document.createElement("block");
+  mainProgramBlocklycomponent.setAttribute("type","appshare_ui_component");
 
-        var mainStatement = document.createElement("statement");
-        mainStatement.setAttribute("name","main div element");
-        mainProgramBlocklycomponent.appendChild(mainStatement);
+  var mainStatement = document.createElement("statement");
+  mainStatement.setAttribute("name","main div element");
+  mainProgramBlocklycomponent.appendChild(mainStatement);
 
-        lastblock = null;
+  lastblock = null;
 
-        // go through all the top level blocks
-        for (var bi = 0; bi  < bc; bi++) {
-          blocks = dom.children;
-          block = blocks[0];
+  // go through all the top level blocks
+  for (var bi = 0; bi  < bc; bi++) {
+    blocks = dom.children;
+    block = blocks[0];
 
-          // remove any screen positions from the blocks since we are rearranging them
-          block.removeAttribute('x');
-          block.removeAttribute('y');
-          //console.log("next block is: " + bi + " : " + block.getAttribute('type'));
-
-
-          // if it is a custom component then make sure it calls something valid
-          if (block.getAttribute('type') == 'appshare_call_custom_component') {
-            childr = block.children;
-            chi = childr[0];
-            if (component_names.indexOf(chi.textContent) == -1 ) {
-              //console.log("call: " + " : " + chi.textContent);
-              chi.textContent = 'default-component';
-              //block.parentElement.removeChild(block);
-            };
-          }
+    // remove any screen positions from the blocks since we are rearranging them
+    block.removeAttribute('x');
+    block.removeAttribute('y');
+    //console.log("next block is: " + bi + " : " + block.getAttribute('type'));
 
 
-          // if it is a custom component then move it to the top
-          if (block.getAttribute('type') == 'appshare_custom_component') {
-            //console.log("Found custom component ");
-            mainCustomComponents.appendChild(block);
-          }
-          // if it is a deprecated UI component then remove it
-          else if (block.getAttribute('type') == 'appshare_ui_component') {
-            //console.log("Removed block ");
-            block.parentElement.removeChild(block);
-          }
-
-          // if it is a code then remove it
-          else if (block.getAttribute('type').startsWith('appshare_code_')) {
-            //console.log("Removed block ");
-            block.parentElement.removeChild(block);
-          }
-          // if it is defining the database then remove it
-          else if (block.getAttribute('type').startsWith('appshare_definedb_')) {
-            //console.log("Removed block ");
-            //block.parentElement.removeChild(block);
-            defineDbComponents.appendChild(block);
-          }
-
-          // otherwise it is a UI component
-          else {
-            if (foundFirstMainUiBlock == false) {
-              mainStatement.appendChild(block);
-            }
-            else {
-              var nextelement = document.createElement("next");
-              lastblock.appendChild(nextelement);
-              nextelement.appendChild(block);
-            }
-            foundFirstMainUiBlock = true;
-            lastblock = findFirstNextElement(block);
-            //console.log("    last block is: " + bi + " : " + lastblock.getAttribute('type'));
-          }
-        };
+    // if it is a custom component then make sure it calls something valid
+    if (block.getAttribute('type') == 'appshare_call_custom_component') {
+      childr = block.children;
+      chi = childr[0];
+      if (component_names.indexOf(chi.textContent) == -1 ) {
+        //console.log("call: " + " : " + chi.textContent);
+        chi.textContent = 'default-component';
+        //block.parentElement.removeChild(block);
+      };
+    }
 
 
+    // if it is a custom component then move it to the top
+    if (block.getAttribute('type') == 'appshare_custom_component') {
+      //console.log("Found custom component ");
+      mainCustomComponents.appendChild(block);
+    }
+    // if it is a deprecated UI component then remove it
+    else if (block.getAttribute('type') == 'appshare_ui_component') {
+      //console.log("Removed block ");
+      block.parentElement.removeChild(block);
+    }
 
-        var customBlocks = mainCustomComponents.children;
-        customBlocksLength = customBlocks.length;
-        for (i = 0; i  < customBlocksLength; i++) {
-          customBlocks = mainCustomComponents.children;
-          cblock = customBlocks[0];
-          cblock.removeAttribute('x');
-          cblock.removeAttribute('y');
-          dom.appendChild(cblock);
-        }
+    // if it is a code then remove it
+    else if (block.getAttribute('type').startsWith('appshare_code_')) {
+      //console.log("Removed block ");
+      block.parentElement.removeChild(block);
+    }
+    // if it is defining the database then remove it
+    else if (block.getAttribute('type').startsWith('appshare_definedb_')) {
+      //console.log("Removed block ");
+      //block.parentElement.removeChild(block);
+      defineDbComponents.appendChild(block);
+    }
 
-
-
-
-        var defineDbBlocks = defineDbComponents.children;
-        defineDbBlocksLength = defineDbBlocks.length;
-        for (i = 0; i  < defineDbBlocksLength; i++) {
-          defineDbBlocks = defineDbComponents.children;
-          cblock = defineDbBlocks[0];
-          cblock.removeAttribute('x');
-          cblock.removeAttribute('y');
-          dom.appendChild(cblock);
-        }
-
-        dom.appendChild(mainProgramBlocklycomponent);
-
+    // otherwise it is a UI component
+    else {
+      if (foundFirstMainUiBlock == false) {
+        mainStatement.appendChild(block);
       }
-
-
-
-
-
-
-
-
-
-
-      function getBlocklyValue()
-      {
-
-        var dom = Blockly.Xml.workspaceToDom(workspace);
-        var headlessWorkspace = new Blockly.Workspace();
-        rearrangeDom(dom);
-        Blockly.Xml.domToWorkspace(dom, headlessWorkspace );
-
-
-        var inline = Blockly.ClojureScript.workspaceToCode(headlessWorkspace);
-        headlessWorkspace.dispose();
-        var code = inline;
-
-        return code;
+      else {
+        var nextelement = document.createElement("next");
+        lastblock.appendChild(nextelement);
+        nextelement.appendChild(block);
       }
+      foundFirstMainUiBlock = true;
+      lastblock = findFirstNextElement(block);
+      //console.log("    last block is: " + bi + " : " + lastblock.getAttribute('type'));
+    }
+  };
 
 
 
-      function getBlocklyOptimizedValue()
-      {
-
-        var dom = Blockly.Xml.workspaceToDom(workspace);
-        var headlessWorkspace = new Blockly.Workspace();
-        rearrangeDom(dom);
-        Blockly.Xml.domToWorkspace(dom, headlessWorkspace );
-
-
-        var inline = Blockly.ClojureScriptOptimized.workspaceToCode(headlessWorkspace);
-        headlessWorkspace.dispose();
-        var code = inline;
-
-        return code;
-      }
-
+  var customBlocks = mainCustomComponents.children;
+  customBlocksLength = customBlocks.length;
+  for (i = 0; i  < customBlocksLength; i++) {
+    customBlocks = mainCustomComponents.children;
+    cblock = customBlocks[0];
+    cblock.removeAttribute('x');
+    cblock.removeAttribute('y');
+    dom.appendChild(cblock);
+  }
 
 
 
 
+  var defineDbBlocks = defineDbComponents.children;
+  defineDbBlocksLength = defineDbBlocks.length;
+  for (i = 0; i  < defineDbBlocksLength; i++) {
+    defineDbBlocks = defineDbComponents.children;
+    cblock = defineDbBlocks[0];
+    cblock.removeAttribute('x');
+    cblock.removeAttribute('y');
+    dom.appendChild(cblock);
+  }
 
-      function getBlocklyValueOptimized()
-      {
+  dom.appendChild(mainProgramBlocklycomponent);
 
-        var dom = Blockly.Xml.workspaceToDom(workspace);
-        var headlessWorkspace = new Blockly.Workspace();
-        rearrangeDom(dom);
-        Blockly.Xml.domToWorkspace(dom, headlessWorkspace);
-
-
-        var inline = Blockly.ClojureScriptOptimized.workspaceToCode(headlessWorkspace);
-        headlessWorkspace.dispose();
-        var code = inline;
-        //console.log(code);
-        return code;
-      }
-
-      var lastkeypresstime = -1;
-      var updateblockly = false;
-      var keypressed = false;
-      var savelagtime = 3000;
-
-      lastEval = -1;
-
-      function myChangeFunction(event) {
-        //console.log("Event.type= " + event.type + " : " + event.oldValue  + " : " + event.newValue + " : " + event.name + " : " + event.blockId);
-        if (event.name == "TABLENAME") {
-          if (myappshare.mainapp.table_block_exists(event.blockId)) {
-            myappshare.mainapp.set_new_table_name( event.blockId, event.newValue );
-          }
-          else {
-            myappshare.mainapp.set_old_table_name( event.blockId, event.oldValue );
-          }
-        } else if (event.name == "COLUMNNAME") {
-          var bblock = workspace.getBlockById(event.blockId);
-          var prbl = bblock.getParent();
-          var tablename = prbl.getFieldValue('TABLENAME');
-          console.log("    column: " +   event.newValue + " :parent id:" + prbl.id + " :parent name:" + tablename);
-          if (myappshare.mainapp.table_block_exists(prbl.id)) {
-          }
-          else {
-            myappshare.mainapp.set_new_table_name( prbl.id, tablename );
-          }
-
-
-          if (myappshare.mainapp.column_block_exists(prbl.id ,  event.blockId)) {
-            myappshare.mainapp.set_new_column_name(  prbl.id, event.blockId, event.newValue );
-          }
-          else {
-            myappshare.mainapp.set_old_column_name(  prbl.id, event.blockId, event.oldValue );
-          }
-
-        };
-
-        if ((event.type == Blockly.Events.CHANGE) &&
-            (event.oldValue != event.newValue)) {
-          if ((lastkeypresstime == -1) || ((new Date().getTime() - lastkeypresstime) < savelagtime)) {
-            updateblockly = false;
-          } else {
-            updateblockly = true;
-          };
-
-          keypressed = true;
-
-          lastkeypresstime = new Date().getTime();
-          return;}
+}
 
 
 
-        if (event.type == 'ui') {return;}
-        inEval = myappshare.mainapp.inEval();
-        if (inEval) {return;}
-        calcEvals = myappshare.mainapp.calcEvals();
-        //console.log("Event.type= " + event.type + " : " + calcEvals);
-        if (calcEvals == lastEval) {return;}
-
-        lastEval = calcEvals;
 
 
-        updateblockly = true;
 
-      }
+
+
+
+
+// --------------------------------------------------------------------
+//                      function getBlocklyValue
+//
+//  input:        none
+//  output:       the clojure code represented by blockly
+//  dependencies: The global blockly workspace
+//
+// --------------------------------------------------------------------
+function getBlocklyValue()
+{
+
+  var dom = Blockly.Xml.workspaceToDom(workspace);
+  var headlessWorkspace = new Blockly.Workspace();
+  rearrangeDom(dom);
+  Blockly.Xml.domToWorkspace(dom, headlessWorkspace );
+
+
+  var inline = Blockly.ClojureScript.workspaceToCode(headlessWorkspace);
+  headlessWorkspace.dispose();
+  var code = inline;
+
+  return code;
+}
+
+
+
+// --------------------------------------------------------------------
+//                      function getBlocklyOptimizedValue
+//
+//  input:        none
+//  output:       the clojurescript code represented by blockly with no
+//                macros
+//  dependencies: The global blockly workspace
+//
+// --------------------------------------------------------------------
+function getBlocklyOptimizedValue()
+{
+
+  var dom = Blockly.Xml.workspaceToDom(workspace);
+  var headlessWorkspace = new Blockly.Workspace();
+  rearrangeDom(dom);
+  Blockly.Xml.domToWorkspace(dom, headlessWorkspace );
+
+
+  var inline = Blockly.ClojureScriptOptimized.workspaceToCode(headlessWorkspace);
+  headlessWorkspace.dispose();
+  var code = inline;
+
+  return code;
+}
+
+
+
+
+
+
+// --------------------------------------------------------------------
+//                      function getBlocklyValueOptimized
+//
+//  input:        none
+//  output:       the clojurescript code represented by blockly with no
+//                macros
+//  dependencies: The global blockly workspace
+//
+// --------------------------------------------------------------------
+function getBlocklyValueOptimized()
+{
+
+  var dom = Blockly.Xml.workspaceToDom(workspace);
+  var headlessWorkspace = new Blockly.Workspace();
+  rearrangeDom(dom);
+  Blockly.Xml.domToWorkspace(dom, headlessWorkspace);
+
+
+  var inline = Blockly.ClojureScriptOptimized.workspaceToCode(headlessWorkspace);
+  headlessWorkspace.dispose();
+  var code = inline;
+  //console.log(code);
+  return code;
+}
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------
+//                      function myChangeFunction
+//
+//  input:        a blockly event
+//  output:       ??
+//  dependencies: The global blockly workspace
+//
+// --------------------------------------------------------------------
+function myChangeFunction(event) {
+  //console.log("Event.type= " + event.type + " : " + event.oldValue  + " : " + event.newValue + " : " + event.name + " : " + event.blockId);
+  if (event.name == "TABLENAME") {
+    if (myappshare.mainapp.table_block_exists(event.blockId)) {
+      myappshare.mainapp.set_new_table_name( event.blockId, event.newValue );
+    }
+    else {
+      myappshare.mainapp.set_old_table_name( event.blockId, event.oldValue );
+    }
+  } else if (event.name == "COLUMNNAME") {
+    var bblock = workspace.getBlockById(event.blockId);
+    var prbl = bblock.getParent();
+    var tablename = prbl.getFieldValue('TABLENAME');
+    console.log("    column: " +   event.newValue + " :parent id:" + prbl.id + " :parent name:" + tablename);
+    if (myappshare.mainapp.table_block_exists(prbl.id)) {
+    }
+    else {
+      myappshare.mainapp.set_new_table_name( prbl.id, tablename );
+    }
+
+
+    if (myappshare.mainapp.column_block_exists(prbl.id ,  event.blockId)) {
+      myappshare.mainapp.set_new_column_name(  prbl.id, event.blockId, event.newValue );
+    }
+    else {
+      myappshare.mainapp.set_old_column_name(  prbl.id, event.blockId, event.oldValue );
+    }
+
+  };
+
+  if ((event.type == Blockly.Events.CHANGE) &&
+      (event.oldValue != event.newValue)) {
+    if ((lastkeypresstime == -1) || ((new Date().getTime() - lastkeypresstime) < savelagtime)) {
+      updateblockly = false;
+    } else {
+      updateblockly = true;
+    };
+
+    keypressed = true;
+
+    lastkeypresstime = new Date().getTime();
+    return;}
+
+
+
+  if (event.type == 'ui') {return;}
+  inEval = myappshare.mainapp.inEval();
+  if (inEval) {return;}
+  calcEvals = myappshare.mainapp.calcEvals();
+  //console.log("Event.type= " + event.type + " : " + calcEvals);
+  if (calcEvals == lastEval) {return;}
+
+  lastEval = calcEvals;
+
+
+  updateblockly = true;
+
+}
 
 
 
