b78a2c09dd06299eee1246ef60d8f1a4a443d590
Zubair Quraishi <ZQ@corp.novocorp.net>
2017-01-30 19:41:01 +0100

    Changed Name to GoShareData
    New name is GoShareData

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-      <h1 class="display-2">Starship Data</h1>
+      <h1 class="display-2">Go Share Data</h1>
     <img width='100%' src="https://i.kinja-img.com/gawker-media/image/upload/s--PqnVC4BL--/c_scale,fl_progressive,q_80,w_800/18nscxevw4bh2png.png">
   </div>
 </template>
