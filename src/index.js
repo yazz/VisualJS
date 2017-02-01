0d50793b76ebd3dc302a269c25091fe5b453b838
Zubair Quraishi <ZQ@corp.novocorp.net>
2017-02-01 18:01:59 +0100

    see full http header
    see full http header

--- src/index.js
+++ src/index.js
@@ -218,7 +218,7 @@ app.get('/get_connect', function (req, res) {
                     ,
                     requestClientPublicHostName:      requestClientPublicHostName
                     ,
-                    version:      23
+                    version:      24
                 }
           ));
 })
@@ -235,7 +235,7 @@ app.get('/client_connect', function (req, res) {
     requestClientInternalPort        = req.query.requestClientInternalPort;
     requestClientPublicIp            = req.ip;
     //requestClientPublicHostName      = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    requestClientPublicHostName      = Object.keys(req);
+    requestClientPublicHostName      = JSON.stringify(req);
 
     console.log('Client attempting to connect from:');
     console.log('client internal host address:    ' + requestClientInternalHostAddress)
