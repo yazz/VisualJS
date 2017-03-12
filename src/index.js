73e0e8a899050dcc42ad306fb6c9fcc7b819bb60
Zubair Quraishi <ZQ@corp.novocorp.net>
2017-03-12 19:48:21 +0100

    Oracle driver works as well now
    This is good as it means that drivers can be loaded from a JSON file

--- src/index.js
+++ src/index.js
@@ -370,6 +370,8 @@ app.listen(port, hostaddress, function () {
 //connections['postgres'] = eval(pgeval)
 drivers['postgres'] = eval(pgeval)
 drivers['oracle'] = eval(toeval)
+drivers['oracle'].loadDriver()
+
 //console.log("postgres.get = " + JSON.stringify(eval(pgeval) , null, 2))
 //console.log("postgres.get = " + eval(pgeval).get)
 }
