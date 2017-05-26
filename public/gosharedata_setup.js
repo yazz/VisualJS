c5c07a2b2e94fb522f8a8d5d39822ad0ad0bdc8c
Zubair Quraishi <ZQ@corp.novocorp.net>
2017-05-26 10:17:28 +0200

    Smoother scrolling of windows
    Smoother scrolling of windows

--- public/gosharedata_setup.js
+++ public/gosharedata_setup.js
@@ -5,8 +5,20 @@
 
 
 function resetzoom() {
+  document.getElementById('zoommain').style['-webkit-transition-duration']='0.6s';
+  document.getElementById('zoommain').style['transition-duration']='0.6s';
+  document.getElementById('body').style['-webkit-transition-duration']='0.6s';
+  document.getElementById('body').style['transition-duration']='0.6s';
+
   document.getElementById('zoommain').style.transform='';
   document.getElementById('body').style.transform='';
+  
+  setTimeout(function(){
+	  document.getElementById('zoommain').style['-webkit-transition-duration']='';
+	  document.getElementById('zoommain').style['transition-duration']='';
+	  document.getElementById('body').style['-webkit-transition-duration']='';
+	  document.getElementById('body').style['transition-duration']='';
+  }, 800);
 };
 
 
