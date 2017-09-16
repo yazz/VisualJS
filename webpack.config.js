57ad1196fb373d6a066be738ab6706c2e5e5182f
Zubair Quraishi <zubairq@gmail.com>
2017-09-16 09:34:06 +0200

    Removed source maps from production
    Since the last change got the load time down by half a second, page size
    1.5MB, and loads only 13 files now

--- webpack.config.js
+++ webpack.config.js
@@ -130,7 +130,6 @@ module.exports = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
