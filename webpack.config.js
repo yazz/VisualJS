1ca80eab9d34c259c31d7a949bfd058be3cfe5db
Zubair Quraishi <zubairq@gmail.com>
2017-09-16 09:57:19 +0200

    Got file to 1.5MB again
    Still slow though... now trying to disable source maps again in
    production

--- webpack.config.js
+++ webpack.config.js
@@ -137,7 +137,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
