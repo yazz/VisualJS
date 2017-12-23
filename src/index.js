3c30be212abcc8fc084079d516edd20e9b6c49e2
Zubair Quraishi <zubairq@gmail.com>
2017-12-23 16:56:35 +0100

    Calculate the CPU load in Windows
    All the NodeJS modules I tried didn't work, but someone seemed to have
    figured it out here

--- src/index.js
+++ src/index.js
@@ -2722,34 +2722,48 @@ function startServices() {
 }
 
 
+function cpuAverage() {
 
+  //Initialise sum of idle and time of cores and fetch CPU info
+  var totalIdle = 0, totalTick = 0;
+  var cpus = os.cpus();
 
+  //Loop through CPU cores
+  for(var i = 0, len = cpus.length; i < len; i++) {
 
-var osu 	= require('os-utils');
+    //Select CPU core
+    var cpu = cpus[i];
 
-setInterval(
-        function() {
+    //Total up the time in the cores tick
+    for(var type in cpu.times) {
+      totalTick += cpu.times[type];
+   }     
 
-            var loads = os.loadavg();
-                console.log("loadavg: "+loads);
-            console.log("totalmem: "+os.totalmem());
-            console.log("freemem: "+os.freemem())
+    //Total up the idle time of the core
+    totalIdle += cpu.times.idle;
+  }
 
-            console.log( 'cpucount2 (%): ' + osu.cpuCount() );
-            console.log( 'loadvag2 (%): ' + osu.loadavg(1) );
-        },1000)
+  //Return the average Idle and Tick times
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+}
 
+//Grab first CPU Measure
+var startMeasure = cpuAverage();
 
+//Set delay for second Measure
+setInterval(function() { 
 
+  //Grab second Measure
+  var endMeasure = cpuAverage(); 
 
+  //Calculate the difference in idle and total time between the measures
+  var idleDifference = endMeasure.idle - startMeasure.idle;
+  var totalDifference = endMeasure.total - startMeasure.total;
 
-        var cpu = require('windows-cpu');
+  //Calculate the average percentage CPU usage
+  var percentageCPU = 100 - ~~(100 * idleDifference / totalDifference);
 
-        // Get total load on server for each CPU
-        cpu.totalLoad(function(error, results) {
-        	if(error) {
-        	return console.log(error);
-        	}
+  //Output result to console
+  console.log(percentageCPU + "% CPU Usage.");
 
-        	console.log('Total Windows  Load: ' + results.load);
-        });
+}, 1000);
