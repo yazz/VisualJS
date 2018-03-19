3376451b6f903040f08d70c0d70348294fedab28
System <contact@visifile.com>
2018-03-19 10:38:51 +0100

    Made a simple V2 driver
    Just outputs some text

--- src/outlook2010.js
+++ src/outlook2010.js
@@ -1,63 +1,14 @@
 {
     name: 'outlook2010'
     ,
-    type: 'document_driver'
-    ,
-    setup: function(connection, callfn) {
-          var config = {
-            id:                connection.id,
-            user:              connection.user,
-            database:          connection.database,
-            password:          connection.password,
-            host:              connection.host,
-            port:              connection.port,
-            max:               connection.max,
-            idleTimeoutMillis: connection.idleTimeoutMillis
-          };
-
-          connection.connection = new postgresdb.Client(config);
-          connection.connection.connect(function (err) {
-            if (err) {
-                callfn({error: '' + err});
-                throw err;
-            }
-          });
-          connection.status = 'connected';
-      },
-
-
-    get_v2: function( connection , parameters , callfn )
+    functions: [
         {
-			var sql = parameters.sql;
-            //console.log('******************************** in postgres get');
-            if (
-                (connection.status == 'disconnected')
-                ||
-                (connection.status == null)
-              )
-             {
-                 try {
-                    drivers['postgres']['setup'](connection, callfn);
-                 } catch (error) {
-                     console.log('postgres connection error: ' + error.toString());
-                     return;
-                 };
+            on: "startup",
+            do: `
+            function() {
+                console.log("outlook2010 driver")
             }
-
-          //console.error('drivers[postgres][get]');
-          // execute a query on our database
-          connection.connection.query(sql, [], function (err, result) {
-            if (err) {
-				callfn({error: '' + err});
-			} else {
-				// just print the result to the console
-				//console.log(result.rows); // outputs: { name: 'brianc' }
-				callfn(result.rows);
-			};
-
-
-            // disconnect the client
-            //connection.connection.end(function (err) {
-            });
-          }
+            `
+        }
+    ]
 }
