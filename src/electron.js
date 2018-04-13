35255a99da20c72787462cd6ae05aa2ffc3670c7
Sherlock Holmes <zubairq@gmail.com>
2018-04-13 13:37:26 +0200

    added child process helpers
    To create child processes and stuff like that

--- src/electron.js
+++ src/electron.js
@@ -160,6 +160,488 @@ var PDFParser       = require("pdf2json");
 
 
 
+
+
+
+
+
+
+
+
+
+function setUpChildListeners(processName, fileName, debugPort) {
+
+    forkedProcesses[processName].on('close', function() {
+        console.log("Child process " + processName + " exited.. restarting... ")
+        setupForkedProcess(processName, fileName, debugPort)
+    });
+
+
+    forkedProcesses[processName].on('message', (msg) => {
+        //console.log("message from child: " + JSON.stringify(msg,null,2))
+        //console.log("message type from child: " + JSON.stringify(msg.message_type,null,2))
+        if (msg.message_type == "return_test_fork") {
+            //console.log('Message from child', msg);
+            sendOverWebSockets({
+                                    type:   "test_fork",
+                                    value:  "Counter: " + msg.counter + ", count data_states from sqlite: " + msg.sqlite
+                                    });
+
+
+        } else if (msg.message_type == "return_set_connection") {
+
+
+
+        } else if (msg.message_type == "return_set_query") {
+
+            //console.log(".. Main process received a 'return_set_query' message")
+            var query = JSON.stringify(
+                          {  id:            msg.id,
+                             name:          msg.name,
+                             connection:    msg.connection,
+                             driver:        msg.driver,
+                             size:          msg.size,
+                             hash:          msg.hash,
+                             fileName:      msg.fileName,
+                             type:          msg.type,
+                             definition:    msg.definition,
+                             preview:       msg.preview
+                         })
+
+            sendOverWebSockets({
+                                    type: "uploaded",
+                                    id:    msg.id,
+                                    query:
+                                    {
+                                    }});
+
+            sendOverWebSockets({
+                                    type: "update_query_item",
+                                    query: query
+            });
+
+
+            sendOverWebSockets({   type: "client_get_all_queries_done"  });
+
+
+        // this needs to be fixed so that it only sends the similar documents
+        // to the client that requested them
+        } else if (msg.message_type == "return_similar_documents") {
+
+
+
+
+        } else if (msg.message_type == "createdTablesInChild") {
+            forkedProcesses["forked"].send({ message_type: "init" });
+            if (!mainNodeProcessStarted) {
+                mainNodeProcessStarted = true
+                getPort()
+            }
+
+
+        } else if (msg.message_type == "parentSetSharedGlobalVar") {
+
+
+
+
+
+        } else if (msg.message_type == "getResultReturned") {
+            var newres = queuedResponses[ msg.seqNum ]
+            newres.writeHead(200, {'Content-Type': 'text/plain'});
+            newres.end(JSON.stringify(msg.result));
+            newres = null;
+
+
+
+
+        } else if (msg.message_type == "returnDownloadWebDocument") {
+            var rett = eval("(" + msg.returned + ")");
+            var newres = queuedResponses[ msg.seq_num ]
+
+            newres.writeHead(200, {'Content-Type': 'text/plain'});
+            newres.end(JSON.stringify({  result: rett.result}));
+
+            newres = null;
+
+
+
+        } else if (msg.message_type == "return_add_local_driver_results_msg") {
+            //console.log("6 - return_get_search_results: " + msg.returned);
+            var rett = eval("(" + msg.success + ")");
+            var newCallbackFn = queuedResponses[ msg.seq_num ]
+
+            if (msg.success) {
+                newCallbackFn("Driver added")
+            } else {
+                newCallbackFn("Driver not added: " + msg.error_message)
+            }
+
+
+            newres = null;
+
+
+
+
+        } else if (msg.message_type == "return_get_search_results") {
+            //console.log("6 - return_get_search_results: " + msg.returned);
+            var rett = eval("(" + msg.returned + ")");
+            var newres = queuedResponses[ msg.seq_num ]
+
+            newres.writeHead(200, {'Content-Type': 'text/plain'});
+            newres.end(msg.returned);
+
+            newres = null;
+
+
+
+    } else if (msg.message_type == "return_get_search_results_json") {
+        console.log("3 - /client/1/search: return_get_search_results_json")
+
+            //console.log("6 - return_get_query_results: " + JSON.stringify(msg,null,2));
+            var rett = eval("(" + msg.returned + ")");
+            var newres = queuedResponses[ msg.seq_num ]
+
+            var result = {
+                            message:          "Search results for: '" + msg.search_term + "'",
+                            results_count:    -1,
+                            links:           {"self": { "href": "/client/1/search" }},
+                            results:         []
+                        }
+
+            var realCount = 0
+            for (var i = 0; i < rett.data_states.length; i++) {
+                var resitem = rett.data_states[i];
+                if (resitem && (resitem.data.length > 0)) {
+                    result.results.push({
+                        query_id:      resitem.id,
+                        computer_name: username + "@" + hostaddress + ":" + port,
+                        file_name:          resitem.file_name,
+						name:               resitem.name,
+                        type:               resitem.type,
+                        size:               resitem.size,
+                        driver:             resitem.driver,
+                        when_timestamp:     resitem.when_timestamp
+                    })
+                    realCount ++
+                }
+            }
+            result.results_count = realCount
+//msg.result
+
+
+            newres.writeHead(200, {'Content-Type': 'application/json'});
+            newres.end(JSON.stringify(result));
+
+            newres = null;
+
+
+
+
+
+        } else if (msg.message_type == "return_get_query_results") {
+                //console.log("6 - return_get_query_results: " + msg.result);
+                var rett = eval("(" + msg.result + ")");
+                var newres = queuedResponses[ msg.seq_num ]
+
+                newres.writeHead(200, {'Content-Type': 'text/plain'});
+                newres.end(msg.result);
+
+                newres = null;
+
+
+
+
+
+
+        } else if (msg.message_type == "return_get_all_table") {
+                //console.log("6 - return_get_all_table: " );
+                var newres = queuedResponses[ msg.seq_num ]
+
+                newres.writeHead(200, {'Content-Type': 'text/plain'});
+                newres.end(msg.result);
+
+                newres = null;
+
+
+
+        } else if (msg.message_type == "returnDownloadDocuments") {
+            var newres = queuedResponses[ msg.seq_num ]
+
+            if (msg.returned.error) {
+                newres.end(JSON.stringify({  error: msg.returned.error}));
+
+            } else if (msg.returned.result) {
+                var contentRecord = msg.returned.result
+                var content = Buffer.from(JSON.parse(msg.content).data);
+
+                //console.log("9: " + contentRecord.content_type);
+                //console.log("10: " + contentRecord.id);
+                //console.log("11: " + contentRecord.driver);
+                //console.log("12: " + content.length);
+                //console.log("13: " + content);
+                //console.log("14: " + Object.keys(contentRecord));
+                newres.writeHead(
+
+                                200,
+
+                                {
+                                    'Content-Type': contentRecord.content_type,
+                                    'Content-disposition': 'attachment;filename=' + contentRecord.id  + "." + getFileExtension(contentRecord.driver),
+                                    'Content-Length': content.length
+                                });
+
+
+                newres.end(new Buffer(  content, 'binary'  ));
+            } else {
+                newres.end(JSON.stringify({  error: "No results"}));
+            }
+
+            newres = null;
+
+
+        } else if (msg.message_type == "returnIntranetServers") {
+            var newres = queuedResponses[ msg.seq_num ]
+
+            newres.writeHead(200, {'Content-Type': 'text/plain'});
+
+
+            if (msg.returned) {
+                newres.end( JSON.stringify( {  allServers:         msg.returned,
+                                               intranetPublicIp:   msg.requestClientPublicIp}) );
+            } else {
+                //console.log( "8: " + msg.error );
+                newres.end(JSON.stringify( {  allServers:        [],
+                                              intranetPublicIp:  msg.requestClientPublicIp}) );
+            }
+            newres = null;
+
+
+
+
+        } else if (msg.message_type == "returnIntranetServers_json") {
+            var newres = queuedResponses[ msg.seq_num ]
+
+            newres.writeHead(200, {'Content-Type': 'application/json'});
+
+            var result = {
+                            list:               [],
+                            links:              {"self": { "href": "/start" }},
+                        }
+
+
+            if (msg.returned) {
+                result.links.servers    = {}
+                result.intranetPublicIp = msg.requestClientPublicIp
+                result.error            = false
+                result.count            = msg.returned.length
+
+                if (msg.returned.length > 0) {
+
+                    result.main_user    = msg.returned[0].client_user_name
+                    result.main         = msg.returned[0].internal_host + ":" + msg.returned[0].internal_port
+                    result.main_url     = "http://" +  msg.returned[0].internal_host + ":" +
+                                            msg.returned[0].internal_port + "/home"
+                }
+
+
+                for (var i =0 ; i< msg.returned.length; i ++) {
+
+                    var addr = msg.returned[i].internal_host + ":" + msg.returned[i].internal_port
+                    result.list.push( addr )
+                    result.links.servers[addr] =
+                        {"href":        "http://" +  addr + "/home" ,
+                         "user":         msg.returned[i].client_user_name}
+                    }
+
+                    newres.end(JSON.stringify(result));
+            } else {
+                newres.end(JSON.stringify( {  allServers:        [],
+                                              error:              true}) );
+            }
+            newres = null;
+
+
+
+
+
+
+        } else if (msg.message_type == "returnClientConnect") {
+            //console.log("6: returnClientConnect")
+            //console.log("6.1: " + msg)
+            //console.log("7: " + msg.returned)
+            var newres = queuedResponses[ msg.seq_num ]
+
+
+
+            if (msg.returned) {
+                newres.writeHead(200, {'Content-Type': 'text/plain'});
+                newres.end( JSON.stringify( JSON.stringify({  connected:         msg.returned })) );
+            }
+            newres = null;
+
+
+
+
+
+        } else if (msg.message_type == "return_query_item") {
+            //console.log("6: return_query_item")
+            //console.log("6.1: " + msg)
+            //console.log("7: " + msg.returned)
+            var new_ws = queuedResponses[ msg.seq_num ]
+
+            if (msg.returned) {
+                sendToBrowserViaWebSocket(
+                new_ws,
+                {
+                    type: "update_query_item",
+                    query: msg.returned
+                });
+            }
+
+
+
+
+
+
+
+        } else if (msg.message_type == "return_query_items_ended") {
+            //console.log("6: return_query_items_ended")
+            //console.log("6.1: " + msg)
+            var new_ws = queuedResponses[ msg.seq_num ]
+
+            sendToBrowserViaWebSocket(      new_ws,
+                                        {   type: "client_get_all_queries_done"  });
+            //new_ws = null;
+        }
+
+
+//
+//
+
+
+
+
+
+
+
+    });
+}
+
+
+
+
+
+
+
+
+
+function setupForkedProcess(processName,fileName,debugPort) {
+    var debugArgs =[];
+    if (debug) {
+        if (semver.gte(process.versions.node, '6.9.0')) {
+            debugArgs = ['--inspect=' + debugPort];
+        } else {
+            debugArgs = ['--debug=' + debugPort];
+        };
+    };
+    if (isWin) {
+        forkedProcesses[  processName  ] = fork.fork(path.join(__dirname, '../src/' + fileName), [], {execArgv: debugArgs});
+    } else {
+        forkedProcesses[  processName  ] = fork.fork(path.join(__dirname, '../src/' + fileName), [], {execArgv: debugArgs});
+    }
+    setUpChildListeners(processName, fileName, debugPort);
+
+
+    if (processName == "forked") {
+        forkedProcesses["forked"].send({ message_type: "createTables" });
+        forkedProcesses["forked"].send({ message_type: "greeting", hello: 'world' });
+    }
+
+    if (processName == "forkedIndexer") {
+        forkedProcesses["forkedIndexer"].send({ message_type: "init" });
+		forkedProcesses["forkedIndexer"].send({ message_type: "childRunIndexer" });
+    }
+
+
+
+    if (processName == "forkedFileScanner") {
+        if (runServices) {
+            forkedProcesses["forkedFileScanner"].send({ message_type: "init" });
+        }
+    }
+
+    if (processName == "forkedPowershell") {
+        forkedProcesses["forkedPowershell"].send({ message_type: "init" });
+        if (runServices) {
+            forkedProcesses["forkedPowershell"].send({ message_type: "call_powershell" });
+        }
+    }
+
+
+
+    console.log("Started subprocess '" + processName + "' ")
+
+
+}
+
+
+
+
+
+
+function setupChildProcesses() {
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+
+    setupForkedProcess("forkedIndexer","child.js", 40000)
+    setupForkedProcess("forkedFileScanner","child.js", 40001)
+
+    if (isWin) {
+        setupForkedProcess("forkedPowershell","powershell.js", 40002)
+    }
+}
+
+
+
+
+
+function setupChildProcesses2() {
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+    //console.log("-------------------------------------------------------------------");
+
+    setupForkedProcess("forked", "child.js", 40003)
+}
+
+
+
+
+
+
+function sendOverWebSockets(data) {
+    var ll = serverwebsockets.length;
+    //console.log('send to sockets Count: ' + JSON.stringify(serverwebsockets.length));
+    for (var i =0 ; i < ll; i++ ) {
+        var sock = serverwebsockets[i];
+        sock.emit(data.type,data);
+        //console.log('                    sock ' + i + ': ' + JSON.stringify(sock.readyState));
+    }
+}
+
+
+
+
+
+
+
+
+
+
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
