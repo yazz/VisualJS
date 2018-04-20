915a29bff40e41a01b650af5440d0ddd4abd9d40
Sherlock Holmes <zubairq@gmail.com>
2018-04-20 09:08:28 +0200

    Testing cross platform notifications
    I am trying node-notifier as this seems to be an issue getting the native Electron notifications to work cross platform on WIndows and Mac

--- src/electron.js
+++ src/electron.js
@@ -106,6 +106,45 @@ var queuedResponses                     = new Object();
 var queuedResponseSeqNum                = 0;
 var alreadyOpen = false;
 
+
+
+
+
+
+
+
+const notifier = require('node-notifier');
+
+notifier.notify(
+  {
+    title: 'My awesome title',
+    message: 'Hello from node, Mr. User!',
+    icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+    sound: true, // Only Notification Center or Windows Toasters
+    wait: true // Wait with callback, until user action is taken against notification
+  },
+  function(err, response) {
+    // Response is response from notification
+  }
+);
+
+notifier.on('click', function(notifierObject, options) {
+  // Triggers if `wait: true` and user clicks notification
+});
+
+notifier.on('timeout', function(notifierObject, options) {
+  // Triggers if `wait: true` and notification closes
+});
+
+
+
+
+
+
+
+
+
+
 console.log('Starting services');
 
 app.use(compression())
