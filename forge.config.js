d0d76bb7c1cbc74bea546c2db626ec7c4e36384f
Sherlock Holmes <zubairq@gmail.com>
2018-04-20 19:01:43 +0200

    Electron ASAR packaging doesn't work with node-notifications

--- forge.config.js
+++ forge.config.js
@@ -8,7 +8,7 @@ module.exports = {
     }
     ,
     "electronPackagerConfig": {
-        "asar": true,
+        "asar": false,
         "packageManager": "npm",
         "extraResources": [
           "src/",
