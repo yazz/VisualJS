319dad2c1aa64dd3b8faf9e5051a14f426003ade
Sherlock Holmes <zubairq@gmail.com>
2018-04-20 09:52:41 +0200

    dont show notification in headless mode
    dont show notification in headless mode

--- src/electron.js
+++ src/electron.js
@@ -113,31 +113,6 @@ var alreadyOpen = false;
 
 
 
-const notifier = require('node-notifier');
-
-notifier.notify(
-  {
-    title: 'My awesome title',
-    message: 'Hello from node, Mr. User!',
-    icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
-    sound: true, // Only Notification Center or Windows Toasters
-    wait: true // Wait with callback, until user action is taken against notification
-  },
-  function(err, response) {
-    // Response is response from notification
-  }
-);
-
-notifier.on('click', function(notifierObject, options) {
-  // Triggers if `wait: true` and user clicks notification
-});
-
-notifier.on('timeout', function(notifierObject, options) {
-  // Triggers if `wait: true` and notification closes
-});
-
-
-
 
 
 
@@ -2550,7 +2525,36 @@ function startServices() {
               myNotification.onclick = () => {
                 console.log('Notification clicked')
               }
+
+
+
+
+                      const notifier = require('node-notifier');
+
+                      notifier.notify(
+                        {
+                          title: 'My awesome title',
+                          message: 'Hello from node, Mr. User!',
+                          icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+                          sound: true, // Only Notification Center or Windows Toasters
+                          wait: true // Wait with callback, until user action is taken against notification
+                        },
+                        function(err, response) {
+                          // Response is response from notification
+                        }
+                      );
+
+                      notifier.on('click', function(notifierObject, options) {
+                        // Triggers if `wait: true` and user clicks notification
+                      });
+
+                      notifier.on('timeout', function(notifierObject, options) {
+                        // Triggers if `wait: true` and notification closes
+                      });
+
+
         }
+
 
 
 
