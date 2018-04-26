6b63b40728184260e7b3c7fa17f5dfe0b7e9f3c8
Sherlock Holmes <zubairq@gmail.com>
2018-04-26 18:23:13 +0200

    Created a table to hold the jobs queue to be processed

--- src/db_helper.js
+++ src/db_helper.js
@@ -50,7 +50,9 @@ module.exports = {
 
             "CREATE TABLE IF NOT EXISTS data_atlas ( id	TEXT, type	TEXT,name	TEXT, parent	TEXT,parent_root	TEXT,properties	TEXT);",
 
-            "CREATE TABLE IF NOT EXISTS system_process_info (process	TEXT PRIMARY KEY, process_id	TEXT, running_since	TEXT, job_count	INTEGER);"
+            "CREATE TABLE IF NOT EXISTS system_process_info (process	TEXT PRIMARY KEY, process_id	TEXT, running_since	TEXT, job_count	INTEGER);",
+
+            "CREATE TABLE IF NOT EXISTS system_process_jobs (id	TEXT,	driver	TEXT,	method	TEXT,	status	TEXT,	args	TEXT,	return_value	TEXT,	creation_time	INTEGER,	execution_duration	INTEGER,process	TEXT);"
                 ],
 
         function(a,b){
