ec34a74a357e457186f93f6ae638c78ec1df85d7
Sherlock Holmes <zubairq@gmail.com>
2018-05-23 21:00:40 +0200

    Can change data in the search app

--- public/visifile_drivers/apps/search.js
+++ public/visifile_drivers/apps/search.js
@@ -16,20 +16,34 @@
         "This will return the search app": {
             on: "app",
             do: function(args, returnfn) {
-                returnfn(
-                    new Moon({
-                      el: "#" + args.root_element
-                      ,
-                      template: "<div>this is the moon seach app</div>"
-                      ,
-                      data: {
-                        msg: ""
-                    },
-                    methods: {
-                    },
-                      store: store
-                    })
-                )
+                var mm = new Moon({
+                  el: "#" + args.root_element
+                  ,
+                  template: "<div>this is the moon seach app {{msg}}</div>"
+                  ,
+                  data: {
+                    msg: "..."
+                },
+                methods: {
+                    search: function() {
+                        callDriverMethod( "commandLine",
+                                          "ls"
+                                          ,{}
+                                    ,
+                                    function(result) {
+                                        console.log("3) returned result: " + JSON.stringify(result,null,2))
+                                    })
+                    }
+                },
+                  store: store
+                })
+                setTimeout(function() {
+                    mm.set("msg", "zoo")
+                    alert("done")
+                }
+                ,1000)
+
+                returnfn({})
 
 
             }, end: null
