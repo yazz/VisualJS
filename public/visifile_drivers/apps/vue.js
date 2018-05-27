2af61a4accade6fdc344eed10b8a37088e5889e2
Sherlock Holmes <zubairq@gmail.com>
2018-05-27 22:02:38 +0200

    VueJS apps now load into DataBox

--- public/visifile_drivers/apps/vue.js
+++ public/visifile_drivers/apps/vue.js
@@ -16,14 +16,15 @@
         "This will return the test app": {
             on: "app",
             do: function(args, returnfn) {
-                returnfn(
-                    new Vue({
-                      el: "#" + args.root_element
-                      ,
-                      template: `Okhay this is a Vue test app: 2
-                       `
+                new Vue({
+                  el: "#" + args.root_element
+                  ,
+                  template: `<div>Okhay this is a Vue test app: 2</div>
+                   `
 
-                    })
+                })
+                //alert("root: " + args.root_element +".")
+                returnfn(
                 )
 
 
