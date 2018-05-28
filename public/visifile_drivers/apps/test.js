2bec54f393d872e2e4734e51ac55855c5ccf2a99
Sherlock Holmes <zubairq@gmail.com>
2018-05-28 08:21:38 +0200

    Dynamically load a component into test app

--- public/visifile_drivers/apps/test.js
+++ public/visifile_drivers/apps/test.js
@@ -16,34 +16,39 @@
         "This will return the test app": {
             on: "app",
             do: function(args, returnfn) {
-                returnfn(
-                    new Vue({
-                      el: "#" + args.root_element
-                      ,
-                      template: `<div v-on:click='loadc'>Okhay this is a test app: {{msg}} 2
-                        <component v-bind:is="{component_name}" v-if="component_name" > </component>
-                      </div>
-                       `
-                      ,
-                      data: {
-                        msg: "some data",
-                        component_name: null
-                    },
-                    methods: {
-                        loadc: function() {
-                            alert("Loading")
-                            Vue.component('button-counter', {
-                              data: function () {
-                                return {
-                                  count: 0
-                                }
-                              },
-                              template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-                            })
-                            this.component_name = 'button-counter'
-                        }
+
+                new Vue({
+                  el: "#" + args.root_element
+                  ,
+                  template: `<div>
+                                <div v-on:click='loadc'>Okhay this is a test app: {{msg}} 2
+                                </div>
+                                <component v-bind:is="component_name" v-if="component_name" > ccc </component>
+                            </div>
+                   `
+                  ,
+                  data: {
+                    msg: "some data",
+                    component_name: null
+                },
+                methods: {
+                    loadc: function() {
+                        Vue.component('button-counter', {
+                          data: function () {
+                            return {
+                              count: 0
+                            }
+                          },
+                          template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+                        })
+                        this.component_name = 'button-counter'
                     }
-                    })
+                }
+                })
+
+
+
+                returnfn(
                 )
 
 
