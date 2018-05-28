8a22d54e25309f4a3889e756b0ecc6e926369a18
Sherlock Holmes <zubairq@gmail.com>
2018-05-28 08:16:56 +0200

    trying to add components

--- public/visifile_drivers/apps/test.js
+++ public/visifile_drivers/apps/test.js
@@ -20,13 +20,28 @@
                     new Vue({
                       el: "#" + args.root_element
                       ,
-                      template: `<div>Okhay this is a test app: {{msg}} 2</div>
+                      template: `<div v-on:click='loadc'>Okhay this is a test app: {{msg}} 2
+                        <component v-bind:is="{component_name}" v-if="component_name" > </component>
+                      </div>
                        `
                       ,
                       data: {
-                        msg: "some data"
+                        msg: "some data",
+                        component_name: null
                     },
                     methods: {
+                        loadc: function() {
+                            alert("Loading")
+                            Vue.component('button-counter', {
+                              data: function () {
+                                return {
+                                  count: 0
+                                }
+                              },
+                              template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+                            })
+                            this.component_name = 'button-counter'
+                        }
                     }
                     })
                 )
