2d1e3f1ff0ebb6d82cbba234ab763d5e42e35bd4
Sherlock Holmes <zubairq@gmail.com>
2018-06-03 07:48:46 +0200

    Code can be saved from the browser! YAY!!!

--- public/visifile_drivers/apps/appEditor.js
+++ public/visifile_drivers/apps/appEditor.js
@@ -96,6 +96,17 @@
             do: function(args,returnfn) {
                 console.log("Saving in Sqlite: " + args.code_id)
                 console.log("Saving in Sqlite: " + args.code)
+                var stmtUpdateCode = dbsearch.prepare(" update   system_code   set code = ? where id = ?");
+                    dbsearch.serialize(function() {
+                    dbsearch.run("begin exclusive transaction");
+                    stmtUpdateCode.run(
+                          args.code,
+                          args.code_id)
+
+                    dbsearch.run("commit");
+                    stmtUpdateCode.finalize();
+
+                })
                 returnfn({})
             }
             ,
