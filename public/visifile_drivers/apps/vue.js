390b1807c2f21b8a2f85631dad6a75eb536e9692
Sherlock Holmes <zubairq@gmail.com>
2018-06-24 21:00:44 +0200

    Testing in browser promises

--- public/visifile_drivers/apps/vue.js
+++ public/visifile_drivers/apps/vue.js
@@ -1,16 +1,36 @@
 function(args) {
 /*
+base_component_id("vue")
+created_timestamp(1529866767800)
 is_app(true)
 display_name("VueJS test App")
-base_component_id("vue")
 description("This will return the Vue test app")
 */
 
-    Vue.component("VueApp", {
-      template: `<div>Vue test app:</div>
+    var ww = Vue.component("VueApp", {
+      template: `<div>Vue test app:
+        <div>{{value}}</div>
+      </div>
        `
+       ,data: function() {
+        return {
+                    value: "waiting for promise..."
+        }},
+        mounted: async function() {
+            var ww =this
+            var promise = new Promise(result => {
+                setTimeout(function() {
+                    //alert(1)
+                    result( "Promise fulfilled...")
+                },2500)
+            });
+            var ret = await promise;
+            ww.value = ret;
+        }
+
 
     })
+
 
     return {name: "VueApp"}
 }
