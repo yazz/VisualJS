0d5c3e9dde7c6f51549b198bf302a987562402b1
Sherlock Holmes <zubairq@gmail.com>
2018-06-25 17:04:13 +0200

    made the Vue app async

--- public/visifile_drivers/apps/vue.js
+++ public/visifile_drivers/apps/vue.js
@@ -6,43 +6,32 @@ is_app(true)
 display_name("VueJS test App")
 description("This will return the Vue test app")
 */
+var promise = new Promise(result => {
+    setTimeout(function() {
+        //alert(1)
+        result( "Promise fulfilled...")
 
-   var ww = Vue.component("VueApp", {
-     template: `<div>Vue test app:
-       <div>{{value}}</div>
-     </div>
-      `
-      ,data: function() {
-       return {
-                   value: "waiting for promise..."
-       }},
-       mounted: async function() {
-           var ww =this
-           var promise = new Promise(result => {
-               setTimeout(function() {
-                   //alert(1)
-                   result( "Promise fulfilled...")
-               },2500)
-           });
-           var ret = await promise;
-           ww.value = ret;
+           var ww = Vue.component("VueApp", {
+             template: `<div>Vue test app:
+               <div>{{value}}</div>
+             </div>
+              `
+              ,data: function() {
+               return {
+                           value: "waiting for promise..."
+               }},
+               mounted: async function() {
+                   var ww =this
 
 
-           var promise2 = new Promise(result => {
-           setTimeout(function() {
-               result( "Promise fulfilled.2..")
-           },6500)
-           })
-
-           setTimeout(async function() {
-               var ret2 = await promise2;
-               alert(ret2);
-           },1500)
-           ;
-       }
+                   ww.value = "DONE";
+               }
 
 
-   })
+       })
+   },2500)
+});
+   var ret = await promise;
 
 
    return {name: "VueApp"}
