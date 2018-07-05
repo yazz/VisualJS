6c6f5fe569f41ab392b3752fa375f51cbd29052d
Sherlock Holmes <zubairq@gmail.com>
2018-07-05 17:09:58 +0200

    Can click on app image in the app store to go to app

--- public/visifile_drivers/apps/listApps.js
+++ public/visifile_drivers/apps/listApps.js
@@ -16,11 +16,13 @@ load_once_from_file(true)
                         <div class="card" style="width: 20rem;" v-for="item in apps">
                         <img    v-if='item.logo_url'
                                 v-bind:src='item.logo_url'
-                                style='width: 100%;'></img>
+                                style='width: 100%;'
+                                v-on:click='document.location="/?goto=" + item.display_name;return false;'
+                                ></img>
                           <div class="card-body">
                             <h4 class="card-title">{{item.display_name}}</h4>
                             <p class="card-text"></p>
-                            <a v-bind:href='"http://139.162.228.5/?goto=" + item.display_name' class="btn btn-primary">Run</a>
+                            <a v-bind:href='"/?goto=" + item.display_name' class="btn btn-primary">Run</a>
                           </div>
                         </div>
                         </div>
