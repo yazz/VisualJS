85b6aeb0d769b0ef5eb29ffcd9ef88b276d2b4fc
Sherlock Holmes <zubairq@gmail.com>
2018-07-14 16:15:59 +0200

    Made saveCode async

--- public/visifile_drivers/apps/appEditorV2SaveCode.js
+++ public/visifile_drivers/apps/appEditorV2SaveCode.js
@@ -1,15 +1,20 @@
-function saveCode( args ) {
+async function saveCode( args ) {
 /*
 base_component_id("appEditorV2SaveCode")
 description("Save the editor code")
 load_once_from_file(true)
 */
+    var promise = new Promise(returnFn => {
+        console.log("in SaveCode::")
+        console.log(JSON.stringify(args,null,2))
+        if (args) {
+            saveCodeV2(  args.base_component_id, args.code_id  ,  args.code)
+        }
+        console.log("leaving SaveCode::")
+        returnFn({})
+    })
 
-    console.log("in SaveCode::")
-    console.log(JSON.stringify(args,null,2))
-    if (args) {
-        saveCodeV2(  args.base_component_id, args.code_id  ,  args.code)
-    }
-    console.log("leaving SaveCode::")
+    var ret = await promise;
+    //return ret
     return args
 }
