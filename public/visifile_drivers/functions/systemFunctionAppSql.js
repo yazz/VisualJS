81b276ffdb8e370693a4ca431a946c681362b8ea
Sherlock Holmes <zubairq@gmail.com>
2018-07-15 13:10:02 +0200

    Hard coded an app SQL call

--- public/visifile_drivers/functions/systemFunctionAppSql.js
+++ public/visifile_drivers/functions/systemFunctionAppSql.js
@@ -5,18 +5,28 @@ base_component_id("systemFunctionAppSql")
 load_once_from_file(true)
 */
 
+
     var getSqlResults = new Promise(returnResult => {
-        dbsearch.serialize(
+        var dbPath = path.join(userData, '25efc280-87f4-11e8-b9e5-0f3fea0e9972' + '.visi.db')
+        console.log("dbPath: " + JSON.stringify(dbPath,null,2))
+        var appDb = new sqlite3.Database(dbPath);
+        appDb.run("PRAGMA journal_mode=WAL;")
+
+        appDb.serialize(
             function() {
-                dbsearch.all(
+                appDb.run("begin exclusive transaction");
+                appDb.all(
                     args.sql
                     ,
 
                     function(err, results)
                     {
+                    console.log("Results: " + JSON.stringify(results,null,2))
+                        appDb.run("commit");
+                        appDb.run("PRAGMA wal_checkpoint;")
                         returnResult(results)
                     })
-        }, sqlite3.OPEN_READONLY)
+         })
     })
 
 
