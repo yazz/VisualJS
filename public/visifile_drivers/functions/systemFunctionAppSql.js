b354d2e230fd4e05eaf8a54a00e53cb00d113a23
Sherlock Holmes <zubairq@gmail.com>
2018-07-15 13:12:12 +0200

    Name the app database as the base component ID

--- public/visifile_drivers/functions/systemFunctionAppSql.js
+++ public/visifile_drivers/functions/systemFunctionAppSql.js
@@ -7,7 +7,7 @@ load_once_from_file(true)
 
 
     var getSqlResults = new Promise(returnResult => {
-        var dbPath = path.join(userData, '25efc280-87f4-11e8-b9e5-0f3fea0e9972' + '.visi.db')
+        var dbPath = path.join(userData, args.base_component_id + '.visi.db')
         console.log("dbPath: " + JSON.stringify(dbPath,null,2))
         var appDb = new sqlite3.Database(dbPath);
         appDb.run("PRAGMA journal_mode=WAL;")
