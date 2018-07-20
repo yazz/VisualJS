f74c5a153bb457cbbf756f45b3fb226e2e2fb9e6
Sherlock Holmes <zubairq@gmail.com>
2018-07-20 21:25:55 +0200

    Allow embedded apps to take up the whole webpage

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -17,7 +17,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxASDxIPEBI
             component_name_2:    null
         }
       },
-      template: `<div  class="container-fluid">
+      template: `<div  class="container-fluid" style='padding:0;margin:0'>
 
                     <div class="row" style='background-color: black; color: white; padding-top: 20px;padding-bottom: 20px;'>
                         <div class="col">
