07db097487602b1d76f546615a14cf1d0721a948
Sherlock Holmes <zubairq@gmail.com>
2018-07-28 14:50:07 +0200

    moved logo more to the left

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -16,33 +16,34 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxASDxIPEBI
       template: `<div  class="container-fluid" style='padding:0;margin:0'>
 
                     <div class="row" style='background-color: black; color: white; padding-top: 20px;padding-bottom: 20px;'>
-                        <div class="col-md-1"></div>
                         <div class="col-md-5">
-                          <a-scene style='width: 80%; height: 20%;' embedded vr-mode-ui="enabled: false">
+                        <a-scene style='width: 80%; height: 20%;' embedded vr-mode-ui="enabled: false">
 
-                              <a-assets>
-                                  <a-mixin id="RobotoFont" text="font: /public/aframe_fonts/Roboto-msdf.json"></a-mixin>
-                                  <a-mixin id="SourceCodeProFont" text="font: /public/aframe_fonts/SourceCodePro.fnt"></a-mixin>
-                                  <a-mixin id="AileronFont" text="font: /public/aframe_fonts/Aileron-Semibold.fnt"></a-mixin>
-                              </a-assets>
+                            <a-assets>
+                                <a-mixin id="RobotoFont" text="font: /public/aframe_fonts/Roboto-msdf.json"></a-mixin>
+                                <a-mixin id="SourceCodeProFont" text="font: /public/aframe_fonts/SourceCodePro.fnt"></a-mixin>
+                                <a-mixin id="AileronFont" text="font: /public/aframe_fonts/Aileron-Semibold.fnt"></a-mixin>
+                            </a-assets>
 
-                                <a-entity camera look-controls>
-                                    <a-entity geometry="primitive: plane; height: 0.2; width: 0.2" position="1 0 -1"
-                                            material="opacity: 0"
-                                            spin="">
-                                                    <a-box position="2 10 -10" rotation="0 0 0" color="#4CC3D9"  >
-                                                  <a-entity
-                                                       mixin="RobotoFont"
-                                                       position="1 0 .6"
-                                                       text='color: white; align: left; value: AppShare ; width: 9; opacity:1;'>
-                                                       </a-entity>
-                                                   <a-animation attribute="position"
-                                											to="0 0 -1.5" dur="2000" direction="normal" ></a-animation>
-                                              </a-box>
-                                               </a-entity>
-                                               </a-entity>
-                                              <a-sky color="black"></a-sky>
-                            </a-scene>
+                              <a-entity camera look-controls>
+                                  <a-entity geometry="primitive: plane; height: 0.2; width: 0.2" position="1 0 -1"
+                                          material="opacity: 0"
+                                          >
+                                                  <a-box position="2 10 -10" rotation="0 0 0" color="#4CC3D9"  >
+                                                <a-entity
+                                                     mixin="RobotoFont"
+                                                     position="1 0 .6"
+                                                     text='color: white; align: left; value: AppShare ; width: 9; opacity:1;'>
+                                                     </a-entity>
+                                                 <a-animation attribute="position"
+                                                                          to="0 0 -1.5" dur="2000" direction="normal" ></a-animation>
+                                            </a-box>
+                                             </a-entity>
+                                             </a-entity>
+                                            <a-sky color="black"></a-sky>
+                          </a-scene>
+                        </div>
+                        <div class="col-md-1">
                         </div>
                         <div class="col-md-5 text-right">
                             Contact@AppShare.co   +45 2859 5405
