2256fc2b03757a65e0157df870d0d148196fb977
Sherlock Holmes <zubairq@gmail.com>
2018-08-21 19:12:32 +0200

    Use the mailchimp email system

--- public/visifile_drivers/apps/homepage_1.js
+++ public/visifile_drivers/apps/homepage_1.js
@@ -66,56 +66,23 @@ logo_url("https://moe.it.slotshaven.dk/wp/wp-content/uploads/2017/11/homepage.pn
               <div style='height:20px; width: 10px;'></div>
 
 
-              <template v-if='!subscribed'>
-                      <div class="form-group">
-                          <div class='text-center' style='font-weight:bold;padding-bottom: 10px;'>Subscribe to the Appshare newsletter</div>
-                          <input id=add placeholder="email address" type="email" class='form-control' v-model="email_address" style='margin-bottom: 10px;'></input>
-                          <button class="btn btn-info btn-block" v-on:click='insert_email(email_address)'>Subscribe</button>
-                      </div>
-              </template>
-              <template v-if='subscribed' >
-                  <div class='text-center' style='font-weight:bold;padding-bottom: 10px;'>
-                      Thanks for subscribing to the Appshare newsletter {{email_address}}!
-                  </div>
-              </template>
+              <div id="mc_embed_signup">
+<form action="https://zubairquraishi.us7.list-manage.com/subscribe/post?u=46afb6bb668c1280c3e739c54&amp;id=72288e6dc0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Subscribe to our mailing list</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_46afb6bb668c1280c3e739c54_72288e6dc0" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
 
 
             </div>
 </div>`
 
-,
-
-
-
-
-methods: {
-     insert_email: async function(email) {
-          await sql( "insert into users (id, email, when_created) values (?,?,?)"
-                      ,
-                      [  uuidv4(),  email  ,  new Date().getTime() ])
-          this.subscribed = true
-
-      }
-}
-,
-    data: function() {
-        return {
-                    apps: [],
-                    email_address: "",
-                    subscribed: false
-                }}
 
 
     })
-    /*
-    sqlite(
-    [
-        "Create the initial users table to store the email addresses",
-        [
-            "CREATE TABLE users (id	TEXT, email	TEXT, when_created INTEGER);"
-        ]
-
-    ])//sqlite
-
-    */
 }
