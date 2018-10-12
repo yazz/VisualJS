af2c1aa7e81b5c23cec1016ecd0f4df99fa26f69
Sherlock Holmes <zubairq@gmail.com>
2018-10-12 16:41:05 +0200

    Can now call app functions with async await :)

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -227,22 +227,20 @@ load_once_from_file(true)
 
             copyAppMethod: async function( appId , newAppId) {
                 var mm = this
-                callDriverMethod( {driver_name: "copyApp",
-                                   method_name: "copyAppshareApp"}
-                                  ,{
-                                      base_component_id:    appId,
-                                      new_app_id:           newAppId
-                                   }
-                            ,
-                            async function(result) {
-                                await mm.load_new_app( result.value.base_component_id )
-                                mm.$root.$emit('message', {
-                                                                type:               "insert_app_at",
-                                                                base_component_id:   result.value.base_component_id,
-                                                                card_index:          mm.card_index
-                                                            })
+                var result = await callFunction(
+                                    {
+                                        driver_name: "copyApp",
+                                        method_name: "copyAppshareApp"  }
+                                        ,{
+                                              base_component_id:    appId,
+                                              new_app_id:           newAppId   })
+                await mm.load_new_app( result.value.base_component_id )
+                mm.$root.$emit('message', {
+                                                type:               "insert_app_at",
+                                                base_component_id:   result.value.base_component_id,
+                                                card_index:          mm.card_index
+                                            })
 
-                            })
             }
             ,
 
