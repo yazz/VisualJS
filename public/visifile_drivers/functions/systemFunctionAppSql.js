b12fcbdada75aa54d9538b8cedbccecb772b3890
Sherlock Holmes <zubairq@gmail.com>
2018-10-31 16:55:54 +0100

    Don't access the app sqlite dbs in WAL mode

--- public/visifile_drivers/functions/systemFunctionAppSql.js
+++ public/visifile_drivers/functions/systemFunctionAppSql.js
@@ -10,7 +10,6 @@ load_once_from_file(true)
         var dbPath = path.join(userData, 'app_dbs/' + args.base_component_id + '.visi')
         //console.log("dbPath: " + JSON.stringify(dbPath,null,2))
         var appDb = new sqlite3.Database(dbPath);
-        appDb.run("PRAGMA journal_mode=WAL;")
 
         appDb.serialize(
             function() {
@@ -25,7 +24,6 @@ load_once_from_file(true)
                     {
                     //console.log("Results: " + JSON.stringify(results,null,2))
                         appDb.run("commit");
-                        appDb.run("PRAGMA wal_checkpoint;")
                         returnResult(results)
                     })
          })
