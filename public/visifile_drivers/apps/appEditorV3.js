79d61cab243b5b5a7eb11c102c43664520937b8a
Sherlock Holmes <zubairq@gmail.com>
2018-11-01 11:00:28 +0100

    trying to make the Ui scaffolding for the code profiler
    Liek the Algojammer profiler for Python

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -101,15 +101,57 @@ load_once_from_file(true)
 
                     </div>
 
-                <div>
-      </div>
-       `
+
+    </div>
+
+
+
+
+    <div v-if='mode == "profiler"'>
+        <div class='caption'  style='color: black;'>
+            Profiler
+        </div>
+
+        <div class='container'>
+
+            <div class='row'>
+                <div class='md-col4'>
+                    <textarea>
+                        This is some code
+                        line 2
+                    </textarea>
+                </div>
+
+
+                <div class='md-col7'>
+                    <div style='color: black; position: relative;'>
+
+                        <div    v-for='exePoint in execution_timeline'
+                                v-bind:style='" color: black;position: absolute;" +
+
+                                                "top:" + (exePoint.line * 10) + ";" +
+                                                "left:" + (exePoint.time * 5) +
+
+                                                " ;border: 1px solid black;width:5px;height: 5px; background-color: black;"'>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+     </div>
+
+
+</div>
+`
        ,
        data: function() {
            return {
                editor_loaded:       false,
                selected_app:        '',
                editor_component:    null,
+               execution_timeline:  null,
                app_loaded:          false,
                app_component_name:  null,
                base_component_id:   null,
@@ -441,6 +483,8 @@ load_once_from_file(true)
 
             mounted: async function () {
                 var mm = this
+
+                this.execution_timeline = executionTimeline
 
                 //
                 // make sure we load the component for this app
