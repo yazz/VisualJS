b82cc7c1d4615e44f01e65fd315ad43d889c3025
Sherlock Holmes <zubairq@gmail.com>
2018-11-21 17:01:01 +0100

    timeline debugger works again now
    Why should padding here make a difference though?

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -72,7 +72,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
 
                                    <div v-if="item.type == 'app'" >
                                        <div v-if="(edit_app == item.data.id)"
-                                               style="position: fixed; left:0px; top:0px; height:100%; width: 100vw ;z-index: 200000;background-color: white;overflow-y:scroll; padding: 0px;">
+                                               style="position: fixed; left:0px; top:0px; height:100%; width: 100vw ;z-index: 200000;background-color: white;overflow-y:scroll; padding: 20px;">
                                                <component v-if='' :is='"app_editor_3"' v-bind:app_id='item.data.id' v-bind:card_index='index'></component>
                                        </div>
 
