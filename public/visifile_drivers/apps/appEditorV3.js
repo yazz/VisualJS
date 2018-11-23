5769631226edc184a7d13f4e3d96af30fdff29bc
Sherlock Holmes <zubairq@gmail.com>
2018-11-23 14:31:14 +0100

    Don't show all the buttons in debug mode

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -38,12 +38,12 @@ load_once_from_file(true)
             <button  type=button class='btn btn-primary btn-sm'      v-on:click='chooseApp()'  >App</button>
             <button  type=button class=' btn btn-secondary btn-sm'   v-on:click='chooseCode()' >Code</button>
             <button  type=button class=' btn btn-info btn-sm'        v-on:click='chooseBoth()' >Both</button>
-            <button  type=button class=' btn btn-primary btn-sm'        v-on:click='chooseProfiler()' >Profiler</button>
-            <button  type=button class=' btn btn-secondary btn-sm'   v-on:click='copyAppMethod(base_component_id,null)' >Copy app</button>
-            <button  type=button class=' btn btn-info btn-sm'        v-on:click='embedApp(base_component_id)' >Embed app</button>
-            <button  v-if='(editor_component != "editor_component") && (!read_only)' type=button class=' btn btn-secondary btn-sm'   v-on:click='editAsText()' >Edit as text</button>
-            <button  v-if='(!read_only) && (visibility == "PUBLIC")' type=button class=' btn btn-success btn-sm'   v-on:click='setVisibility("PRIVATE")' >Public: Switch to private</button>
-            <button  v-if='(!read_only) && (visibility == "PRIVATE")' type=button class=' btn btn-secondary btn-sm'   v-on:click='setVisibility("PUBLIC")' >Private: Switch to public</button>
+            <button  v-if='(mode != "profiler")' type=button class=' btn btn-primary btn-sm'        v-on:click='chooseProfiler()' >Profiler</button>
+            <button  v-if='(mode != "profiler")' type=button class=' btn btn-secondary btn-sm'   v-on:click='copyAppMethod(base_component_id,null)' >Copy app</button>
+            <button  v-if='(mode != "profiler")' type=button class=' btn btn-info btn-sm'        v-on:click='embedApp(base_component_id)' >Embed app</button>
+            <button  v-if='(editor_component != "editor_component") && (!read_only) && (mode != "profiler")' type=button class=' btn btn-secondary btn-sm'   v-on:click='editAsText()' >Edit as text</button>
+            <button  v-if='(!read_only) && (visibility == "PUBLIC") && (mode != "profiler")' type=button class=' btn btn-success btn-sm'   v-on:click='setVisibility("PRIVATE")' >Public: Switch to private</button>
+            <button  v-if='(!read_only) && (visibility == "PRIVATE") && (mode != "profiler")' type=button class=' btn btn-secondary btn-sm'   v-on:click='setVisibility("PUBLIC")' >Private: Switch to public</button>
             <button  type=button class=' btn btn-danger btn-sm'   v-on:click='closeApp()' >Close</button>
         </div>
     </div>
