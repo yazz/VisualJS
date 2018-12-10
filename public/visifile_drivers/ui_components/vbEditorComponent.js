5808adacd0d3dd7b36c16a683eefc165b9e89eda
Sherlock Holmes <zubairq@gmail.com>
2018-12-10 10:50:03 +0100

    Trying to fix the editor colors

--- public/visifile_drivers/ui_components/vbEditorComponent.js
+++ public/visifile_drivers/ui_components/vbEditorComponent.js
@@ -277,7 +277,7 @@ load_once_from_file(true)
 
 
     <div    v-if='design_mode'
-          v-bind:style='(design_mode?"border: 0px solid black;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);":"") + " position:absolute;top:0px;right:0px;width: 250px;height: 75vmin; display: inline-block;overflow-x: none;overflow-y: auto;vertical-align: top;padding:0px;height:100%; "'
+          v-bind:style='(design_mode?"border: 4px solid lightgray;box-shadow: 2px 2px 10px lightgray;":"") + " position:absolute;top:0px;right:0px;width: 250px;height: 75vmin; display: inline-block;overflow-x: none;overflow-y: auto;vertical-align: top;padding:0px;height:100%; "'
           v-bind:refresh='refresh'>
 
         <div    v-bind:refresh='refresh'
