6c0e7d7cf2681c0c1da2d31ae5c34ad63664bd12
Sherlock Holmes <zubairq@gmail.com>
2018-12-15 06:54:52 +0100

    Allow all the old app types to be created (at the bottom)

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -129,6 +129,22 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
 
     </div>
 
+    <div style='margin-top:1000px; text-align: center;'>
+        Surprises :)
+
+        <button style='margin-bottom:10px;' class='btn btn-primary btn-sm' v-on:click='copyAndEditApp($event,"game")'>
+            Make 3d app
+        </button>
+        <button style='margin-bottom:10px;' class='btn btn-primary btn-sm' v-on:click='copyAndEditApp($event,"todo")'>
+            todo
+        </button>
+        <button style='margin-bottom:10px;' class='btn btn-primary btn-sm' v-on:click='copyAndEditApp($event,"new")'>
+            Hello World
+        </button>
+        <button style='margin-bottom:10px;' class='btn btn-primary btn-sm' v-on:click='copyAndEditApp($event,"bubblesort")'>
+            Bubble Sort
+        </button>
+    </div>
 </div>`
       ,
 
