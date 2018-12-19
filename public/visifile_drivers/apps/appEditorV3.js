16f8668782bd8441610c8a56100d05f799c8d3eb
Sherlock Holmes <zubairq@gmail.com>
2018-12-19 12:23:52 +0100

    ...

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -73,16 +73,16 @@ load_once_from_file(true)
 
 
              <div v-if='extra_menu' class='btn-group' role=group style='box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-right: 20px;'>
-                 <button  v-if='(mode != "profiler")' type=button class=' btn btn-info btn-sm'   v-on:click='extra_menu=false;copyAppMethod(base_component_id,null)' >Copy</button>
-                 <button  v-if='(mode != "profiler")' type=button class=' btn btn-info btn-sm'        v-on:click='extra_menu=false;embedApp(base_component_id)' >Embed</button>
-                 <button  v-if='(editor_component != "editor_component") && (!read_only) && (mode != "profiler")' type=button class=' btn btn-info btn-sm'   v-on:click='extra_menu=false;editAsText()' >Edit as text</button>
+                 <button  v-if='(mode != "profiler")' type=button class=' btn btn-info btn-sm'   v-on:click='copyAppMethod(base_component_id,null)' >Copy</button>
+                 <button  v-if='(mode != "profiler")' type=button class=' btn btn-info btn-sm'        v-on:click='embedApp(base_component_id)' >Embed</button>
+                 <button  v-if='(editor_component != "editor_component") && (!read_only) && (mode != "profiler")' type=button class=' btn btn-info btn-sm'   v-on:click='editAsText()' >Edit as text</button>
                  <button vbind:style='"box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);height: 25px;"'
-                         v-if='(!read_only) && (visibility == "PUBLIC") && (mode != "profiler")' type=button class='btn btn-danger btn-sm'   v-on:click='extra_menu=false;setVisibility("PRIVATE")' >
+                         v-if='(!read_only) && (visibility == "PUBLIC") && (mode != "profiler")' type=button class='btn btn-danger btn-sm'   v-on:click='setVisibility("PRIVATE")' >
                      Set to private
                  </button>
 
                  <button vbind:style='"box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);height: 25px;"'
-                         v-if='(!read_only) && (visibility == "PRIVATE") && (mode != "profiler")' type=button class='btn btn-info btn-sm'   v-on:click='extra_menu=false;setVisibility("PUBLIC")' >
+                         v-if='(!read_only) && (visibility == "PRIVATE") && (mode != "profiler")' type=button class='btn btn-info btn-sm'   v-on:click='setVisibility("PUBLIC")' >
                      Set to public
                  </button>
              </div>
