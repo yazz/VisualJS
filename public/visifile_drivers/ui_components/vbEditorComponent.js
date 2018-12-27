0ef01da2badef03f9f072c1bf524532fe314f70c
Sherlock Holmes <zubairq@gmail.com>
2018-12-27 06:40:58 +0100

    ...

--- public/visifile_drivers/ui_components/vbEditorComponent.js
+++ public/visifile_drivers/ui_components/vbEditorComponent.js
@@ -338,6 +338,9 @@ load_once_from_file(true)
                 </div>
             </div>
 
+
+
+
             <!--
 
                     The drag drop UI editor.
@@ -347,8 +350,9 @@ load_once_from_file(true)
                     Also the main vire of the App
 
             -->
+
             <div    v-if='(!design_mode) || (design_mode && (design_mode_pane.type=="drag_drop"))'
-                    v-bind:style='"margin: 2px; display: inline-block; vertical-align: top;  width: " + model.forms[model.active_form].width +  ";height: " + model.forms[model.active_form].height +  " ;" + (design_mode?"border: 0px solid lightgray; padding:0px;margin: 15px;":"margin: 0px;" ) '>
+                    v-bind:style='"margin: 2px; display: inline-block; vertical-align: top;  width: " + model.forms[model.active_form].width +  ";height: " + model.forms[model.active_form].height +  " ;" + (design_mode?"border: 0px solid lightgray; padding:0px;margin: 0px;margin-left:15px;margin-top:15px;":"margin: 0px;" ) '>
 
                 <div    v-if='design_mode'
                         style='display:inline-block;font-size:14px;font-weight:bold;border-radius: 10px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);background-image: linear-gradient(to right,  #000099, lightblue); color: white; border: 4px solid lightgray; padding:4px; margin:0;border-bottom: 0px;width:100%;'>
