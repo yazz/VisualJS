d7e936dd7ccf6ba9ab6c0e960efab6bc2102c1e1
Sherlock Holmes <zubairq@gmail.com>
2019-01-04 19:25:57 +0100

    Made the group control show a black border

--- public/visifile_drivers/controls/group.js
+++ public/visifile_drivers/controls/group.js
@@ -2,9 +2,9 @@ function(args) {
 /*
 is_app(true)
 control_type("VB")
-display_name("Label control")
+display_name("Group control")
 description("This will return the label control")
-base_component_id("label_control")
+base_component_id("group_control")
 load_once_from_file(true)
 visibility("PRIVATE")
 read_only(true)
@@ -21,15 +21,23 @@ properties(
             name:   "Background color",
             type:   "String"
         }
+        ,
+        {
+            id:         "is_container",
+            name:       "Is Container?",
+            type:       "Boolean",
+            default:    true,
+            hidden:     true
+        }
     ]
 )//properties
 logo_url("/driver_icons/group_control.png")
 */
 
-    Vue.component("label_control",{
+    Vue.component("group_control",{
       props: ["args"]
       ,
-      template: `<div v-bind:style='"height:100%;width:100%; border: 0px;" +
+      template: `<div v-bind:style='"height:100%;width:100%; border: 1px solid gray;color: black;" +
                                     "background-color: "+    args["background_color"]  +  ";"'>
 
                                                 {{args.text}}
