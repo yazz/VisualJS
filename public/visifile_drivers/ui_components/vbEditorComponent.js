407612b6220f9d2bdbc573a23c5673cd0fe29dea
Sherlock Holmes <zubairq@gmail.com>
2019-01-08 06:51:58 +0100

    Added a control details editor

--- public/visifile_drivers/ui_components/vbEditorComponent.js
+++ public/visifile_drivers/ui_components/vbEditorComponent.js
@@ -139,6 +139,37 @@ uses_javascript_librararies(["aframe"])
 
             <!--
 
+                    The control details editor
+
+            -->
+
+
+            <div    v-if='(!design_mode && design_mode_pane) || (design_mode && (design_mode_pane.type=="control_details_editor"))'
+                    v-bind:style='"margin: 2px; display: inline-block; vertical-align: top; width: 100%;height: 65vh ;" + (design_mode?"border: 0px solid lightgray; padding:0px;margin: 15px;":"margin: 0px;" ) '>
+
+                <div    v-if='design_mode'
+                        style='font-family:verdana;font-size: 13px;font-weight:bold;border-radius: 10px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);background-image: linear-gradient(to right,  #000099, lightblue); color: white; border: 4px solid lightgray; padding:4px; margin:0;border-bottom: 0px;'>
+
+                    <div    style='height: 30px;' >
+                        Control details
+                        <button  type=button class=' btn btn-danger btn-sm'
+                                 style="float: right;box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;margin-bottom: 4px;"
+                                 v-on:click='closeUiCodeEditor()' >x</button>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+            <!--
+
                     The drag drop UI editor.
 
                     but...
@@ -1458,7 +1489,13 @@ ${eventMessage.code}
 
         showComponentDetailedDesignUi: async function(index) {
            var mm = this
-           //this.selectForm(this.model.active_form)
+           mm.design_mode_pane =
+           {
+               type: "control_details_editor",
+               active_form:            mm.model.active_form,
+               active_component_index: mm.model.active_component_index,
+           }
+
            setTimeout(function() {
                mm.refresh ++
                mm.$forceUpdate();
