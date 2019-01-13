4256aa5024346361fc53803b0d9fecd48f47725a
Sherlock Holmes <zubairq@gmail.com>
2019-01-13 05:54:18 +0100

    can position the eyes and ears in 3d in pose estimator

--- public/visifile_drivers/apps/kinetic.js
+++ public/visifile_drivers/apps/kinetic.js
@@ -1,9 +1,9 @@
 async function kinetic_app(args) {
 /*
-created_timestamp(1547352694251)
+created_timestamp(1547355211458)
 base_component_id("kinetic")
 visibility("PUBLIC")
-display_name("Kinetic app")
+display_name("Copy of Copy of Copy of Kinetic app")
 is_app(true)
 description('Kinetic app')
 uses_javascript_librararies(["advanced_bundle"])
@@ -41,15 +41,23 @@ logo_url("/man.jpg")
             style="display: inline-block;width:45%;vertical-align:top;">
            <a-scene physics-world="" physics="debug: true" style='width: 80%; height: 80%;' embedded>
 
-               <a-box    id="interact_box_three"
-                         position="-1 3.5 -3"
-                         body="shape: box; mass: 2"
-                         rotation="0 45 0"
-                         color="#4CC3D9">
-               </a-box>
+            <a-entity position="0 1.25 -5" radius="1.25" color="#EF2D5E">
+               <a-sphere v-bind:position='" " + nose_x + " " + nose_y + " -5"'
+                         radius=".1" color="#EF2D5E"></a-sphere>
 
-               <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-               <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+               <a-sphere v-bind:position='" " + left_eye_x + " " + left_eye_y + " -5"'
+                         radius=".1" color="blue"></a-sphere>
+               <a-sphere v-bind:position='" " + right_eye_x + " " + right_eye_y + " -5"'
+                         radius=".1" color="blue"></a-sphere>
+
+               <a-sphere v-bind:position='" " + left_ear_x + " " + left_ear_y + " -5"'
+                         radius=".1" color="blue"></a-sphere>
+               <a-sphere v-bind:position='" " + right_ear_x + " " + right_ear_y + " -5"'
+                         radius=".1" color="blue"></a-sphere>
+            </a-entity>
+
+
+
 
                <a-plane  static-body
                          position="0 0 -4"
@@ -77,11 +85,50 @@ logo_url("/man.jpg")
       ,
       methods: {
           evt: async function()  {
-                alert("Started")
-                var aqq = document.getElementById('videoElement');
+                //alert("Started")
+                var aqq = document.getElementById('man22');
                 var net = await posenet.load();
                 var  pose2 = await net.estimateSinglePose(aqq,0.5,false,16)
                 alert(JSON.stringify(pose2,null,2))
+                var keypoints = pose2.keypoints
+                for (var rrr = 0; rrr < keypoints.length; rrr++  ) {
+                    var point = keypoints[rrr]
+                    if (point.part=="nose") {
+                        this.nose_x = (point.position.x / 20) - 5
+                        this.nose_y = 3 + (point.position.y / 100)
+                    }
+                    if (point.part=="leftEye") {
+                        this.left_eye_x = (point.position.x / 20) - 5
+                        this.left_eye_y = (point.position.y / 60) + 2
+                    }
+                    if (point.part=="rightEye") {
+                        this.right_eye_x = (point.position.x / 20) - 5
+                        this.right_eye_y = (point.position.y / 60) + 2
+                    }
+                    if (point.part=="leftEar") {
+                        this.left_ear_x = (point.position.x / 20) - 5
+                        this.left_ear_y = (point.position.y / 60) + 2
+                    }
+                    if (point.part=="rightEar") {
+                        this.right_ear_x = (point.position.x / 20) - 5
+                        this.right_ear_y = (point.position.y / 60) + 2
+                    }
+                }
+
+            }
+      },
+      data: function() {
+          return {
+              nose_x: 0,
+              nose_y: 0,
+              left_eye_x: 0,
+              left_eye_y: 0,
+              right_eye_x: 0,
+              right_eye_y: 0,
+              left_ear_x: 0,
+              left_ear_y: 0,
+              right_ear_x: 0,
+              right_ear_y: 0
           }
       }
 
