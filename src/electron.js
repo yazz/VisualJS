866f5c95a6cefe8b63d97f3aa67530a3cfdb45cc
Sherlock Holmes <zubairq@gmail.com>
2019-01-15 21:47:47 +0100

    redirect all http requests to https

--- src/electron.js
+++ src/electron.js
@@ -2047,6 +2047,17 @@ function add_new_queryFn(req, res) {
 // This starts all the system services
 //------------------------------------------------------------
 function startServices() {
+    if (useHttps) {
+        app.use (function (req, res, next) {
+                if (req.secure) {
+                        // request was via https, so do no special handling
+                        next();
+                } else {
+                        // request was via http, so redirect to https
+                        res.redirect('https://' + req.headers.host + req.url);
+                }
+        });    
+    }
     app.use(cors({ origin: '*' }));
     app.use(function (req, res, next) {
 
