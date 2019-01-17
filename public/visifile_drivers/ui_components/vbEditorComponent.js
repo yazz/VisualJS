703bef5c70792e4ada28c294f7c2d40f40e311cc
Sherlock Holmes <zubairq@gmail.com>
2019-01-17 09:52:29 +0100

    Can resize the bottom right of grouped elements

--- public/visifile_drivers/ui_components/vbEditorComponent.js
+++ public/visifile_drivers/ui_components/vbEditorComponent.js
@@ -1963,8 +1963,8 @@ ${eventMessage.code}
                  if (!this.model.forms[this.model.active_form].components[data.index].is_container) {
                      if (parentId) {
                         this.model.forms[this.model.active_form].components[data.index].parent = parentName
-                        newLeftX = newLeftX - parentOffsetX
-                        newTopY = newTopY - parentOffsetY
+                        newX = newX - parentOffsetX
+                        newY = newY - parentOffsetY
                      } else {
                         this.model.forms[this.model.active_form].components[data.index].parent = null
                      }
