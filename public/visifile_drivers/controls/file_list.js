3e368a4f972b45335a3afef0556f684777d28489
Sherlock Holmes <zubairq@gmail.com>
2019-01-21 16:33:21 +0100

    Created dummy folder and file list icons

---
+++ public/visifile_drivers/controls/file_list.js
@@ -0,0 +1,44 @@
+function(args) {
+/*
+is_app(true)
+control_type("VB")
+display_name("File list control")
+description("This will return the file list control")
+base_component_id("file_list_control")
+load_once_from_file(true)
+visibility("PRIVATE")
+read_only(true)
+properties(
+    [
+        {
+            id:     "text",
+            name:   "Text",
+            type:   "String"
+        }
+        ,
+        {
+            id:     "background_color",
+            name:   "Background color",
+            type:   "String"
+        }
+    ]
+)//properties
+logo_url("/driver_icons/file_list.png")
+*/
+
+    Vue.component("file_list_control",{
+      props: ["args"]
+      ,
+      template: `<div v-bind:style='"height:100%;width:100%; border: 0px;" +
+                                    "background-color: "+    args["background_color"]  +  ";"'>
+
+                                                {{args.text}}
+                 </div>`
+      ,
+      data: function() {
+       return {
+         msg: "..."
+     }
+      },
+    })
+}
--- src/child.js
+++ src/child.js
@@ -832,6 +832,8 @@ async function setUpComponentsLocally() {
     await evalLocalSystemDriver('timer_control',   path.join(__dirname, '../public/visifile_drivers/controls/timer.js'))
     await evalLocalSystemDriver('drive_list_control',   path.join(__dirname, '../public/visifile_drivers/controls/drive_list.js'))
 
+    await evalLocalSystemDriver('folder_list_control',   path.join(__dirname, '../public/visifile_drivers/controls/folder_list.js'))
+    await evalLocalSystemDriver('file_list_control',   path.join(__dirname, '../public/visifile_drivers/controls/file_list.js'))
 
     await evalLocalSystemDriver('threedee_control',        path.join(__dirname, '../public/visifile_drivers/controls/threedee.js'))
     await evalLocalSystemDriver('threedee_item_control',   path.join(__dirname, '../public/visifile_drivers/controls/threedee_item.js'))
