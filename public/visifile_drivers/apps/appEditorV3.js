9e541fb4a626f675aeb2e3ff5429b5fd500acbc7
Sherlock Holmes <zubairq@gmail.com>
2019-02-07 04:55:04 +0100

    Open app in new window from editor

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -223,7 +223,7 @@ load_once_from_file(true)
 
 
 
-            <div    v-on:click='window.open(location.protocol + "//" + location.hostname + ":" + location.port + "/app/" + base_component_id + ".html")'
+            <div    v-on:click='var win = window.open(location.protocol + "//" + location.hostname + ":" + location.port + "/app/" + base_component_id + ".html", "_blank"); win.focus();'
                     v-bind:style="'text-decoration: underline;color:blue;padding: 5px; margin-top: 3px; position: relative; border: 0px;border-bottom: 4px solid lightsteelblue;'">
 
                 Shareable link:<input   readonly
