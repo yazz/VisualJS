6f3dc958bffb1b408bf453adc09e721edcb902d8
Sherlock Holmes <zubairq@gmail.com>
2019-02-07 19:14:10 +0100

    added tabulator table

--- public/visifile_drivers/controls/table.js
+++ public/visifile_drivers/controls/table.js
@@ -56,6 +56,8 @@ logo_url("/driver_icons/table.png")
 `<div   v-bind:style='"width:100%;overflow-y:auto;height:100%"
         v-bind:refresh='refresh'>
 
+        <div ref="exampletable"></div>
+
     <div v-bind:style='"height:100%;width:100%; border: 0px;color:black;"'
          v-if='design_mode == "detail_editor"'>
 
@@ -164,6 +166,31 @@ logo_url("/driver_icons/table.png")
                 this.value = this.args.value
              }
          }
+
+         var table = new Tabulator(this.$refs.exampletable, {
+            	data: [],           //load row data from array
+            	layout:"fitColumns",      //fit columns to width of table
+            	responsiveLayout:"hide",  //hide columns that dont fit on the table
+            	tooltips:true,            //show tool tips on cells
+            	addRowPos:"top",          //when adding a new row, add it to the top of the table
+            	history:true,             //allow undo and redo actions on the table
+            	pagination:"local",       //paginate the data
+            	paginationSize:7,         //allow 7 rows per page of data
+            	movableColumns:true,      //allow column order to be changed
+            	resizableRows:true,       //allow row order to be changed
+            	initialSort:[             //set the initial sort order of the data
+            		{column:"name", dir:"asc"},
+            	],
+            	columns:[                 //define the table columns
+            		{title:"Name", field:"name", editor:"input"},
+            		{title:"Task Progress", field:"progress", align:"left", formatter:"progress", editor:true},
+            		{title:"Gender", field:"gender", width:95, editor:"select", editorParams:{"Male":"male", "Female":"female"}},
+            		{title:"Rating", field:"rating", formatter:"star", align:"center", width:100, editor:true},
+            		{title:"Color", field:"col", width:130, editor:"input"},
+            		{title:"Date Of Birth", field:"dob", width:130, sorter:"date", align:"center"},
+            		{title:"Driver", field:"car", width:90,  align:"center", formatter:"tickCross", sorter:"boolean", editor:true},
+            	],
+            });
 
 
       }
