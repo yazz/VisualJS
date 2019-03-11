b3494b8e3be855f833cc2e735bd1927ec27c83f9
Sherlock Holmes <zubairq@gmail.com>
2019-03-11 08:21:32 +0100

    Try to check for null values

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -1076,11 +1076,14 @@ load_once_from_file(true)
                                         ,{
                                               base_component_id:    appId,
                                               new_app_id:           newAppId   })
-                mm.$root.$emit('message', {
-                                                type:               "insert_app_at",
-                                                base_component_id:   result.value.base_component_id,
-                                                card_index:          1
-                                            })
+                if (isValidObject(result)) {
+                    mm.$root.$emit('message', {
+                                                    type:               "insert_app_at",
+                                                    base_component_id:   result.value.base_component_id,
+                                                    card_index:          1
+                                                })
+
+                }
                 setTimeout(async function() {
                     mm.console_output = ""
                     await mm.load_new_app( result.value.base_component_id )
@@ -1149,11 +1152,6 @@ load_once_from_file(true)
                dev_app_component_loaded = new Object()
                this.editor_loaded = false
                await this.load_app(baseComponentId)
-               mm.$root.$emit('message', {
-                                               type:               "insert_app_at",
-                                               base_component_id:   result.value.base_component_id,
-                                               card_index:          1
-                                           })
            }
            ,
 
