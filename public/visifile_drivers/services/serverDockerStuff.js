d7f2da8bded2c119df8fcb23d27096fb012bb0e5
Sherlock Holmes <zubairq@gmail.com>
2019-03-14 11:50:52 +0100

    Added docs

--- public/visifile_drivers/services/serverDockerStuff.js
+++ public/visifile_drivers/services/serverDockerStuff.js
@@ -5,7 +5,15 @@ base_component_id("server_docker_stuff")
 load_once_from_file(true)
 only_run_on_server(true)
 */
-
+//
+// On Mac Docker doesn't seem to expose itself via a port by default, so we did
+// this:
+// docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 127.0.0.1:1234:1234 bobrik/socat TCP-LISTEN:1234,fork UNIX-CONNECT:/var/run/docker.sock
+// export DOCKER_HOST=tcp://localhost:1234
+//
+// Based on this URL:
+// https://github.com/docker/for-mac/issues/770
+//
 var Docker = require('dockerode')
 console.log("//var docker = require('dockerode')")
     var promise = new Promise(async function(returnfn) {
