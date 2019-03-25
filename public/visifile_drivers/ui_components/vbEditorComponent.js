7540e10a628ded30262508d7981027e918f6ce24
Sherlock Holmes <zubairq@gmail.com>
2019-03-25 20:09:42 +0100

    Control methods now return a value

--- public/visifile_drivers/ui_components/vbEditorComponent.js
+++ public/visifile_drivers/ui_components/vbEditorComponent.js
@@ -1063,8 +1063,8 @@ uses_javascript_librararies(["advanced_bundle"])
                 //alert(JSON.stringify(componentDetails.name,null,2))
                 var controlDetails = globalControl[componentDetails.name]
                 var fnDetails = controlDetails[methodId]
-                await fnDetails(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10)
-
+                var retv = await fnDetails(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10)
+                return retv
             }
 
          }
