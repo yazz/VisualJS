50217635e37e1a7c26eabf9adeb7142480dac830
Sherlock Holmes <zubairq@gmail.com>
2019-03-25 13:18:55 +0100

    connect to postgres

--- public/visifile_drivers/services/postgres_server.js
+++ public/visifile_drivers/services/postgres_server.js
@@ -6,6 +6,20 @@ load_once_from_file(true)
 only_run_on_server(true)
 */
 
+    var config = {
+      user:              "postgres",
+      database:          "postgres",
+      password:          "mysecretpassword",  // this password is only used for testing so no security problem here
+      host:              "localhost",
+      port:               5432
+    };
+
+    var dbconnection = new postgresdb.Client(config);
+    dbconnection.connect(function (err) {
+      if (err) {
+          console.log({error: '' + err});
+      }
+    });
 
     return {data: "postres returned"}
 }
