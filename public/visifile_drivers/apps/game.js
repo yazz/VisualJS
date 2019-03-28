4f64179b8b62d1f1d94618672240849b2e5dd1a5
Sherlock Holmes <zubairq@gmail.com>
2019-03-28 16:24:19 +0100

    Can levitate different objects with Oculus GO

--- public/visifile_drivers/apps/game.js
+++ public/visifile_drivers/apps/game.js
@@ -25,13 +25,17 @@ read_only(true)
                             line="opacity:1.0;">
                 </a-entity>
                 <a-box    id="interact_box_three"
-                          position="-1 3.5 -3"
+                          position="-1 1 -3"
                           body="shape: box; mass: 2"
                           rotation="0 45 0"
                           color="#4CC3D9">
                 </a-box>
 
-                <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+                <a-sphere body="shape: box; mass: 2"
+                          position="0 1.25 -5" radius="1.25" color="#EF2D5E">
+                </a-sphere>
+
+
                 <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D">
                     <a-entity position="2 2 1"  v-bind:text='"width:10;value: " + msg + ";color:black;"'>
                     </a-entity>
@@ -55,15 +59,23 @@ read_only(true)
       mounted: function() {
           var mm = this
           var scene = document.querySelector('a-scene');
-          scene.addEventListener('click', function () {
-              var box = document.getElementById('interact_box_three');
-              var impulse = { x: 0, y: 10, z: 0 };
-              var point = { x: 0.5, y: 0, z: 0 };
-              box['body'].applyImpulse(impulse, point);
-          });
+
+          var box = document.getElementById('interact_box_three');
+      box.addEventListener('click', function () {
+          var impulse = { x: 0, y: 5, z: 0 };
+          var point = { x: 0.5, y: 0, z: 0 };
+          box['body'].applyImpulse(impulse, point);
+      });
+
+        var sphere = document.querySelector('a-sphere');
+      sphere.addEventListener('click', function () {
+          var impulse = { x: 0, y: 5, z: 0 };
+          var point = { x: 0.5, y: 0, z: 0 };
+          sphere['body'].applyImpulse(impulse, point);
+      });
 
 
-          
+
           var laser = document.querySelector('#laser');
           laser.addEventListener('trackpaddown', function (ee) {
                 mm.msg = "trackpaddown"
