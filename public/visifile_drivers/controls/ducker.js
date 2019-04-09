2583435dd20e6c806e313309a0587b41317bf2d8
Sherlock Holmes <zubairq@gmail.com>
2019-04-09 12:47:29 +0200

    refactoring

--- public/visifile_drivers/controls/ducker.js
+++ public/visifile_drivers/controls/ducker.js
@@ -64,6 +64,14 @@ properties(
             default:    true,
             hidden:     true
         }
+        ,
+        {
+            id:         "getFilteredContainerList",
+            pre_snippet:    `await `,
+            snippet:    `getFilteredContainerList()`,
+            name:       "Get Container List",
+            type:       "Action"
+        }
     ]
 )//properties
 children([
@@ -84,51 +92,94 @@ logo_url("/driver_icons/ducker.png")
 */
 
     Vue.component("docker_control",{
-      props: ["meta", "args","design_mode","refresh", "children"]
-      ,
-      template: `<div v-bind:style='"height:100%;width:100%; border: 0px;" +
-                                    "background-color: "+    args["background_color"]  +  ";"'>
 
-                                    <div v-if="design_mode && (children.length == 0)">
-                                        {{args.text}}
-                                     </div>
+        props: ["meta", "args","design_mode","refresh", "children"]
 
-                                     <div v-bind:style='"position:relative;width:100%;height:100%;border: 0px solid gray;background-color: "+    args["background_color"]  +  ";"'>
-                                         <div style="position:absolute;top:0px">
-                                             <slot v-bind:refresh='refresh'>
-                                             </slot>
-                                         </div>
-                                     </div>
-                 </div>`
-      ,
-      data: function() {
-       return {
-         msg: "..."
-     }
-      },
-
-      mounted: async function() {
-        registerComponent(this)
-
-        if (!this.design_mode) {
-            var result = await callFunction(
-                                {
-                                    driver_name: "serverDockerStuff",
-                                    method_name: "serverDockerStuff"  }
-                                    ,{
-                                        host: this.args.host ,
-                                        port: this.args.port
-                                     })
-
-           //alert(JSON.stringify(result.value,null,2))
-           if (result.value) {
-                this.args.container_list = result.value
-
-           }
+        ,
 
 
-           }
-       }
+
+        template:
+
+`<div v-bind:style='"height:100%;width:100%; border: 0px;" +
+    "background-color: "+    args["background_color"]  +  ";"'>
+
+    <div v-if="design_mode && (children.length == 0)">
+        {{args.text}}
+    </div>
+
+    <div v-bind:style='"position:relative;width:100%;height:100%;border: 0px solid gray;background-color: "+    args["background_color"]  +  ";"'>
+        <div style="position:absolute;top:0px">
+            <slot v-bind:refresh='refresh'>
+            </slot>
+        </div>
+    </div>
+</div>`
+
+        ,
+
+        data: function() {
+            return {
+                msg: "..."
+            }
+        }
+
+        ,
+
+        mounted: async function() {
+            registerComponent(this)
+
+            if (!this.design_mode) {
+                var x = await this.readFromDocker()
+                this.args.container_list = x
+            }
+        }
+        ,
+
+
+        methods: {
+            readFromDocker: async function() {
+                var result = await callFunction(
+                {
+                    driver_name: "serverDockerStuff",
+                    method_name: "serverDockerStuff"
+                }
+                ,
+                {
+                    host: this.args.host ,
+                    port: this.args.port
+                })
+
+                //alert(JSON.stringify(result.value,null,2))
+                if (result.value) {
+                    return result.value
+                }
+                return null
+            }
+            ,
+
+
+            getFilteredContainerList: async function() {
+                var qwe = await this.readFromDocker()
+                var newList = []
+
+                for (var aa = 0; aa < qwe.length; aa ++) {
+
+                    var newObject = {
+                                        image:          qwe[aa].Image,
+                                        privatePort:    qwe[aa].Ports[0].PrivatePort,
+                                        publicPort:     qwe[aa].Ports[0].PublicPort,
+                                        state:          qwe[aa].State,
+                                        status:         qwe[aa].Status
+                                    }
+                    newList.push(newObject)
+                }
+                return newList
+            }
+
+        }
+
+
 
 
     })
