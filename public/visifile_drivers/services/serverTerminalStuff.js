abd1d48c7fe40bc7b92044851e33f23041eb32c9
Sherlock Holmes <zubairq@gmail.com>
2019-04-30 14:56:02 +0200

    server control executes "ls"

--- public/visifile_drivers/services/serverTerminalStuff.js
+++ public/visifile_drivers/services/serverTerminalStuff.js
@@ -5,5 +5,17 @@ base_component_id("server_terminal_stuff")
 load_once_from_file(true)
 only_run_on_server(true)
 */
-    return {value: "server returned temrinal results"}
+    var exec = require('child_process').exec;
+
+    var execPromise = new Promise(
+                        done => {
+                                    exec('ls', function(error, stdout, stderr)
+                                    {
+                                        done(stdout)
+                                    })
+                                })
+
+    var val = await execPromise
+    
+    return {value: val}
 }
