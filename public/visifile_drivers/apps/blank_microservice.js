8936894fc15e1b4a1f723969a8efbb4716e94057
Zubair <zubairq@gmail.com>
2019-06-10 17:23:26 +0300

    Show the URL params in a microservice

--- public/visifile_drivers/apps/blank_microservice.js
+++ public/visifile_drivers/apps/blank_microservice.js
@@ -1,4 +1,4 @@
-function new_microservice() {
+function new_microservice(args) {
 /*
 base_component_id("new_microservice")
 display_name("New microservice")
@@ -8,7 +8,6 @@ rest_api("test2")
 logo_url("/driver_icons/microservice.png")
 */
     console.log("Hello World")
-    return {
-        ok: "we return this",
-    }
+    return args
+    
 }
