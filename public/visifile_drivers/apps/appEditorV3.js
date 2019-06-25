50e0630e4c687a2b16a814a3682e1762c6edfaea
Zubair <zubairq@gmail.com>
2019-06-25 16:26:24 +0200

    Moved the "Copy app" button

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -98,10 +98,10 @@ load_once_from_file(true)
 
                 <button  v-if='(mode != "profiler")' type=button class=' btn btn-sm btn-warning'        v-on:click='if (!read_only) {edit_name=true;show_name=false;}' >Rename</button>
 
-
                 <button  v-if='(editor_component != "editor_component") && (!read_only) && (mode != "profiler")' type=button class=' btn btn-info btn-sm'   v-on:click='editAsText()' >Edit as text</button>
-            </div>
 
+                <button  v-if='(mode != "profiler") && (!editor_overloaded)' type=button class=' btn btn-sm btn-warning'        v-on:click='setTimeout(function(){copyAppMethod(base_component_id, null)},100)' >Copy app</button>
+            </div>
 
 
             <div v-if='!extra_menu' class='btn-group' role=group style='box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);margin-right: 20px;'>
@@ -223,32 +223,7 @@ load_once_from_file(true)
 
                     </button>
 
-                    <button   v-bind:style="'margin-left:20px;margin-right: 6px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);visibility: ' + (code_shown?'':'hidden') + ';' "
-                              v-on:click='setTimeout(function(){copyAppMethod(base_component_id, null)},100)'
-                              v-if="!editor_overloaded"
-                              v-on:mouseenter='setInfo("Make an editable copy of this app")'
-                              v-on:mouseleave='setInfo(null)'
-                              type="button" class="btn btn-light">
 
-                              <svg  version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                    width="35px"
-                              	    viewBox="0 0 210.107 210.107"
-                                    height="35px"
-                                    style="enable-background:new 0 0 210.107 210.107;margin-right:10px;"
-                                    xml:space="preserve">
-                              <g>
-                              	<path style="fill:#1D1D1B;" d="M168.506,0H80.235C67.413,0,56.981,10.432,56.981,23.254v2.854h-15.38
-                              		c-12.822,0-23.254,10.432-23.254,23.254v137.492c0,12.822,10.432,23.254,23.254,23.254h88.271
-                              		c12.822,0,23.253-10.432,23.253-23.254V184h15.38c12.822,0,23.254-10.432,23.254-23.254V23.254C191.76,10.432,181.328,0,168.506,0z
-                              		 M138.126,186.854c0,4.551-3.703,8.254-8.253,8.254H41.601c-4.551,0-8.254-3.703-8.254-8.254V49.361
-                              		c0-4.551,3.703-8.254,8.254-8.254h88.271c4.551,0,8.253,3.703,8.253,8.254V186.854z M176.76,160.746
-                              		c0,4.551-3.703,8.254-8.254,8.254h-15.38V49.361c0-12.822-10.432-23.254-23.253-23.254H71.981v-2.854
-                              		c0-4.551,3.703-8.254,8.254-8.254h88.271c4.551,0,8.254,3.703,8.254,8.254V160.746z"/>
-                              </svg>Copy app
-
-                    </button>
 
                     <button   v-bind:disabled='read_only?"":false'
                               v-bind:style="'margin-left:20px;margin-right: 6px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);visibility: ' + (code_shown?'':'hidden') + ';' + (read_only?'opacity:.3;':'')"
