fe95b4fe7e7fb5721bf4f338ad62a983a870b4fa
Zubair <zubairq@gmail.com>
2019-11-02 08:21:37 +0100

    Trying to get memory usage of all nodejs processes

--- src/electron.js
+++ src/electron.js
@@ -20,6 +20,7 @@ console.log('...');
 var ls = require('ls-sync');
 var rimraf = require("rimraf");
 
+var pidusage        = require("pidusage");
 var fs              = require('fs');
 var mkdirp          = require('mkdirp')
 var rmdir           = require('rmdir-sync');
@@ -532,6 +533,12 @@ function setUpChildListeners(processName, fileName, debugPort) {
         } else if (msg.message_type == "createdTablesInChild") {
             forkedProcesses["forked"].send({         message_type: "setUpSql" });
             forkedProcesses["forked"].send({         message_type: "greeting" , hello: 'world' });
+
+            if (showDebug) {
+                console.log("mainNodeProcessStarted: " + mainNodeProcessStarted)
+            } else {
+                console.log(".")
+            }
 
             if (!mainNodeProcessStarted) {
                 mainNodeProcessStarted = true
@@ -2980,3 +2987,46 @@ function findDriversWithMethodLike(methodName, callbackFn) {
                 })
     }, sqlite3.OPEN_READONLY)
 }
+
+
+function getChildMem(childProcessName,stats,totalmem) {
+    var memoryused = 0
+    if (stats) {
+        memoryused = stats.memory / 1024 / 1024;
+        totalmem += memoryused
+    }
+    console.log(`${childProcessName}: ${Math.round(memoryused * 100) / 100} MB`);
+    return totalmem
+}
+
+var inmemcalc = false
+setInterval(function(){
+    if (showDebug) {
+        if (!inmemcalc) {
+            inmemcalc = true
+            var totalmem = 0
+            const used = process.memoryUsage().heapUsed / 1024 / 1024;
+            totalmem += used
+            console.log(`Main: ${Math.round(used * 100) / 100} MB`);
+            var allForked = Object.keys(forkedProcesses)
+            var returnedmemCount = 0
+            for (var ttt=0; ttt< allForked.length; ttt++) {
+                var childProcessName = allForked[ttt]
+                const childprocess = forkedProcesses[childProcessName]
+
+                pidusage(childprocess.pid, function (err, stats) {
+                    totalmem = getChildMem(childProcessName,stats,totalmem)
+                    returnedmemCount ++
+                    if (returnedmemCount == allForked.length) {
+                        console.log("------------------------------------")
+                        console.log(" TOTAL MEM = " + totalmem + "MB")
+                        console.log("------------------------------------")
+                        inmemcalc = false
+                    }
+                });
+            }
+        }
+    } else {
+        process.stdout.write(".");
+    }
+},3000)
