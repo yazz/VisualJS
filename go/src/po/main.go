727d438b8facbe682ae5be3127ff5472e508619a
Zubair <zubairq@gmail.com>
2019-12-04 07:02:21 +0100

    Starting to parse command line arguments in "po"

--- go/src/po/main.go
+++ go/src/po/main.go
@@ -1,5 +1,27 @@
 package main
-import "fmt"
+
+import (
+    "fmt"
+    "os"
+)
+
 func main() {
+
+    programPath     := os.Args
+    argsWithoutProg := os.Args[1:]
+
+
     fmt.Println("Welcome to Yazz Pilot")
+
+    fmt.Println(programPath)
+    fmt.Println(argsWithoutProg)
+
+    var argsLength = len(argsWithoutProg)
+    fmt.Println("Arg count:", argsLength )
+    if (argsLength == 0) {
+        fmt.Println("Welcome to the Yazz Pilot command line")
+    }
+    if (argsLength >= 1) {
+        fmt.Println("1 arg")
+    }
 }
