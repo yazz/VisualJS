820eb9bf6e4fd09572fa108c55909fa9b9536b07
Zubair <zubairq@gmail.com>
2019-12-23 12:29:03 +0100

    JSON traverse service returns list of paths now

--- public/visifile_drivers/services/json_traverse_service.js
+++ public/visifile_drivers/services/json_traverse_service.js
@@ -6,9 +6,31 @@ load_once_from_file(true)
 only_run_on_server(true)
 */
 
+ function pathToString(pp) {
+    var s = ""
+    for (  var aa = 0  ;  aa < pp.length  ;  aa ++  ) {
+        if (isNaN(parseInt(pp[aa]))) {
+            s += pp[aa]
+        } else {
+            s += "[]"
+        }
+        if (aa < pp.length -1) {
+            s += "."
+        }
+    }
+    return s
+}
 
     console.log("** json_traverse_service **")
     //console.log(JSON.stringify(args,null,2))
+    //var tt = traverse(args.input).paths()
+    var paths=new Object()
+    var scrubbed = traverse(args.input).map(function (x) {
+        if (this.circular) this.remove()
+        var rt = pathToString(this.path)
+        paths[rt]=true
+    });
+
     console.log("** json_traverse_service **")
-    return args.input
+    return paths
 }
