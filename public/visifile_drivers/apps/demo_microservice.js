d83582f2146c205327f6370e376b499af898c3a2
Zubair <zubairq@gmail.com>
2020-04-20 13:31:07 +0200

    Added a Demo Microservice for use with the 3Scale Demo app

---
+++ public/visifile_drivers/apps/demo_microservice.js
@@ -0,0 +1,14 @@
+function demo_microservice(args) {
+/*
+base_component_id("demo_microservice")
+display_name("Demo microservice")
+only_run_on_server(true)
+visibility("PRIVATE")
+rest_api("test2")
+logo_url("/driver_icons/rest.png")
+*/
+    console.log("This is console.log output for a demo Microservice")
+    //return args
+    return {a: 1, b: 2, c: "Hello Pilot!"}
+
+}
