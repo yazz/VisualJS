f7021967265972aba8f760576fbec82125f9f4d9
Zubair <zubairq@gmail.com>
2020-04-22 10:42:28 +0200

    Added Echo microservice

---
+++ public/visifile_drivers/apps/echo_microservice.js
@@ -0,0 +1,14 @@
+function echo_microservice(args) {
+/*
+base_component_id("echo_microservice")
+display_name("Echo microservice")
+only_run_on_server(true)
+visibility("PUBLIC")
+rest_api("echo")
+logo_url("/driver_icons/rest.png")
+*/
+    console.log("This is console.log output for a Echo Microservice")
+    //return args
+    return args.params
+
+}
