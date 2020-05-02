45e51654f410b5645c6d767324b72aa1d873d8f5
Zubair <zubairq@gmail.com>
2020-05-02 19:48:49 +0200

    Managed to get first message to client

--- public/visifile_drivers/services/kafka_service.js
+++ public/visifile_drivers/services/kafka_service.js
@@ -7,13 +7,13 @@ only_run_on_server(true)
 */
     var promise = new Promise(async function(returnfn) {
         var kafkaConnection = new Kafka({
-            clientId: 'myapp2',
+            clientId: 'myapp23',
             brokers: ['localhost:9092','localhost:9092']
         })
         console.log(1)
         const producer = kafkaConnection.producer()
         console.log(2)
-        const consumer = kafkaConnection.consumer({ groupId: 'test-group2' })
+        const consumer = kafkaConnection.consumer({ groupId: 'test-group22' })
         console.log(3)
         await consumer.connect()
         console.log(4)
@@ -24,6 +24,7 @@ only_run_on_server(true)
         console.log(6)
         await consumer.run({
           eachMessage: async ({ topic, partition, message }) => {
+              consumer.pause([{ topic }])
             console.log({
               partition,
               offset: message.offset,
@@ -31,9 +32,10 @@ only_run_on_server(true)
           })
             dd.push(message.value.toString())
             console.log(8)
+            returnfn(dd)
           }
         })
-        returnfn(dd)
+
     })
     var ret = await promise
     return ret
