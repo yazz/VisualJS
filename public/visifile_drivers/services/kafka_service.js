e263d29419293bcfe85ce91402de873ede7018ff
Zubair <zubairq@gmail.com>
2020-05-03 09:21:37 +0200

    code clean up

--- public/visifile_drivers/services/kafka_service.js
+++ public/visifile_drivers/services/kafka_service.js
@@ -7,21 +7,15 @@ only_run_on_server(true)
 */
     var promise = new Promise(async function(returnfn) {
         var kafkaConnection = new Kafka({
-            clientId: 'myapp23',
+            clientId: 'myapp',
             brokers: ['localhost:9092','localhost:9092']
         })
-        console.log(1)
         const producer = kafkaConnection.producer()
-        console.log(2)
         const consumer = kafkaConnection.consumer({ groupId: uuidv1() })
-        console.log(3)
         await consumer.connect()
-        console.log(4)
         await consumer.subscribe({ topic: 'test', fromBeginning: true })
-        console.log(5)
 
         var dd=[]
-        console.log(6)
         await consumer.run({
           eachMessage: async ({ topic, partition, message }) => {
               consumer.pause([{ topic }])
@@ -31,7 +25,6 @@ only_run_on_server(true)
               value: message.value.toString(),
           })
             dd.push(message.value.toString())
-            console.log(8)
             returnfn(dd)
           }
         })
