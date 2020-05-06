a0f5c1921da0bc6f36cd0b1239962695cc725d78
Zubair <zubairq@gmail.com>
2020-05-06 16:12:21 +0200

    fixed docs

--- public/visifile_drivers/controls/kubernetes.js
+++ public/visifile_drivers/controls/kubernetes.js
@@ -88,11 +88,23 @@ properties(
                             From your command line, when logged in to your OpenShift/Kubernetes cluster:<br />
                             <div style="font-family: monospace; ">
                                 <br />
-                                TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.token}"|base64 --decode)
+                                TOKEN=$(kubectl get secret $(kubectl get serviceaccount deployer -o jsonpath='{.secrets[0].name}') -o jsonpath='{.data.token}' | base64 --decode )
+                                <br /><br />
+
+                                : or :
+                                <br /><br />
+
+                                TOKEN=$(kubectl get secret $(kubectl get serviceaccount deployer -o jsonpath='{.secrets[1].name}') -o jsonpath='{.data.token}' | base64 --decode )
 
                                 <br /><br />
                                 echo $TOKEN
+
                                 <br /><br />
+                                : and maybe :
+                                <br /><br />
+                                oc adm policy add-role-to-user edit -z deployer
+                                <br /><br />
+
                             </div>
                             If the result is empty then you need to ask your system administrator for rights to do this.  Paste the token into
                             the "K8s Authentication Token" field. Note that on OpenShift you can replace "kubectl" with the "oc" command.
