88d9b19e1b1b0e6e746b75fc630cdce4bd8fb144
Zubair <zubairq@gmail.com>
2020-05-09 07:59:08 +0200

    Trying to fix drivelist component
    The previous one doesn'work in Windows

--- public/visifile_drivers/services/serverDriveList.js
+++ public/visifile_drivers/services/serverDriveList.js
@@ -7,26 +7,27 @@ only_run_on_server(true)
 */
 
 
-    var njds = require('nodejs-disks');
-    var promise = new Promise(async function(returnfn) {
-
-        njds.drives(
-            function (err, drives) {
-                njds.drivesDetail(
-                    drives,
-                    function (err, data) {
-                        returnfn(data)
-
-
-
-
-                    }
-                );
+    var nodeDiskInfo = require('node-disk-info');
+    var disks = []
+    var returnVal = []
+    try {
+        disks = nodeDiskInfo.getDiskInfoSync();
+        //console.log(JSON.stringify(disks,null,2));
+        for (var qq=0; qq<disks.length;qq++) {
+            var disk = disks[qq]
+            if (disk) {
+                console.log(disk._blocks);
+                if (disk._blocks > 0) {
+                    console.log("Added "+ disk._mounted);
+                    returnVal.push(disk._mounted)
+                    //console.log("returnVal "+ returnVal);
+                }
             }
-        )
-    })
-    var ret = await promise
-
-
-    return ret
+        }
+    } catch (e) {
+        console.error(e);
+        return {error: e}
+    }
+    console.log("returnVal "+ returnVal);
+    return returnVal
 }
