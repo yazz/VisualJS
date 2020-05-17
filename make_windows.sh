9d200d76dbb7dafe62f713f8d413b779ff059207
Zubair Quraishi <zubairq@gmail.com>
2020-05-17 17:57:01 +0200

    Update make_windows.sh

--- make_windows.sh
+++ make_windows.sh
@@ -1,3 +1,3 @@
-nexe src/electron.js -r public\ -r src\ -r package.json -r node_sqlite3_win64.rename  -r node_sqlite3_macos64.rename  -o pilot.exe --build
+nexe src/electron.js -r public\ -r src\ -r package.json -r node_sqlite3_win64.rename  -r node_sqlite3_macos64.rename  -o pilot.exe # --build
 mv .\pilot ..\nexe\pilot
 chmod +x ..\nexe\pilot
