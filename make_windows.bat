4d2e356ec0f88ca61623694f89001e758f9c44fb
Zubair Quraishi <zubairq@gmail.com>
2020-05-18 13:12:31 +0200

    Update make_windows.bat

--- make_windows.bat
+++ make_windows.bat
@@ -1,3 +1,2 @@
-nexe src/electron.js -r public\ -r src\ -r package.json -r node_sqlite3_win64.rename  -r node_sqlite3_macos64.rename  -o pilot.exe 
-mv .\pilot.exe ..\nexe\pilot.exe
-chmod +x ..\nexe\pilot.exe
+nexe src/electron.js -r public\ -r src\ -r package.json -r node_sqlite3_win64.rename  -r node_sqlite3_macos64.rename  -o pilot.exe
+copy .\pilot.exe ..\nexe\pilot.exe /Y
