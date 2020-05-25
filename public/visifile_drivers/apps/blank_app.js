b17725752fe33a0a15a447a67f2c36b04f6e20c7
Zubair <zubairq@gmail.com>
2020-05-25 09:45:14 +0200

    Default JS apps built in front end now run in browser

--- public/visifile_drivers/apps/blank_app.js
+++ public/visifile_drivers/apps/blank_app.js
@@ -3,6 +3,7 @@ function() {
 base_component_id("new")
 display_name("New app")
 visibility("PRIVATE")
+only_run_on_frontend(true)
 logo_url("/driver_icons/terminal.png")
 */
     console.log("Hello World")
