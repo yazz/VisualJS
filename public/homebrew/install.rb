1606288659a5fb53265f5cfc46757c1f9feb3613
Zubair Quraishi <fquraish@redhat.com>
2020-07-06 10:35:58 +0200

    Trying to create a HomeBrew Installer

--- public/homebrew/install.rb
+++ public/homebrew/install.rb
@@ -1,6 +1,8 @@
-class Pilot < Formula
+class install < Formula
   desc "The flexible, powerful and beautiful programming language"
   homepage "https://yazz.com"
+  url "http://0.0.0.0/yazz.zip"
+  sha256 ""
 
   def install
   end
