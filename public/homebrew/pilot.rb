de38bdcffb648a0816145ad13c39b4897a58a482
Zubair Quraishi <fquraish@redhat.com>
2020-07-06 13:05:50 +0200

    Fixing HomeBrew installer

--- public/homebrew/pilot.rb
+++ public/homebrew/pilot.rb
@@ -1,10 +1,11 @@
-class install < Formula
+class Pilot < Formula
   desc "The flexible, powerful and beautiful programming language"
   homepage "https://yazz.com"
   url "http://0.0.0.0/yazz.zip"
   sha256 ""
 
   def install
+      system "./node", "src/electron.js"
   end
 
 
