8f9960ed70fc528c74889f3872df678a92ec6b94
Zubair Quraishi <fquraish@redhat.com>
2020-07-18 16:51:42 +0200

    Change the Binary make scripts to build AppShare instead of pilot

--- make_linux_fedora.sh
+++ make_linux_fedora.sh
@@ -1,3 +1,3 @@
-nexe src/electron.js -r public/ -r src/ -r package.json -r node_sqlite3_linux64.rename  -o pilot
-mv ./pilot ../nexe/pilot
-chmod +x ../nexe/pilot
+nexe src/electron.js -r public/ -r src/ -r package.json -r node_sqlite3_linux64.rename  -o appshare
+mv ./pilot ../nexe/appshare
+chmod +x ../nexe/appshare
