fea6b97c8ca94fb4cc146804e6d2101521b330c5
Zubair Quraishi <fquraish@redhat.com>
2020-07-22 07:49:11 +0200

    Can see the file format now

--- src/import_mdb.js
+++ src/import_mdb.js
@@ -18,13 +18,21 @@ var binary = fs.readFileSync(dbFileName);
 show("Buffer loaded?: " , Buffer.isBuffer( binary))
 
 function show(title, value ) {
-    console.log(title + ": " + JSON.stringify(value))
+    //console.log(title + ": " + JSON.stringify(value))
+    console.log(title + ": " + value)
 }
 
 function find(offset, length ) {
     let value = binary.slice(offset, offset + length)
+    return value
 }
 
 
 let headerMagicNumber = find(0, 4)
 show("magic number", headerMagicNumber)
+
+
+
+
+let fileFormatID = find(4, 16)
+show("file format ID", fileFormatID)
