549965b456a6802ba035040d65968fcb29ca0869
Zubair Quraishi <fquraish@redhat.com>
2020-07-22 07:22:12 +0200

    read Access file into memory

--- src/import_mdb.js
+++ src/import_mdb.js
@@ -1,2 +1,30 @@
+
+var fs = require("fs");
+
 let dbFileName = process.argv[2]
+
 console.log("importing MDB: " + dbFileName )
+
+
+
+var stats = fs.statSync(dbFileName)
+var fileSizeInBytes = stats["size"]
+
+
+console.log("fileSizeInBytes: " + fileSizeInBytes )
+
+var binary = fs.readFileSync(dbFileName);
+
+show("Buffer loaded?: " , Buffer.isBuffer( binary))
+
+function show(title, value ) {
+    console.log(title + ": " + JSON.stringify(value))
+}
+
+function find(offset, length ) {
+    let value = binary.slice(offset, offset + length)
+}
+
+
+let headerMagicNumber = find(0, 4)
+show("magic number", headerMagicNumber)
