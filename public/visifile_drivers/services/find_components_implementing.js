b3eb968a26e48cc1066647d6584dc5d0863fa8a3
Zubair Quraishi <fquraish@redhat.com>
2020-09-07 10:31:48 +0100

    Trying to match data source components (not working yet)

--- public/visifile_drivers/services/find_components_implementing.js
+++ public/visifile_drivers/services/find_components_implementing.js
@@ -5,14 +5,33 @@ base_component_id("find_components_implementing")
 load_once_from_file(true)
 only_run_on_server(true)
 */
-    var promise = new Promise(async function(returnfn) {
+    var getAppsList = new Promise(function(resolve, reject) {
+            dbsearch.serialize(
+                function() {
+                    dbsearch.all(
+                        "SELECT  base_component_id, display_name  FROM system_code where code_tag = 'LATEST' and base_component_id in (select cn from (select distinct(component_name) cn ,count(component_name) ccn from component_properties  where property_name in ('background_color')  group by component_name ) where ccn = 1);"
+                        ,
 
-        //
-        //    test_connection
-        //
-        returnfn(["a","ae","a","a",])
+                        function(err, results)
+                        {
+                            //console.log("*) results: " + JSON.stringify(results,null,2));
+                            var list = [
+                            ]
+                            for (var tt = 0; tt < results.length ; tt ++) {
+                                list.push(results[tt])
+                            }
+                            //console.log("*) list: " + JSON.stringify(list,null,2));
+                            resolve(list)
+                        })
+            }, sqlite3.OPEN_READONLY)
+      })
 
-    })
-    var ret = await promise
-    return ret
+      try {
+             //console.log("*) var list2 = await getAppsList() ");
+             var list2 = await getAppsList
+             console.log("*) returning list: " + JSON.stringify(list2,null,2));
+             return list2
+      } catch(err) {
+          console.log("*) Error: " + JSON.stringify(err,null,2));
+      }
 }
