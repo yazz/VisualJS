1b8b1fd5d0b62da204419baa9753ca2497d28586
Zubair Quraishi <fquraish@redhat.com>
2020-09-08 15:27:18 +0100

    Trying to create Chart control

--- public/visifile_drivers/controls/chart.js
+++ public/visifile_drivers/controls/chart.js
@@ -169,7 +169,9 @@ logo_url("/driver_icons/chart.png")
          items:             [],
          new_value:         "",
          new_text:          "",
-         canvasId:           uuidv4()
+         canvasId:           uuidv4(),
+         mainData: null,
+         mainOptions: null
        }
      }
      ,
@@ -194,42 +196,7 @@ logo_url("/driver_icons/chart.png")
              }
          }
          var ctx = document.getElementById(this.canvasId).getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+         var myChart = new Chart(ctx, this.mainData,this.mainOptions);
       }
       ,
       methods: {
