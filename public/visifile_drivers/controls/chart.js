402fe35faed3fda5f7fc8f4fb7acead18e3c0496
Zubair Quraishi <fquraish@redhat.com>
2020-09-08 15:08:26 +0100

    Added a dummy chart

--- public/visifile_drivers/controls/chart.js
+++ public/visifile_drivers/controls/chart.js
@@ -149,17 +149,7 @@ logo_url("/driver_icons/chart.png")
                        "background-color: "+    args["background_color"]  +  ";"'
          v-else>
 
-         {{args.text?args.text:""}}
-
-        <select
-            v-on:change='changedFn();runEventHandler()'>
-
-            <option v-for='opt in args.items'
-                    v-bind:selected='args.default_selection == opt.value'
-                    v-bind:value='opt.value'>
-                {{opt.text}}
-            </option>
-        </select>
+         <canvas id="myChart" width="400" height="400"></canvas>
     </div>
 
 
@@ -195,6 +185,43 @@ logo_url("/driver_icons/chart.png")
                 this.value = this.args.value
              }
          }
+         var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
       }
       ,
       methods: {
