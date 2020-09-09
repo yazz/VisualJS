8d2da5b4c68a7b72dc9f5064898ac3b471321fee
Zubair Quraishi <fquraish@redhat.com>
2020-09-09 11:09:10 +0100

    Added a chart editor

--- public/visifile_drivers/controls/chart.js
+++ public/visifile_drivers/controls/chart.js
@@ -77,82 +77,16 @@ logo_url("/driver_icons/chart.png")
     <div v-bind:style='"height:100%;width:100%; border: 0px;color:black;"'
          v-if='design_mode == "detail_editor"'>
 
-         <div class="form-group">
-             <label for="usr">Display:</label>
-             <input v-model="new_text" type="text" class="form-control" id="usr">
-         </div>
-         <div class="form-group">
-             <label for="usr">Value:</label>
-             <input v-model="new_value" type="text" class="form-control" id="usr">
-         </div>
-         <div    class="btn btn-sm btn-info"
-                 v-on:click="items.push({value: new_value, text:new_text});new_value='';new_text='';"
-                 style="margin-bottom: 30px;"
-         >
-                 Add
-         </div>
 
+         <label for="usr">Data:</label>
+         <textarea style="font-family:monospace; height:80%"
+                   v-model="args.mainData"
+                   type="" class="form-control" id="usr"
+                   cols="50"
 
-        <table class="table">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Display</th>
-              <th scope="col">Value</th>
-              <th scope="col"></th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
+                   >
+         </textarea>
 
-
-
-          <tbody  v-bind:refresh='refresh'
-                  v-for='(child_item,index)  in  items'>
-
-             <tr v-if='child_item && isValidObject(child_item)'
-                     v-bind:refresh='refresh'>
-
-               <th scope="row">{{child_item.text}}</th>
-
-               <td>{{child_item.value}}</td>
-
-               <td>
-                   <div    class='btn btn-info'
-                           v-bind:refresh='refresh'
-                           v-on:click='var x = items[index];items.splice(index, 1);items.splice(index - 1, 0, x);changedFn();'
-                           v-if='child_item'
-                           v-bind:style='"box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;padding:0px; z-index: 21474836;opacity:1;"  +
-                           "width: 60px; height: 20px; line-height:20px;text-align: center;vertical-align: middle;margin-left: 20px;"'>
-                           UP
-
-                   </div>
-                   <div    class='btn btn-info'
-                           v-bind:refresh='refresh'
-                           v-on:click='var x = items[index];items.splice(index, 1);items.splice(index + 1, 0, x);changedFn();'
-                           v-if='child_item'
-                           v-bind:style='"box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;padding:0px; z-index: 21474836;opacity:1;"  +
-                           "width: 60px; height: 20px; line-height:20px;text-align: center;vertical-align: middle;margin-left: 20px;"'>
-                           DOWN
-
-                   </div>
-               </td>
-
-               <td>
-                   <div    class='btn btn-danger'
-                           v-bind:refresh='refresh'
-                           v-if='child_item'
-                           v-on:click='items.splice(index, 1);changedFn();'
-                           v-bind:style='"box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;padding:0px; z-index: 21474836;opacity:1;"  +
-                           "width: 20px; height: 20px; line-height:20px;text-align: center;vertical-align: middle;margin-left: 20px;"'>
-                           X
-                   </div>
-               </td>
-
-             </tr>
-
-
-
-       </tbody>
-     </table>
 
      </div>
 
@@ -164,6 +98,13 @@ logo_url("/driver_icons/chart.png")
                     width="400" height="400"
                     v-if='!design_mode'>
          </canvas>
+
+         <div       width="400"
+                    height="400"
+                    v-if='design_mode'>
+
+                    Chart - {{args.name}}
+         </div>
     </div>
 
 
