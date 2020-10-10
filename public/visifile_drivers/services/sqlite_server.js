523047c523c6af639bbc539b28fd68d712dac9bc
Zubair Quraishi <zubairq@gmail.com>
2020-10-10 08:05:34 +0200

    Can read the system tables from sqlite

--- public/visifile_drivers/services/sqlite_server.js
+++ public/visifile_drivers/services/sqlite_server.js
@@ -12,38 +12,28 @@ console.log(1)
     dbsearch.serialize(
         function() {
             var stmt = dbsearch.all(
-                "SELECT  *  FROM   system_code",
+            "SELECT " +
+            "     name " +
+            "FROM " +
+            "    sqlite_master " +
+            "WHERE " +
+            "    type ='table' AND " +
+            "    name NOT LIKE 'sqlite_%';",
                 []
                 ,
 
-                function(err, results)
+
+                function(err, results2)
                 {
-                  console.log(2)
-                    if (results) {
-                        if (results.length > 0) {
-                            var codeId = results[0].id
-                                dbsearch.all(
-                                    "SELECT dependency_name FROM app_dependencies where code_id = ?; ",
-                                    codeId,
+                    returnFn(results2)
+                }
 
-                                    function(err, results2)
-                                    {
-                                        results[0].libs = results2
-                                        console.log(3)
-                                        returnFn(results2)
-                                    })
-                        }
-
-                    }
-
-                })
+                )
     }, sqlite3.OPEN_READONLY)
   })
   console.log(4)
   var ret = await promise
   //console.log("ret: "  +  JSON.stringify(ret,null,2))
-   let tables =  [{name: "server_fdfd"},{name: "fdfdsffdsd"},
-                  {name: "fdssdfdffd"},{name: "fff"},{name: "fgg"}]
-    console.log(5)
+   let tables =  ret
   return  tables
 }
