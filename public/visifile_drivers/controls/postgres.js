91a59b8b212d1c4d4c6d9a672a59dffd6ee65dcb
Zubair Quraishi <zubairq@gmail.com>
2020-10-26 04:54:40 +0100

    add debug code

--- public/visifile_drivers/controls/postgres.js
+++ public/visifile_drivers/controls/postgres.js
@@ -8,6 +8,7 @@ base_component_id("postgres_client_component")
 load_once_from_file(true)
 visibility("PRIVATE")
 read_only(true)
+docker("docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data  postgres")
 properties(
     [
         {
