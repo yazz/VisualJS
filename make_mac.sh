1d8a683e8e5b009f580fae842070bce1376c3bbd
Farouk Z Quraishi <faroukzquraishi@Farouks-MacBook-Air.local>
2021-01-04 09:43:04 +0100

    fixing build

--- make_mac.sh
+++ make_mac.sh
@@ -1,4 +1,5 @@
 npm install -g nexe@2.0.0-next.14
-nexe src/electron.js --build --loglevel verbose -r public/ -r src/ -r package.json -r node_sqlite3_macos64.rename  -o vjs
+nexe --build --clean
+nexe src/electron.js --use_strict --build --loglevel verbose -r public/ -r src/ -r package.json -r node_modules/  -r node_sqlite3_macos64.rename -o ./vjs
 mv ./vjs ../nexe/vjs
 chmod +x ../nexe/vjs
