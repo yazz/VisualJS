3805575c3fdccccc66c25fdbe7857d0ae2addebd
Your Name <you@example.com>
2021-01-05 06:39:03 +0100

    trying to fix binary build on mac

--- make_mac.sh
+++ make_mac.sh
@@ -1,4 +1,3 @@
-npm install -g nexe@2.0.0-next.14
 nexe --build --clean
 nexe src/electron.js --use_strict --build --loglevel verbose -r public/ -r src/ -r package.json -r node_modules/  -r node_sqlite3_macos64.rename -o ./vjs
 mv ./vjs ../nexe/vjs
