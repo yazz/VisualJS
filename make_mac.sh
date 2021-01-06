bfde99db9aa623355d268668c88539851ea77381
Zubair Quraishi <zubairq@gmail.com>
2021-01-06 17:08:55 +0100

    trying to fix nexe build

--- make_mac.sh
+++ make_mac.sh
@@ -1,4 +1,4 @@
 nexe --build --clean
-nexe src/electron.js --use_strict --build --loglevel verbose -r public/ -r src/ -r package.json -r node_modules/  -r node_sqlite3_macos64.rename -o ./vjs
+nexe src/electron.js -r public/ -r src/ -r package.json -r node_modules/  -r node_sqlite3_macos64.rename -o ./vjs
 mv ./vjs ../nexe/vjs
 chmod +x ../nexe/vjs
