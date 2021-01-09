068f9a475b8f3c06341156dd4478c0c0c70e6329
Your Name <you@example.com>
2021-01-09 13:31:54 +0100

    Update make_mac.sh

--- make_mac.sh
+++ make_mac.sh
@@ -1,4 +1,4 @@
-nexe --build --clean
-nexe src/electron.js --build --loglevel verbose -r public/ -r src/ -r package.json -r node_modules/  -r node_sqlite3_macos64.rename -o ./vjs
+npm install -g pkg
+pkg . --output ./vjs
 mv ./vjs ../nexe/vjs
 chmod +x ../nexe/vjs
