a5b04e455eee9a3552e00375a4684a86c708bd56
Your Name <you@example.com>
2021-01-16 20:47:12 +0100

    Update make_windows.bat

--- make_windows.bat
+++ make_windows.bat
@@ -1,2 +1,2 @@
-nexe src/electron.js -r public\ -r src\ -r package.json -r node_sqlite3_win64.rename  -r node_sqlite3_macos64.rename  -o app.exe
-copy .\app.exe ..\nexe\app.exe /Y
+pkg . --output vjs.exe
+copy .\vjs.exe ..\nexe\vjs.exe /Y
