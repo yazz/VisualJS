61ced25e3433d71ac92e47257da20c77acd50ac5
Your Name <you@example.com>
2021-01-20 07:06:20 +0100

    Update make_linux_fedora.sh

--- make_linux_fedora.sh
+++ make_linux_fedora.sh
@@ -1,3 +1,4 @@
-nexe src/electron.js -r public/ -r src/ -r package.json -r node_sqlite3_linux64.rename  -o app
-mv ./app ../nexe/app
-chmod +x ../nexe/app
+#npm install -g pkg
+pkg . --output ./vjs
+mv ./vjs ../nexe/vjs
+chmod +x ../nexe/vjs
