9e051f44871dd5c45d053ad6eeeaad2b14a7d5a1
Zubair Quraishi <fquraish@redhat.com>
2021-03-23 05:06:32 +0000

    Added a basic help menu

--- src/electron.js
+++ src/electron.js
@@ -3,6 +3,7 @@
 
 const electron = require('electron')
 const electronApp = electron.app
+let Menu = electron.Menu
 const BrowserWindow = electron.BrowserWindow
 
 let visifile = null
@@ -3460,6 +3461,107 @@ if (electronApp) {
         //zzz
         findSystemDataDirectoryAndStart()
         finishInit()
+
+
+        const template = [
+          // { role: 'appMenu' }
+          ...(isMac ? [{
+            label: app.name,
+            submenu: [
+              { role: 'about' },
+              { type: 'separator' },
+              { role: 'services' },
+              { type: 'separator' },
+              { role: 'hide' },
+              { role: 'hideothers' },
+              { role: 'unhide' },
+              { type: 'separator' },
+              { role: 'quit' }
+            ]
+          }] : []),
+          // { role: 'fileMenu' }
+          {
+            label: 'File',
+            submenu: [
+              isMac ? { role: 'close' } : { role: 'quit' }
+            ]
+          },
+          // { role: 'editMenu' }
+          {
+            label: 'Edit',
+            submenu: [
+              { role: 'undo' },
+              { role: 'redo' },
+              { type: 'separator' },
+              { role: 'cut' },
+              { role: 'copy' },
+              { role: 'paste' },
+              ...(isMac ? [
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                  label: 'Speech',
+                  submenu: [
+                    { role: 'startSpeaking' },
+                    { role: 'stopSpeaking' }
+                  ]
+                }
+              ] : [
+                { role: 'delete' },
+                { type: 'separator' },
+                { role: 'selectAll' }
+              ])
+            ]
+          },
+          // { role: 'viewMenu' }
+          {
+            label: 'View',
+            submenu: [
+              { role: 'reload' },
+              { role: 'forceReload' },
+              { role: 'toggleDevTools' },
+              { type: 'separator' },
+              { role: 'resetZoom' },
+              { role: 'zoomIn' },
+              { role: 'zoomOut' },
+              { type: 'separator' },
+              { role: 'togglefullscreen' }
+            ]
+          },
+          // { role: 'windowMenu' }
+          {
+            label: 'Window',
+            submenu: [
+              { role: 'minimize' },
+              { role: 'zoom' },
+              ...(isMac ? [
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+              ] : [
+                { role: 'close' }
+              ])
+            ]
+          },
+          {
+            role: 'help',
+            submenu: [
+              {
+                label: 'Learn More',
+                click: async () => {
+                  const { shell } = require('electron')
+                  await shell.openExternal('https://yazz.com/visifile/mac_app.html')
+                }
+              }
+            ]
+          }
+        ]
+
+        const menu = Menu.buildFromTemplate(template)
+        Menu.setApplicationMenu(menu)
     })
 
 
