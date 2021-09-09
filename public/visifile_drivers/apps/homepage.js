7e822f7250eae94baaf8335b89d0bf3a1a545ce4
Zubair Quraishi <fquraish@redhat.com>
2021-09-09 14:59:37 +0200

    Just show import
    It looked a bit confusing

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -233,7 +233,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
                             >
                     </img>
 
-                Import app
+                Import
             </button>
 
 
