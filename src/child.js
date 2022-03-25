b2ed48ae08238f3a7dcd3784c6c0efc37eb3cc39
Zubair Quraishi <zubairq@gmail.com>
2022-03-25 07:46:16 +0100

    removed unused code

--- src/child.js
+++ src/child.js
@@ -35,163 +35,24 @@ var userData
 var childProcessName
 var showDebug = false
 var showProgress = false
-function outputDebug(text) {
-    if (showDebug) {
-         console.log(text);
-    } else {
-        if (showProgress) {
-            process.stdout.write(".");
-        }
-    }
-};
 
 var isWin                               = /^win/.test(process.platform);
-var inScan                              = false;
-var stmt2                               = null;
-var stmt3                               = null;
-var finishedFindingFolders              = false;
 var username                            = "Unknown user";
 var dbsearch;
-var lhs;
-var rhs;
-
-var stmtInsertDependency;
-var stmtInsertSubComponent;
-var stmtUpdateDriver;
-var stmtDeleteDependencies;
-
-var stmtInsertAppDDLRevision;
-var stmtUpdateLatestAppDDLRevision;
-var stmtInsertIntoAppRegistry
-var stmtUpdateAppRegistry
-
-var stmtDeleteTypesForComponentProperty;
-var stmtDeleteAcceptTypesForComponentProperty;
-
-
 
 
 var hostaddress = null
 var port = null
 
-
-//username = os.userInfo().username.toLowerCase();
 username = "node"
-//console.log(username);
 
-//dbsearch.run("PRAGMA synchronous=OFF;")
-//dbsearch.run("PRAGMA count_changes=OFF;")
-//dbsearch.run("PRAGMA journal_mode=MEMORY;")
-//dbsearch.run("PRAGMA temp_store=MEMORY;")
 var callbackIndex = 0;
 var callbackList = new Object()
 
 
 
 
-
-
-
-
-
-
-
 processMessagesFromMainProcess();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------------------------------------------------------------------------//
-//                                                                                         //
-//                                        setUpSql                                         //
-//                                                                                         //
-//   This sets up the SqlLite prepared statements                                          //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//-----------------------------------------------------------------------------------------//
-function setUpSql() {
-
-
-    stmtInsertIntoAppRegistry = dbsearch.prepare(" insert or replace into app_registry " +
-                                "    (id,  username, reponame, version, code_id ) " +
-                                " values " +
-                                "    (?, ?, ?, ?, ? );");
-
-
-    stmtUpdateAppRegistry = dbsearch.prepare(" update app_registry " +
-                                "    set code_id = ? " +
-                                " where " +
-                                "    username = ?  and  reponame = ? and version = ?;");
-
-
-
-    stmtInsertDependency = dbsearch.prepare(" insert or replace into app_dependencies " +
-                                "    (id,  code_id, dependency_type, dependency_name, dependency_version ) " +
-                                " values " +
-                                "    (?, ?, ?, ?, ? );");
-
-    stmtInsertSubComponent = dbsearch.prepare(`insert or ignore
-                                                    into
-                                               component_usage
-                                                    (base_component_id, child_component_id)
-                                               values (?,?)`)
-
-
-
-    stmtDeleteDependencies = dbsearch.prepare(" delete from  app_dependencies   where   code_id = ?");
-
-
-    //zzz
-    stmtDeleteTypesForComponentProperty = dbsearch.prepare(" delete from  component_property_types   where   component_name = ?");
-    stmtDeleteAcceptTypesForComponentProperty = dbsearch.prepare(" delete from  component_property_accept_types   where   component_name = ?");
-
-
-
-
-
-     stmtInsertAppDDLRevision = dbsearch.prepare(  " insert into app_db_latest_ddl_revisions " +
-                                                  "      ( base_component_id,  latest_revision  ) " +
-                                                  " values " +
-                                                  "      ( ?,  ? );");
-
-     stmtUpdateLatestAppDDLRevision = dbsearch.prepare(  " update  app_db_latest_ddl_revisions  " +
-                                                          "     set  latest_revision = ? " +
-                                                          " where " +
-                                                          "     base_component_id =  ? ;");
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -223,55 +84,6 @@ function callDriverMethod( findComponentArgs, args, callbackFn ) {
 
 
 
-//------------------------------------------------------------------------------
-//
-//
-//
-//
-//
-//------------------------------------------------------------------------------
-function findDriversWithMethod(methodName, callbackFn) {
-    dbsearch.serialize(
-        function() {
-            var stmt = dbsearch.all(
-                "SELECT base_component_id FROM system_code where on_condition = '\"" + methodName + "\"'; ",
-
-                function(err, results)
-                {
-                    if (results.length > 0) {
-                        callbackFn(results)
-                    } else {
-                        callbackFn(null)
-                    }
-
-                })
-    }, sqlite3.OPEN_READONLY)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------------------------------------------------------------------------//
-//                                                                                         //
-//                               processMessagesFromMainProcess                            //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//                                                                                         //
-//-----------------------------------------------------------------------------------------//
 function processMessagesFromMainProcess() {
     process.on('message', (msg) => {
       //console.log('Message from parent:', msg);
@@ -306,19 +118,6 @@ function processMessagesFromMainProcess() {
 
 
 
-
-
-
-
-      } else if (msg.message_type == 'childSetSharedGlobalVar') {
-
-
-
-    } else if (msg.message_type == 'greeting') {
-
-        outputDebug("**** greeting");
-
-
     } else if (msg.message_type == 'host_and_port') {
 
         hostaddress         = msg.ip
@@ -333,7 +132,6 @@ function processMessagesFromMainProcess() {
         showDebug           = msg.show_debug
         showProgress        = msg.show_progress
         yazzInstanceId      = msg.yazz_instance_id
-        outputDebug("yazzInstanceId in child: " + yazzInstanceId);
 
 
         ////console.log("Child recieved user data path: " + userData)
@@ -343,7 +141,6 @@ function processMessagesFromMainProcess() {
         dbsearch = new sqlite3.Database(dbPath);
         dbsearch.run("PRAGMA journal_mode=WAL;",function() {
             setTimeout(function(){
-                setUpSql();
                 process.send({  message_type:       "database_setup_in_child" ,
                                 child_process_name:  childProcessName
                                 });
@@ -353,23 +150,11 @@ function processMessagesFromMainProcess() {
 
 
 
-    } else if (msg.message_type == 'setUpSql') {
-
-
-        //setUpSql();
 
     } else if (msg.message_type == 'createTables') {
 
-        outputDebug("**** createTables");
-
         db_helper.createTables(dbsearch,
             function() {
-                outputDebug("");
-                outputDebug("***********************************");
-                outputDebug("**** createTables returned");
-                outputDebug("***********************************");
-                outputDebug("");
-
                 process.send({  message_type:       "createdTablesInChild"  });
 
             });
@@ -396,81 +181,6 @@ function processMessagesFromMainProcess() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//------------------------------------------------------------------------------
-//
-//
-//
-//
-//
-//------------------------------------------------------------------------------
 process.on('exit', function(err) {
     shutdownExeProcess(err);
   });
@@ -483,17 +193,6 @@ process.on('quit', function(err) {
 
 
 
-
-
-
-
-//------------------------------------------------------------------------------
-//
-//
-//
-//
-//
-//------------------------------------------------------------------------------
 function shutdownExeProcess(err) {
     console.log("** child.js process was killed " )
     if (err) {
@@ -504,66 +203,4 @@ function shutdownExeProcess(err) {
     if (dbsearch) {
         dbsearch.run("PRAGMA wal_checkpoint;")
     }
-}
-
-
-
-
-
-
-
-//------------------------------------------------------------------------------
-//
-//
-//
-//
-//
-//------------------------------------------------------------------------------
-function copyFile(source, target, cb) {
-  var cbCalled = false;
-
-  var rd = fs.createReadStream(source);
-  rd.on("error", function(err) {
-    done(err);
-  });
-  var wr = fs.createWriteStream(target);
-  wr.on("error", function(err) {
-    done(err);
-  });
-  wr.on("close", function(ex) {
-    done();
-  });
-  rd.pipe(wr);
-
-  function done(err) {
-    if (!cbCalled) {
-      cb(err);
-      cbCalled = true;
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-//------------------------------------------------------------------------------
-//
-//
-//
-//
-//
-//------------------------------------------------------------------------------
-function isValidObject(variable){
-    if ((typeof variable !== 'undefined') && (variable != null)) {
-        return true
-    }
-    return false
 }
