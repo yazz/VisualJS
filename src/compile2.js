f68c07b89fed18253c78dd57b4939cec970b20e9
Zubair Quraishi <zubairq@gmail.com>
2022-04-04 17:20:42 +0200

    trying to compile contracts and deploy them

--- src/compile2.js
+++ src/compile2.js
@@ -1,5 +1,6 @@
 const compiler = require("simple-solc");
 let path = require("path")
+let Web3 = require("web3")
 let fs = require("fs")
 let helloPath = path.resolve(__dirname, "", "Counter.sol")
 let source = fs.readFileSync(helloPath,"UTF-8")
@@ -9,3 +10,16 @@ const { bytecode, abi } = compiler("Counter", helloPath);
 
 console.log(bytecode);
 console.log(abi);
+
+let web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/1b2e4b6374dc4a898c6ec1d6ead55886"));
+
+
+
+let CounterContract = new web3.eth.Contract(abi);
+
+(async function() {
+  console.log("CounterContract:  " + CounterContract)
+  let val = CounterContract.count
+  console.log("val: " + val)
+
+})()
