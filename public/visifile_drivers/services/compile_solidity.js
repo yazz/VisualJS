fb0148315b066cc8aec11a40e4bc2b88ed65456c
Zubair Quraishi <zubairq@gmail.com>
2022-04-06 17:36:38 +0200

    Return correct Solidity ABI

--- public/visifile_drivers/services/compile_solidity.js
+++ public/visifile_drivers/services/compile_solidity.js
@@ -33,14 +33,19 @@ only_run_on_server(true)
       //let result =  solc.compile(args.sol,1)
       let mainKeys = Object.keys(result)
       console.log("mainKeys: " + JSON.stringify(mainKeys,null,2))
-      let contracts = result
+      let contracts = result.contracts
+      let contractKeys = Object.keys(contracts)
       let errors = result.errors
-      console.log("contracts: " + JSON.stringify(contracts,null,2))
+      console.log("contractKeys: " + JSON.stringify(contractKeys,null,2))
+      let yazzSolKeys = Object.keys(contracts["yazz.sol"])
+      console.log("yazzSolKeys: " + JSON.stringify(yazzSolKeys,null,2))
+      let yazzSolContractKeys = Object.keys(contracts["yazz.sol"].Counter)
+      console.log("yazzSolContractKeys: " + JSON.stringify(yazzSolContractKeys,null,2))
 
       //console.log(JSON.stringify(result.contracts,null,2))
-      //let { bytecode, abi } = result.contracts[":Counter"];
-      let bytecode="1"
-      let abi="2"
+      let { bytecode, abi } = contracts["yazz.sol"].Counter;
+      //let bytecode="1"
+      //let abi="2"
 
 
       //console.log(bytecode);
