288a918519f7537d9bd17b928848043cabe29632
Zubair Quraishi <zubairq@gmail.com>
2022-06-14 17:29:42 +0200

    Trying to load components from IPFS

--- src/electron.js
+++ src/electron.js
@@ -2114,10 +2114,12 @@ function websocketFn(ws) {
             let componentItems = receivedMessage.find_components.items
             let componentIds = []
             let componentHashs = []
+            let componentHashToIds = []
             for (let indexItems = 0 ; indexItems < componentItems.length ; indexItems ++ ) {
                 let componentItem = componentItems[indexItems]
                 if (componentItem.ipfsHashId) {
                     componentHashs.push(componentItem.ipfsHashId)
+                    componentHashToIds.push(componentItem.baseComponentId)
                 } else {
                     componentIds.push(componentItem.baseComponentId)
                 }
@@ -2178,6 +2180,43 @@ function websocketFn(ws) {
                     let r =z
                     //zzz
                 }
+                dbsearch.serialize(
+                    function() {
+                        let stmt = dbsearch.all(
+                            "SELECT  *  FROM   system_code   WHERE   base_component_id in " +
+                            "("  + componentHashToIds.map(function(){ return "?" }).join(",") + " )" +
+                            "   and   code_tag = 'LATEST' ",
+                            componentHashToIds
+                            ,
+
+                            function(err, results)
+                            {
+                                if (results) {
+                                    if (results.length > 0) {
+                                        var codeId = results[0].id
+                                        dbsearch.all(
+                                            "SELECT dependency_name FROM app_dependencies where code_id = ?; ",
+                                            codeId,
+
+                                            function(err, results2)
+                                            {
+                                                results[0].libs = results2
+                                                sendToBrowserViaWebSocket(
+                                                    ws,
+                                                    {
+                                                        type:                   "server_returns_loadUiComponent_to_browser",
+                                                        seq_num:                 receivedMessage.seq_num,
+                                                        record:                  JSON.stringify(results,null,2),
+                                                        args:                    JSON.stringify(receivedMessage.args,null,2),
+                                                        test:                   1
+                                                    });
+                                            })
+                                    }
+
+                                }
+
+                            })
+                    }, sqlite3.OPEN_READONLY)
 
 
 
