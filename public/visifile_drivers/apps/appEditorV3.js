ce34607391a809a260c83f707ac8d165f3ef0e00
Zubair Quraishi <zubairq@gmail.com>
2022-07-22 07:34:54 +0100

    Added a publish button for apps

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -168,11 +168,26 @@ load_once_from_file(true)
                                   style='height:35px; margin-right: 10px;'
                                   class='img-fluid'>
                               </img>
-                             Download .yazz file
+                             Download
 
                     </a>
 
-
+                      <a   v-bind:style="'margin-left:20px;margin-right: 6px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);visibility: ' + (code_shown?'':'hidden') + ';' "
+                           v-bind:href='location.protocol + "//" + location.hostname + ":" + location.port + "/app/yazz_" + editingAppId + ".yazz"'
+                           download
+                           v-if="show_download_save"
+                           v-on:mouseenter='setInfo("Edit the SQlite schema for this app")'
+                           v-on:mouseleave='setInfo(null)'
+                           type="button" class="btn btn-light ">
+    
+                        <img
+                            src='/driver_icons/publish.png'
+                            style='height:35px; margin-right: 10px;'
+                            class='img-fluid'>
+                        </img>
+                        Publish
+    
+                      </a>
 
 
 
