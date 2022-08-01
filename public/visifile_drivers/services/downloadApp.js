242e5456772d27793f3f1d3709d028483682fe9a
Zubair Quraishi <zubairq@gmail.com>
2022-08-01 16:50:22 +0100

    Can call the downloadApp function on the central server now

--- public/visifile_drivers/services/downloadApp.js
+++ public/visifile_drivers/services/downloadApp.js
@@ -5,18 +5,18 @@ base_component_id("downloadApp")
 load_once_from_file(true)
 only_run_on_server(true)
 */
-console.log("centralHost: " + centralHost)
+console.log("envVars: " + JSON.stringify(envVars,null,2))
         let promise = new Promise(async function(returnfn) {
             try {
                 const dataString = JSON.stringify(
                     {
-                        ipfs_hash: ipfs_hash,
-                        ipfs_content: ipfsContent
+                        ipfs_hash: 1,
+                        ipfs_content: 2
                     })
 
                 let options = {
-                    host: centralHost,
-                    port: centralPort,
+                    host: envVars.CENTRALHOST,
+                    port: envVars.CENTRALHOSTPORT,
                     path: '/download_app',
                     method: 'POST',
                     headers: {
@@ -26,27 +26,33 @@ console.log("centralHost: " + centralHost)
                 };
 //https
                 let theHttpsConn = http
-                if (useHttps) {
+                if (envVars.USEHTTPS) {
                     theHttpsConn = https
                 }
+                var response = "";
                 let req = theHttpsConn.request(options, function(res) {
                     console.log('STATUS: ' + res.statusCode);
                     console.log('HEADERS: ' + JSON.stringify(res.headers));
                     res.setEncoding('utf8');
                     res.on('data', function (chunk) {
+                        response += chunk;
                         console.log('BODY: ' + chunk);
                     });
                     res.on('end', function () {
-                        console.log('end: ' );
+
+                        console.log('returnfn: ' + response);
+                        let responseJson = JSON.parse(response)
+                        returnfn({
+                            new_display_name:   "downloaded",
+                            base_component_id:  "downloaded",
+                            response:            responseJson
+                        })
                     });
                 });
                 req.write(dataString)
                 req.end()
-                returnfn({
-                    new_display_name:   "downloaded",
-                    base_component_id:  "downloaded",
-                    return: res
-                })
+                console.log('end: ' );
+
             } catch(er) {
                 console.log(er)
                 returnfn({
