1620692e20f178b7fc7a38b23b2fe819297d171d
Zubair Quraishi <zubairq@gmail.com>
2022-08-04 15:22:32 +0100

    make the "new app" button bigger

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -118,10 +118,30 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
             v-if="(!edit_app) && (main_tab=='apps')"
             v-bind:refresh='refresh'>
             
-        <h1 style='vertical-align:top;display:inline-block;font-size:100px; text-align: center;margin: 0px;padding-left:70px;'>
+        <h1 style='width:100%;vertical-align:top;display:inline-block;font-size:100px; text-align: center;margin: 0px;padding-left:70px;'>
+
+
+
+
+          <button style='opacity:0.7;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius: 40px;margin-top:30;
+          margin-bottom:10px;margin-left:80px;padding:25px;font-size:60px;font-weight: bold;left:30%;'
+                  class='btn btn-lg'
+                  v-if="hideImportButtons"
+                  v-on:click='copyAndEditApp($event,"yazz_blank")'>
+            <img    src='/driver_icons/blocks.png'
+                    style='position:relative;max-width: 100px; left:0px; top: 0px;max-height: 90px;margin-left: auto;margin-right: auto;display: inline-block;'
+            >
+            </img>
+            New App
+          </button>
+
+
+
+
 
             <button style='opacity:0.7;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius: 40px;margin-bottom:10px;margin-left:40px;padding:25px;font-size:45px;font-weight: bold;'
                     class='btn btn-lg'
+                    v-if="!hideImportButtons"
                     v-on:click='copyAndEditApp($event,"yazz_blank")'>
                     <img    src='/driver_icons/blocks.png'
                             style='position:relative;max-width: 70px; left:0px; top: 0px;max-height: 70px;margin-left: auto;margin-right: auto;display: inline-block;'
