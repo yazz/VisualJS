7ebb64a3ec6b6ce8347ba7ca29d642bbc51a64f8
Zubair Quraishi <zubairq@gmail.com>
2022-08-24 06:14:52 +0200

    Update electron.js

--- src/electron.js
+++ src/electron.js
@@ -1917,6 +1917,7 @@ function getRoot(req, res, next) {
     //var homepageUrl = serverProtocol + '://yazz.com/visifile/index.html?time=' + new Date().getTime()
     //var homepageUrl = serverProtocol + '://www.yazz.com'
     var homepageUrl = 'https://yazz.com/app/homepage.html'
+    console.log("req.headers.host: " + req.headers.host)
 	if (req.headers.host) {
         if (req.query.goto) {
             outputDebug("*** FOUND goto")
