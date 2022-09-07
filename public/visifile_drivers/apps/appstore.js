0025ebacb1e6263b6bd504708ea1827d413b7f65
Zubair Quraishi <zubairq@gmail.com>
2022-09-07 13:15:32 +0200

    fixing white background on appstore

--- public/visifile_drivers/apps/appstore.js
+++ public/visifile_drivers/apps/appstore.js
@@ -38,7 +38,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
 
         <div    v-if="(edit_app == item.data.id)"
                 v-bind:refresh='refresh'
-                style="position: fixed; left:0px; top:0px; height:100%; width: 100vw ;z-index: 200000;background-color: white;overflow-y:none; padding: 0px;">
+                style="position: fixed; left:0px; top:0px; height:100%; width: 100vw ;z-index: 200000;background-color: black;overflow-y:none; padding: 0px;">
 
                 <component  id="editor_component2"
                             v-if='isValidObject(item.data)'
