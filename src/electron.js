1ef9b2defbd3331111f73add0ff65ecb6592b9a8
Zubair Quraishi <zubairq@gmail.com>
2022-09-21 23:32:01 +0200

    Use 127.0.0.1 for CORS to work locally

--- src/electron.js
+++ src/electron.js
@@ -3707,7 +3707,9 @@ console.log("\nAppShare Instance ID: " + yazzInstanceId );
 console.log("\nRunning " + executionProcessCount + " virtual processors");
 console.log("\nAppShare started on:");
 console.log("Network Host Address: " + hostaddressintranet)
-let localAddress = serverProtocol + "://" + hostaddress + ':' + port
+//let localAddress = serverProtocol + "://" + hostaddress + ':' + port
+let localAddress = serverProtocol + "://127.0.0.1:" + port
+
 console.log("Local Machine Address: " + localAddress);
 
 
