ccbdab3d1143c5e1e8cb96e57a3233e640eddfb0
Zubair Quraishi <zubairq@gmail.com>
2022-09-21 23:03:42 +0200

    fix

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -696,7 +696,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
 
               let openfileurl = "http" + (($CENTRALHOSTPORT == 443)?"s":"") + "://" + $CENTRALHOST + "/topapps"
               fetch(openfileurl, {
-                  method: 'get',
+                  method: 'post',
                   credentials: "include"
                   })
                   .then((response) => response.json())
