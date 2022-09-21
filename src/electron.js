a757f5bb2f7490198a9da582ff059d487be7c403
Zubair Quraishi <zubairq@gmail.com>
2022-09-21 22:35:18 +0200

    fix headers

--- src/electron.js
+++ src/electron.js
@@ -2831,12 +2831,6 @@ async function startServices() {
             }
 
 
-            // Request methods you wish to allow
-            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-            // Request headers you wish to allow
-            res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
             // Set to true if you need the website to include cookies in the requests sent
             // to the API (e.g. in case you use sessions)
             res.setHeader('Access-Control-Allow-Credentials', true);
