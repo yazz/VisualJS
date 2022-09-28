b893a0a91981298df676ab60cee8818dd7cdd841
Zubair Quraishi <zubairq@gmail.com>
2022-09-28 15:51:02 +0200

    trying to fix download app in production

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -1048,7 +1048,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
                   await mm.selectApp(result.base_component_id)
                   await mm.openAppid(result.base_component_id)
 
-              },50)
+              },550)
 
 
           },
