2a2facda6c2827a37e404df757b05310d6216a98
Zubair Quraishi <zubairq@gmail.com>
2022-10-04 16:56:18 +0200

    Fix the history viewer

--- public/visifile_drivers/ui_components/historyViewerComponent.js
+++ public/visifile_drivers/ui_components/historyViewerComponent.js
@@ -12,14 +12,22 @@ load_once_from_file(true)
     Vue.component("history_viewer_component", {
       data: function () {
         return {
+            text:           args.text
+            ,
             commitsV1: [
                 {codeSha: "fdsfsddfsfsdfds", timestamp: new Date().getTime()},
                 {codeSha: "fdsfsddfsfsdfds", timestamp: new Date().getTime() - 1000},
-                {codeSha: "fdsfsddfsfsdfds", timestamp: new Date().getTime() - 10000},
+                {codeSha: "fdsfsddfsfsdfds", timestamp: new Date().getTime() - 10000}
             ]
         }
       },
       template: `<div style='background-color:white; ' >
+
+                      <div style='box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);background-color: lightgray; padding: 5px;padding-left: 15px;border: 4px solid lightgray;' >
+                        <slot style='box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);display: inline-block;' v-if='text' :text2="text">
+                        </slot>
+                      </div>
+                      
                       Component History
                       
                       <div style="overflow: scroll;height:40vh">
