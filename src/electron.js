ef34d1a469cfc3131bad7943edf51565e8a9dcbe
Zubair Quraishi <zubairq@gmail.com>
2022-11-10 19:55:35 +0100

    dont show all future commit IDs

--- src/electron.js
+++ src/electron.js
@@ -7559,10 +7559,18 @@ async function getQuickSql(sql, params) {
 }
 
 
-async function getRowForCommit(commitId) {
+async function getRowForCommit(commitId, args) {
     let commitStructure = null
+    let excludeCommitId = null
+    if (args) {
+        excludeCommitId = args.excludeCommitId
+    }
     let thisCommit = await getQuickSqlOneRow("select  *  from   system_code  where   id = ? ", [  commitId  ])
-    let parentCommits = await getQuickSql("select  id  from   system_code  where  parent_id = ? ", [  commitId  ])
+    let getFutureCommitsSql = "select  id  from   system_code  where  parent_id = ? "
+    if (excludeCommitId) {
+        getFutureCommitsSql = getFutureCommitsSql + " and parent_id != '" + excludeCommitId + "'"
+    }
+    let parentCommits = await getQuickSql(getFutureCommitsSql, [  commitId  ])
 
     if (thisCommit) {
         let changesList = []
