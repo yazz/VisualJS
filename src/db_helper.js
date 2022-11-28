50b205772c530017281a3ae237aa038addcb0266
Zubair Quraishi <zubairq@gmail.com>
2022-11-28 19:14:18 +0100

    Added a definitive component list , for latest apps and controls

--- src/db_helper.js
+++ src/db_helper.js
@@ -59,7 +59,10 @@ module.exports = {
 
 
             "CREATE TABLE IF NOT EXISTS l2_app_list (id TEXT, base_component_id TEXT, version TEXT,  app_name TEXT, app_description TEXT, icon_image_id TEXT, ipfs_hash TEXT,  avg_rating NUMBER, num_ratings NUMBER);",
-            "CREATE INDEX IF NOT EXISTS app_list_idx                      ON l2_app_list (id);",
+            "CREATE INDEX IF NOT EXISTS l2_app_list_idx                      ON l2_app_list (id);",
+
+            "CREATE TABLE IF NOT EXISTS l2_component_list (id TEXT, base_component_id TEXT, version TEXT,  app_name TEXT, app_description TEXT, icon_image_id TEXT, ipfs_hash TEXT,  avg_rating NUMBER, num_ratings NUMBER);",
+            "CREATE INDEX IF NOT EXISTS l2_component_list_idx                      ON l2_component_list (id);",
 
 
             "CREATE TABLE IF NOT EXISTS component_list (id TEXT, base_component_id TEXT, version TEXT, release TEXT, latest TEXT,  component_name TEXT, component_description TEXT, icon_image_id TEXT, ipfs_hash TEXT, system_code_id TEXT);",
