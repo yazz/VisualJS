8c1339da258388515ab3832283af5d2ada8c02bf
Zubair Quraishi <zubairq@gmail.com>
2022-11-30 05:49:36 +0100

    Get the codeId of the demo app "todo", so that we can release it

--- src/electron.js
+++ src/electron.js
@@ -5771,13 +5771,14 @@ function ipc_child_returning_uploaded_app_as_file_in_child_response(msg) {
 //------------------------------------------------------------------------------
 async function evalLocalSystemDriver(driverName, location, options) {
     outputDebug("*** Loading driver: *** : " + driverName)
+    let ret
     try {
         let evalDriver = fs.readFileSync(location);
-    	await addOrUpdateDriver(driverName, evalDriver,options)
+    	ret = await addOrUpdateDriver(driverName, evalDriver,options)
     } catch (error) {
         outputDebug(error)
     }
-
+    return ret
 }
 
 
@@ -6428,7 +6429,7 @@ async function setUpComponentsLocally() {
     await evalLocalSystemDriver('vue',   path.join(__dirname, '../public/visifile_drivers/apps/vue.js'),{username: "default", reponame: "vue", version: "latest"})
     await evalLocalSystemDriver('bootstrap',   path.join(__dirname, '../public/visifile_drivers/apps/bootstrap.js'),{username: "default", reponame: "bootstrap", version: "latest"})
     await evalLocalSystemDriver('database_reader',   path.join(__dirname, '../public/visifile_drivers/apps/databaseReader.js'),{username: "default", reponame: "database_reader", version: "latest"})
-    await evalLocalSystemDriver('todo',   path.join(__dirname, '../public/visifile_drivers/apps/todo.js'),{save_html: true, username: "default", reponame: "todo", version: "latest", allowChanges: false})
+    let todoRet = await evalLocalSystemDriver('todo',   path.join(__dirname, '../public/visifile_drivers/apps/todo.js'),{save_html: true, username: "default", reponame: "todo", version: "latest", allowChanges: false})
     await evalLocalSystemDriver('todo_app_reader',   path.join(__dirname, '../public/visifile_drivers/apps/todo_app_reader.js'),{username: "default", reponame: "todo_app_reader", version: "latest"})
     await evalLocalSystemDriver('newSql',   path.join(__dirname, '../public/visifile_drivers/apps/newSqlApp.js'),{username: "default", reponame: "newSql", version: "latest"})
     //await evalLocalSystemDriver('yazzcraft',   path.join(__dirname, '../public/visifile_drivers/apps/yazzcraft.js'),{save_html: true, username: "default", reponame: "yazzcraft", version: "latest"})
