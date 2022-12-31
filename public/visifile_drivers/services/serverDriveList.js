403330b9f4adc6d4b557542fad4adbe470439083
Zubair Quraishi <zubairq@gmail.com>
2022-12-31 11:28:30 +0100

    ...bug fix in drive_list_control

--- public/visifile_drivers/services/serverDriveList.js
+++ public/visifile_drivers/services/serverDriveList.js
@@ -1,7 +1,7 @@
 async function serverDriveList(args) {
 /*
 description("Get the list of available drives")
-base_component_id("server_drive_list")
+base_component_id("serverDriveList")
 load_once_from_file(true)
 only_run_on_server(true)
 */
