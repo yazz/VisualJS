69d3b41bbc097b9830eadfeb94c0f90e4fa603df
Zubair Quraishi <zubairq@gmail.com>
2023-02-03 14:40:59 +0000

    trying to allow "copyApp" to use a Commit ID

--- public/visifile_drivers/services/copyApp.js
+++ public/visifile_drivers/services/copyApp.js
@@ -65,83 +65,73 @@ only_run_on_server(true)
         console.log("    argsNewAppId:        " + argsNewAppId)
         console.log("    userId:              " + userId)
 
-        dbsearch.serialize(
-            async function() {
-                dbsearch.all(
-                    "SELECT    ipfs_hash as id, code, component_name as display_name    FROM    yz_cache_released_components   where    " +
-                        " base_component_id = ? ;  "
-                    ,
-                    argsBaseComponentId
-                    ,
-
-                    async function(err, results)
-                    {
-                        if (results.length > 0) {
-                            var code = results[0].code
-                            if (code) {
-                                code = code.toString()
-                            }
-                            var newBaseid = argsBaseComponentId + "_" + uuidv1().replace(/\-/g, '');
-                            if (argsNewAppId) {
-                                newBaseid = argsNewAppId
-                            }
-                            console.log("    newBaseid:           " + newBaseid)
-
-                            var oldDisplayName = results[0].display_name
-                            var parentHashId = results[0].id
-                            var newDisplayName = "Copy of " + oldDisplayName
-                            console.log("    parentHashId:        " + parentHashId)
-                            code = yz.deleteCodeString(code, "load_once_from_file")
-                            code = yz.deleteCodeString(code, "read_only")
-                            code = yz.deleteCodeString(code, "visibility")
-
-                            var componentType = yz.getValueOfCodeString(code, "component_type")
-                            if (componentType) {
-                            }
-                            if (componentType == "SYSTEM") {
-                                code = yz.deleteCodeString(code, "component_type")
-                                code = yz.insertCodeString(code, "component_type", "APP")
-                            }
+        let results = await yz.getQuickSql(
+            dbsearch,
+            "SELECT    ipfs_hash as id, code, component_name as display_name    FROM    yz_cache_released_components   where    " +
+            " base_component_id = ? ;  "
+            ,
+            [argsBaseComponentId])
 
 
-                            var formEditor = yz.getValueOfCodeString(code, "formEditor",")//formEditor")
-                            if (formEditor) {
-                                formEditor.id = newBaseid
-                                code = yz.deleteCodeString(code, "formEditor",")//formEditor")
-                                code = yz.insertCodeString(code, "formEditor", formEditor,")//formEditor")
+        if (results.length > 0) {
+            var code = results[0].code
+            if (code) {
+                code = code.toString()
+            }
+            var newBaseid = argsBaseComponentId + "_" + uuidv1().replace(/\-/g, '');
+            if (argsNewAppId) {
+                newBaseid = argsNewAppId
+            }
+            console.log("    newBaseid:           " + newBaseid)
 
-                                code = yz.deleteCodeString(code, "display_name")
-                                code = yz.insertCodeString(code, "display_name", newDisplayName)
-                            }
-                            code = yz.insertCodeString(code, "visibility", "PRIVATE")
+            var oldDisplayName = results[0].display_name
+            var parentHashId = results[0].id
+            var newDisplayName = "Copy of " + oldDisplayName
+            console.log("    parentHashId:        " + parentHashId)
+            code = yz.deleteCodeString(code, "load_once_from_file")
+            code = yz.deleteCodeString(code, "read_only")
+            code = yz.deleteCodeString(code, "visibility")
 
-
-
-                            code = yz.deleteCodeString(code, "base_component_id")
-                            code = yz.insertCodeString(code, "base_component_id", newBaseid)
-
-
-                            //hack city - Vue and component strings are separated as otherwise they mark the
-                            // code as UI code
-                            var vueIndex = code.indexOf("Vue" + ".component")
-                            if (vueIndex != -1) {
-                                var vueIndexEnd = code.substring(vueIndex).indexOf(",")
-                                if (vueIndexEnd) {
-                                    code = code.substring(0,vueIndex + 14) + "'" + newBaseid + "'" + code.substring(vueIndex + vueIndexEnd )
-                                }
-                            }
-
-                            console.log("    newDisplayName:      " + newDisplayName)
-
-                            //zzz
-                            await saveCopyOfAppWithDependencies(argsBaseComponentId, newBaseid, parentHashId, code, returnfn,newDisplayName)
+            var componentType = yz.getValueOfCodeString(code, "component_type")
+            if (componentType) {
+            }
+            if (componentType == "SYSTEM") {
+                code = yz.deleteCodeString(code, "component_type")
+                code = yz.insertCodeString(code, "component_type", "APP")
+            }
 
 
-                        }
+            var formEditor = yz.getValueOfCodeString(code, "formEditor", ")//formEditor")
+            if (formEditor) {
+                formEditor.id = newBaseid
+                code = yz.deleteCodeString(code, "formEditor", ")//formEditor")
+                code = yz.insertCodeString(code, "formEditor", formEditor, ")//formEditor")
+
+                code = yz.deleteCodeString(code, "display_name")
+                code = yz.insertCodeString(code, "display_name", newDisplayName)
+            }
+            code = yz.insertCodeString(code, "visibility", "PRIVATE")
 
 
-                    })
-        }, sqlite3.OPEN_READONLY)
+            code = yz.deleteCodeString(code, "base_component_id")
+            code = yz.insertCodeString(code, "base_component_id", newBaseid)
+
+
+            //hack city - Vue and component strings are separated as otherwise they mark the
+            // code as UI code
+            var vueIndex = code.indexOf("Vue" + ".component")
+            if (vueIndex != -1) {
+                var vueIndexEnd = code.substring(vueIndex).indexOf(",")
+                if (vueIndexEnd) {
+                    code = code.substring(0, vueIndex + 14) + "'" + newBaseid + "'" + code.substring(vueIndex + vueIndexEnd)
+                }
+            }
+
+            console.log("    newDisplayName:      " + newDisplayName)
+
+            //zzz
+            await saveCopyOfAppWithDependencies(argsBaseComponentId, newBaseid, parentHashId, code, returnfn, newDisplayName)
+        }
 
     })
     var ret = await promise
