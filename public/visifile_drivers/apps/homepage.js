5f65e1b4a346a0a7f0a05e1c73d20883cb212bad
Zubair Quraishi <zubairq@gmail.com>
2023-03-06 14:39:12 +0100

    fixed bug where cant edit a forked component

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -887,9 +887,9 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
                         let cid = text.code_id
                         let fid = text.form_id
                         let cni = text.control_name
-                        globalEditorCommunicationArea.originalFormIdOfEditedUiControl           = fid
-                        globalEditorCommunicationArea.originalNameOfEditedUiControl             = cni
-                        globalEditorCommunicationArea.originalBaseComponentIdOfEditedUiControl  = bci
+                        //globalEditorCommunicationArea.originalFormIdOfEditedUiControl           = fid
+                        //globalEditorCommunicationArea.originalNameOfEditedUiControl             = cni
+                        //globalEditorCommunicationArea.originalBaseComponentIdOfEditedUiControl  = bci
                         globalEditorCommunicationArea.originalCodeIdOfEditedUiControl           = cid
                         setTimeout(function() {
                             mm.editApp(bci,  text.code_id)
