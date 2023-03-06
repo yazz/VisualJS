b7f6fc314007c5fefbf9d9d7ffed83fb9a137398
Zubair Quraishi <zubairq@gmail.com>
2023-03-06 17:21:59 +0100

    ...

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -136,7 +136,7 @@ logo_url("data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxIQEg8SEBE
                 <component  id="editor_component2"
                             v-if='isValidObject(item)'
                             :is='"app_editor_3"'
-                            v-bind:app_base_component_id='item.base_component_id'
+                            v-bind:app_base_component_id='editingBaseComponentId'
                             v-bind:app_code_id='item.code_id'
                             v-bind:card_index='index'>
                 </component>
