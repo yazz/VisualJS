2c09064a1160823534f027bf4157bf692bbac3ea
Zubair Quraishi <zubairq@gmail.com>
2023-03-23 06:11:49 +0100

    Removed new vars which we probably won't need in the end

--- src/yazz_helper_module.js
+++ src/yazz_helper_module.js
@@ -1003,15 +1003,6 @@ for (let i = 0  ;   i < results.length;    i ++ ) {
         "base_component_id": "${results[i].child_base_component_id}"
         }
     
-        GLOBALS.codeCacheV2["${sha1sum}"] = {
-        "code": unescape(\`${newcodeEs}\`)
-        }
-        
-        GLOBALS.componentTypeCacheV2["${baseComponentId}"] = {
-        "code_id": "${results[i].sha1}"
-        }
-    
-    
         GLOBALS.baseComponentIdReturnsCommitId["${results[i].child_base_component_id}"] = "${results[i].sha1}"
     `
     newCode += newCode2
