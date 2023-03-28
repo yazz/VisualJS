87a22e53f6d494fc3399abda7510b4fcdfa70fc6
Zubair Quraishi <zubairq@gmail.com>
2023-03-28 16:43:36 +0200

    replaced vars with lets

--- src/yazz_helper_module.js
+++ src/yazz_helper_module.js
@@ -99,13 +99,13 @@ module.exports = {
     ,
 
      insertCodeString: function(code,st, vall ,optionalEnd) {
-         var endIndicator = ")"
+         let endIndicator = ")"
          if (optionalEnd) {
              endIndicator = optionalEnd
          }
-        var findd = st + "("
-        var startIndexOfComment = code.toString().indexOf("/*")
-        var startIndexOfFn = code.toString().indexOf("{")
+        let findd = st + "("
+        let startIndexOfComment = code.toString().indexOf("/*")
+        let startIndexOfFn = code.toString().indexOf("{")
 
         if (startIndexOfFn != -1) {
             if (startIndexOfComment == -1) {
@@ -129,14 +129,14 @@ module.exports = {
 
 
      deleteCodeString: function(code,st ,optionalEnd) {
-         var endIndicator = ")"
+         let endIndicator = ")"
          if (optionalEnd) {
              endIndicator = optionalEnd
          }
-        var findd = st + "("
-        var codeStart = code.toString().indexOf(findd)
+        let findd = st + "("
+        let codeStart = code.toString().indexOf(findd)
         if (codeStart != -1) {
-            var codeEnd = codeStart + code.toString().substring(codeStart).indexOf(endIndicator)
+            let codeEnd = codeStart + code.toString().substring(codeStart).indexOf(endIndicator)
 
             code = code.toString().substring(0,codeStart) +
                             code.toString().substring(codeEnd + 1 + endIndicator.length)
@@ -149,17 +149,17 @@ module.exports = {
 
 
      getValueOfCodeString: function(code, st,optionalEnd) {
-        var endIndicator = ")"
+        let endIndicator = ")"
         if (optionalEnd) {
             endIndicator = optionalEnd
         }
-        var toFind = st + "("
+        let toFind = st + "("
         if (code.toString().indexOf(toFind) != -1) {
-            var codeStart = code.toString().indexOf(toFind) + toFind.length
-            var codeEnd = codeStart + code.toString().substring(codeStart).indexOf(endIndicator)
+            let codeStart = code.toString().indexOf(toFind) + toFind.length
+            let codeEnd = codeStart + code.toString().substring(codeStart).indexOf(endIndicator)
 
             code = code.toString().substring(codeStart, codeEnd)
-            var val = eval( "(" + code.toString() + ")")
+            let val = eval( "(" + code.toString() + ")")
             return val
 
             }
@@ -167,22 +167,22 @@ module.exports = {
     },
 
     replaceBetween: function(target, start, end, replaceWith) {
-                                        var startIndex = target.indexOf(start) + start.length
-                                        var endIndex = target.indexOf(end)
-                                        var newString = target.substring(0,startIndex) + replaceWith + target.substring(endIndex);
+                                        let startIndex = target.indexOf(start) + start.length
+                                        let endIndex = target.indexOf(end)
+                                        let newString = target.substring(0,startIndex) + replaceWith + target.substring(endIndex);
                                         return newString
     }
     ,
     getBetween: function(target, start, end) {
-        var startIndex = target.indexOf(start) + start.length
-        var endIndex = target.indexOf(end)
-        var newString = target.substring(startIndex,endIndex)
+        let startIndex = target.indexOf(start) + start.length
+        let endIndex = target.indexOf(end)
+        let newString = target.substring(startIndex,endIndex)
         return newString
     }
     ,
 
     replacePropertyValue: function(code, propertyId, propertyValue) {
-      var properties = this.getValueOfCodeString(code,"properties",")//prope" + "rties")
+      let properties = this.getValueOfCodeString(code,"properties",")//prope" + "rties")
       if (properties) {
           let index =0;
           for (let i=0; i < properties.length; i++) {
@@ -205,7 +205,7 @@ module.exports = {
     ,
 
     addProperty: function(code, newProperty) {
-        var properties = this.getValueOfCodeString(code,"properties",")//prope" + "rties")
+        let properties = this.getValueOfCodeString(code,"properties",")//prope" + "rties")
         if (properties) {
             properties.push(newProperty)
 
