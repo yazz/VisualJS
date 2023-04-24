e92c3c0d87eefe6c1e165180beca53813a4121e7
Zubair Quraishi <zubairq@gmail.com>
2023-04-24 13:59:32 +0200

    Removing duplicate code from the VB editor plugin

--- public/visifile_drivers/ui_components/codePlugInVbEditor.js
+++ public/visifile_drivers/ui_components/codePlugInVbEditor.js
@@ -3,6 +3,7 @@ async function component( args ) {
 base_component_id("vb_editor_component")
 component_type("SYSTEM")
 load_once_from_file(true)
+runtime_pipeline(["EDITOR_PLUG_IN"])
 uses_javascript_libraries(["advanced_bundle"])
 */
 
@@ -3010,43 +3011,8 @@ __________
                          }
                         },
         methods: {
-            loadControls:                           async function  () {
-             /*
-            ________________________________________
-            |                                      |
-            |             loadControls             |
-            |                                      |
-            |______________________________________|
-
-            This loads the controls for the control palette, which allows the user
-            to add buttons, labels, and other controls to their app
-
-            __________
-            | Params |
-            |        |______________________________________________________________
-            |
-            |     NONE
-            |________________________________________________________________________ */
-           let mm = this
-           let sql =    "select  base_component_id,  app_icon_data as logo_url  from  yz_cache_released_components  " +
-                        " inner JOIN " +
-                        "     icon_images ON yz_cache_released_components.icon_image_id = icon_images.id " +
-                        "where " +
-                        "    icon_image_id is not null and component_type = 'component'"
-
-           let results = await callComponent({ base_component_id:    "readFromInternalSqliteDatabase"},
-               {   sql: sql  })
-
-           mm.available_components = results
-           let itemsToLoad = []
-           for (let thiscc of results) {
-               let cbase = thiscc.base_component_id
-               //console.log("Component: " + JSON.stringify(cbase))
-               itemsToLoad.push(cbase)
-           }
-           await loadUiComponentsV4(itemsToLoad)
-           //console.log("Time " + (ttq++) + ": " + (new Date().getTime()- startTime))
-         },
+            /* ** *** insert_ui_methods_start *** ** */
+            /* ** *** insert_ui_methods_end *** ** */
             addCodeChange:                          function        (changeText) {
              /*
             ________________________________________
