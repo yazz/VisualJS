3c880a006481743b2a375981fd58d26d6d9a9280
Zubair Quraishi <zubairq@gmail.com>
2023-06-09 10:56:46 +0100

    If we are editing a control in standalone mode then show the "Remix" button

--- public/visifile_drivers/apps/appEditorV3.js
+++ public/visifile_drivers/apps/appEditorV3.js
@@ -477,7 +477,7 @@ ___________
                               v-on:mouseleave='setInfo(null)'
                               v-on:click='setTimeout(function(){copyApp(base_component_id, null,code_id)},100)'
                               type="button" class="btn  btn-primary"
-                              v-if='read_only && (mode != "profiler") && (!editor_overloaded) && (preview_type == "app")'>
+                              v-if='read_only && (mode != "profiler") && (!editor_overloaded) && ((preview_type == "app") || ((preview_type == "control")) && (GLOBALS.lastEditingAppCodeId == null))'>
 
                               <img
                                   src='/driver_icons/remix.png'
