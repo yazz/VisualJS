964bb40acd0913d9f74bed297bb34016887a072a
Zubair Quraishi <zubairq@gmail.com>
2023-06-15 08:00:06 +0100

    Cleanup

--- public/visifile_drivers/ui_components/iconEditorComponent.js
+++ public/visifile_drivers/ui_components/iconEditorComponent.js
@@ -29,17 +29,13 @@ load_once_from_file(true)
                           </div>
         
                 
-                          <!-- ---------------------------------------------------------------------------------------------
-                          Show the new style view 
-                          --------------------------------------------------------------------------------------------- -->
+                          <!-- ------------- Title  ----------------- -->
                           <div  style='overflow: scroll;height:75%;border-radius: 5px;margin-left:15px;margin-top:15px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border: 4px solid lightgray;padding:5px; '>
-                                     
                             <div    style='font-size:14px;font-weight:bold;border-radius: 0px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);background-image: linear-gradient(to right,  #000099, lightblue); color: white; border: 0px solid lightgray; padding:4px; margin:0;padding-left:14px;'>
-                
                               Component Icon Editor
                             </div>
 
-        
+                            <!-- ------------- Drawing canvas  ----------------- -->
                             <canvas v-bind:id='"_canvas_" '
                                     v-bind:refresh='refresh'
                                     style="border: solid black 1px;margin-bottom: 10px;"
@@ -47,20 +43,17 @@ load_once_from_file(true)
                                     v-on:mousedown='mousedown=true;drawNow($event)'
                                     v-on:mouseup='mousedown=false'
                                     v-bind:height='iconHeightPixels + "px"'
-                                    v-bind:width='iconWidthPixels + "px"'
-                            >
+                                    v-bind:width='iconWidthPixels + "px"'>
                             </canvas>
 
                             <div>
+                              <!-- ------------- Color palette  ----------------- -->
                               <div    v-for="color in colors"
                                       v-on:click='draw_color = color;'
                                       v-bind:style="'display: inline-block;width:15px;height:15px;background-color: ' + color">
                               </div>
-        
-        
-        
-        
-        
+                              
+                              <!-- ------------- Brushes ----------------- -->
                               <div    v-for="brush_size in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]"
                                       v-on:click='brush_width = brush_size;'
                                       v-bind:style="'display: inline-block;width:' + brush_size + 'px;height:' + brush_size +
@@ -68,51 +61,23 @@ load_once_from_file(true)
                               </div>
                             </div>
 
-
+                            <!-- ------------- "Save changes" button  ----------------- -->
                             <button    class="btn btn-dark"
                                        v-on:click="save">
                               Save
                             </button>
-
-
-
-
-                  </div>
-
-
-                      </div>
-            `,
+                          </div>
+                      </div>`,
         mounted:    async function() {
         },
         methods:    {
-
-            // editor interface
             getText:                            async function () {
-                 // -----------------------------------------------------
-                 //                      getText
-                 //
-                 // -----------------------------------------------------
-                 if (!isValidObject(this.text)) {
-                     return null
-                 }
-
-                 return this.text
-             },
+                if (!isValidObject(this.text)) {
+                    return null
+                }
+                return this.text
+            },
             setText:                            async function (  textValue  ) {
-
-                /*
-                ________________________________________
-                |                                      |
-                |             setText                  |
-                |                                      |
-                |______________________________________|
-                This is called to set the component state
-                __________
-                | PARAMS |______________________________________________________________
-                |
-                |     textValue     Use the component code to find out what changes
-                |     ---------     have been made to this code
-                |________________________________________________________________________ */
                 let mm     =  this
                 this.text  = textValue
                 if (!isValidObject(this.text)) {
@@ -120,7 +85,6 @@ load_once_from_file(true)
                 }
 
                 this.baseComponentId        = yz.getValueOfCodeString(this.text, "base_component_id")
-
             },
             drawNow: function(event) {
                 var mm= this
