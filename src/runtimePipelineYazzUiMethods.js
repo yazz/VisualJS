a051f6605bf4898b48eb558269c254abe1af117a
Zubair Quraishi <zubairq@gmail.com>
2023-07-17 08:25:58 +0100

    Finally fixed the error with "Form_1" whenever we edit a default control

--- src/runtimePipelineYazzUiMethods.js
+++ src/runtimePipelineYazzUiMethods.js
@@ -6685,121 +6685,126 @@ __________
                             if (mm.old_model) {
                                 ttt = jsondiffpatch2.diff(mm.old_model,mm.model)
                                 //console.log("Changes: "+ JSON.stringify(ttt,null,2))
-                            }
 
-                            let changedUuids = {}
+                                let changedUuids = {}
 
-                            if (ttt) {
-                                mm.old_model = JSON.parse(JSON.stringify(mm.model));
+                                if (ttt) {
+                                    mm.old_model = JSON.parse(JSON.stringify(mm.model));
 
-                                //debugger
-                                //
-                                // find  out what components have changed in the current form
-                                //
-                                if (ttt.forms[this.active_form]) {
-                                    let allComps = Object.keys(ttt.forms[this.active_form].components)
-                                    let numComp = allComps.length
-                                    for (let componentIndex = 0; componentIndex < numComp; componentIndex++){
-                                        let componentNN = allComps[componentIndex]
-                                        let thisComponent = ttt.forms[this.active_form].components[componentNN]
-                                        let nn = parseInt(componentNN)
-                                        if (nn != NaN) {
-                                            let compname = mm.model.forms[this.active_form].components[nn]
-                                            if (compname) {
-                                                //console.log(this.active_form + ": " + compname.name + " = " + JSON.stringify(thisComponent))
-                                                changedUuids[compname.uuid] = true
-                                            }
-                                        }
-                                    }
-                                }
-
-
-
-                                //
-                                // show a list of properties to watch
-                                //
-                                //console.log("Watch list: " + JSON.stringify(this.watchList,null,2))
-                                //console.log(JSON.stringify(this.watchList,null,2))
-
-
-                                //
-                                //
-                                //
-                                //debugger
-                                for (      let componentIndex = 0;
-                                           componentIndex < mm.model.forms[this.active_form].components.length;
-                                           componentIndex++)  {
-
-                                    let thisComponent  = mm.model.forms[this.active_form].components[componentIndex]
-                                    let uuid           = thisComponent.uuid
-                                    let ww2            = this.watchList
-
-                                    //console.log("UUID: " + JSON.stringify(uuid,null,2))
-                                    //console.log(this.watchList[uuid])
-                                    for (  let aaq = 0  ;  aaq < ww2.length  ;  aaq++  ) {
-                                        let ww = ww2[ aaq ]
-
-                                        if (ww) {
-                                            if (ww.from_component_uuid == uuid) {
-                                                if (changedUuids[uuid]) {
-                                                    //debugger
-                                                    //console.log(ww)
-
-                                                    let fromc = mm.form_runtime_info[ww.form_name].component_lookup_by_uuid[uuid]
-                                                    //console.log("fromc: " + JSON.stringify(fromc,null,2))
-
-
-                                                    let touuid = ww.to_component_uuid
-                                                    let toc = mm.form_runtime_info[ww.form_name].component_lookup_by_uuid[touuid]
-                                                    //console.log("toc: " + JSON.stringify(toc,null,2))
-
-
-
-                                                    //mm.model.forms[this.active_form].components[0].text = "" + mm.model.forms[this.active_form].components[1].value
-                                                    let vvvvvv = fromc[ww.from_component_property_name]
-                                                    let toValue = JSON.parse(JSON.stringify(vvvvvv))
-
-                                                    if (ww.transform_fn) {
-                                                        try {
-                                                            let toValueFn = eval("("+ ww.transform_fn + ")")
-                                                            toValue = toValueFn(toValue)
-                                                        } catch (err) {
-                                                            console.log(err)
-                                                        }
+                                    //debugger
+                                    //
+                                    // find  out what components have changed in the current form
+                                    //
+                                    if (isValidObject(ttt.forms)) {
+                                        if (ttt.forms[this.active_form]) {
+                                            let allComps = Object.keys(ttt.forms[this.active_form].components)
+                                            let numComp = allComps.length
+                                            for (let componentIndex = 0; componentIndex < numComp; componentIndex++){
+                                                let componentNN = allComps[componentIndex]
+                                                let thisComponent = ttt.forms[this.active_form].components[componentNN]
+                                                let nn = parseInt(componentNN)
+                                                if (nn != NaN) {
+                                                    let compname = mm.model.forms[this.active_form].components[nn]
+                                                    if (compname) {
+                                                        //console.log(this.active_form + ": " + compname.name + " = " + JSON.stringify(thisComponent))
+                                                        changedUuids[compname.uuid] = true
                                                     }
-                                                    let oldValue = toc[ww.to_component_property_name]
-                                                    toc[ww.to_component_property_name] = toValue
-                                                    //console.log(toValue)
-
-                                                    //debugger
-                                                    mm.processControlEvent(
-                                                        {
-                                                            type:               "subcomponent_event",
-                                                            form_name:           mm.active_form,
-                                                            control_name:        toc.name,
-                                                            sub_type:           "on_property_in",
-                                                            code:                toc.on_property_in,
-                                                            args:               {
-                                                                from_form:            mm.active_form,
-                                                                from_component:       fromc.name,
-                                                                from_property:        ww.from_component_property_name,
-                                                                from_value:           toValue,
-                                                                to_form:              mm.active_form,
-                                                                to_component:         toc.name,
-                                                                to_property:          ww.to_component_property_name,
-                                                                to_value:             toValue,
-                                                                to_old_value:         oldValue,
-                                                                to_new_value:         toValue
-                                                            }
-                                                        })
                                                 }
-
                                             }
                                         }
                                     }
+
+
+
+
+                                    //
+                                    // show a list of properties to watch
+                                    //
+                                    //console.log("Watch list: " + JSON.stringify(this.watchList,null,2))
+                                    //console.log(JSON.stringify(this.watchList,null,2))
+
+
+                                    //
+                                    //
+                                    //
+                                    //debugger
+                                    for (      let componentIndex = 0;
+                                               componentIndex < mm.model.forms[this.active_form].components.length;
+                                               componentIndex++)  {
+
+                                        let thisComponent  = mm.model.forms[this.active_form].components[componentIndex]
+                                        let uuid           = thisComponent.uuid
+                                        let ww2            = this.watchList
+
+                                        //console.log("UUID: " + JSON.stringify(uuid,null,2))
+                                        //console.log(this.watchList[uuid])
+                                        for (  let aaq = 0  ;  aaq < ww2.length  ;  aaq++  ) {
+                                            let ww = ww2[ aaq ]
+
+                                            if (ww) {
+                                                if (ww.from_component_uuid == uuid) {
+                                                    if (changedUuids[uuid]) {
+                                                        //debugger
+                                                        //console.log(ww)
+
+                                                        let fromc = mm.form_runtime_info[ww.form_name].component_lookup_by_uuid[uuid]
+                                                        //console.log("fromc: " + JSON.stringify(fromc,null,2))
+
+
+                                                        let touuid = ww.to_component_uuid
+                                                        let toc = mm.form_runtime_info[ww.form_name].component_lookup_by_uuid[touuid]
+                                                        //console.log("toc: " + JSON.stringify(toc,null,2))
+
+
+
+                                                        //mm.model.forms[this.active_form].components[0].text = "" + mm.model.forms[this.active_form].components[1].value
+                                                        let vvvvvv = fromc[ww.from_component_property_name]
+                                                        let toValue = JSON.parse(JSON.stringify(vvvvvv))
+
+                                                        if (ww.transform_fn) {
+                                                            try {
+                                                                let toValueFn = eval("("+ ww.transform_fn + ")")
+                                                                toValue = toValueFn(toValue)
+                                                            } catch (err) {
+                                                                console.log(err)
+                                                            }
+                                                        }
+                                                        let oldValue = toc[ww.to_component_property_name]
+                                                        toc[ww.to_component_property_name] = toValue
+                                                        //console.log(toValue)
+
+                                                        //debugger
+                                                        mm.processControlEvent(
+                                                            {
+                                                                type:               "subcomponent_event",
+                                                                form_name:           mm.active_form,
+                                                                control_name:        toc.name,
+                                                                sub_type:           "on_property_in",
+                                                                code:                toc.on_property_in,
+                                                                args:               {
+                                                                    from_form:            mm.active_form,
+                                                                    from_component:       fromc.name,
+                                                                    from_property:        ww.from_component_property_name,
+                                                                    from_value:           toValue,
+                                                                    to_form:              mm.active_form,
+                                                                    to_component:         toc.name,
+                                                                    to_property:          ww.to_component_property_name,
+                                                                    to_value:             toValue,
+                                                                    to_old_value:         oldValue,
+                                                                    to_new_value:         toValue
+                                                                }
+                                                            })
+                                                    }
+
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    mm.refresh++
+
                                 }
 
-                                mm.refresh++
                             }
                         }
 
