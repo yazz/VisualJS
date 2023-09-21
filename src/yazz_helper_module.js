4c221ff712996a99209f37042e41acf88d329588
Zubair Quraishi <zubairq@gmail.com>
2023-09-21 15:33:35 +0200

    bug fix

--- src/yazz_helper_module.js
+++ src/yazz_helper_module.js
@@ -2315,7 +2315,7 @@ module.exports = {
                             //console.log("hash record: " + JSON.stringify(hashRecord,null, 2))
                             await mm.executeQuickSql(
                                 thisDb,
-                                "insert into ipfs_hashes_queue_to_download  (  ipfs_hash   ,   master_time_millis  , status  debug_master_time_millis ) values ( ? , ? , ? , ?)",
+                                "insert into ipfs_hashes_queue_to_download  (  ipfs_hash   ,   master_time_millis  , status  ,  debug_master_time_millis ) values ( ? , ? , ? , ?)",
                                 [hashRecord.ipfs_hash, hashRecord.local_time_millis, "QUEUED", mm.msToTime(hashRecord.local_time_millis)]
                             )
                         }
