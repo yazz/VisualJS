d577e15c84fd03bc162739273bea7012aedee59e
Zubair Quraishi <zubairq@gmail.com>
2023-09-29 14:24:32 +0200

    Tell master server not to sync to another master

--- yazz_app_loop.sh
+++ yazz_app_loop.sh
@@ -2,7 +2,7 @@
 
 while true
       do
-          node src/electron.js --port 443 --public $1 --private $2 --host $3  --usehost $3  --https true  --hostport 443 --centralhost $3  --centralhostport 443 --centralhosthttps true --cacert1 $4    --cacert2 $5    --cacert3 $6   --showdebug true  --virtualprocessors 20   --hideimportbuttons true   --deleteonstartup true  --deleteonexit true  > nohup.out
+          node src/electron.js --port 443 --public $1 --private $2 --host $3  --usehost $3  --https true  --hostport 443 --centralhost $3  --centralhostport 443 --centralhosthttps true --cacert1 $4    --cacert2 $5    --cacert3 $6   --showdebug true  --virtualprocessors 20   --hideimportbuttons true   --synctomaster false  --deleteonstartup true  --deleteonexit true  > nohup.out
       done
 
 
