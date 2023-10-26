07b3935f8b016d6cb6213538c67675ef95ae35ce
Zubair Quraishi <zubairq@gmail.com>
2023-10-26 05:30:52 +0200

    "Play" an app works now
    For some reason just making the correct "async" fn call fixed it

--- public/visifile_drivers/apps/homepage.js
+++ public/visifile_drivers/apps/homepage.js
@@ -563,7 +563,7 @@ disableHighlightEditableApp:            {{ disableHighlightEditableApp }}
                     
                                     <button style='position:absolute;top:250px;left:0px;opacity:0.9;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius: 5px;margin-bottom:10px;margin-left:40px;padding:10px;font-size:20px;z-index:2147483647;'
                                             class='btn btn-sm'
-                                            v-on:click='showProgressBar();$event.stopPropagation();downloadAndRunApp(item.ipfs_hash)'>
+                                            v-on:click='showProgressBar();$event.stopPropagation();(async function() {await downloadAndRunApp(item.ipfs_hash)})()'>
                                       <img    src='/driver_icons/play.png'
                                               style='position:relative;max-width: 60px; left:0px; top: 0px;max-height: 40px;margin-left: auto;margin-right: auto;display: inline-block;'
                                       >
