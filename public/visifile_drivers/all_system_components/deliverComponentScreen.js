e2e308d31c403dcbe49866515a3ca1b389622a62
Zubair Quraishi <zubairq@gmail.com>
2023-12-04 13:50:15 +0100

    Can switch to the old DB editor now

--- public/visifile_drivers/all_system_components/deliverComponentScreen.js
+++ public/visifile_drivers/all_system_components/deliverComponentScreen.js
@@ -555,7 +555,7 @@ pane_release_description:               {{pane_release_description}}
         <div>
             <a   v-bind:style="'margin-left:0px;margin-right: 0px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);;' "
                  href="#"
-                 v-on:click='setTimeout(async function(){await switchEditor("sqlite_editor_component")},100)'
+                 v-on:click='setTimeout(async function(){$root.$emit("message", {type:          "switch_editor",editorName: "sqlite_editor_component"})},100)'
                  type="button" class="btn btn-light ">
     
               <img
