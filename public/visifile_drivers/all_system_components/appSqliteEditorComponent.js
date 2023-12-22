58070c5311c8c204cf96d630bd7eeff6170980be
Zubair Quraishi <zubairq@gmail.com>
2023-12-22 13:58:06 +0100

    Trying to allow autosave when editing DB

--- public/visifile_drivers/all_system_components/appSqliteEditorComponent.js
+++ public/visifile_drivers/all_system_components/appSqliteEditorComponent.js
@@ -248,7 +248,7 @@ load_once_from_file(true)
         mounted:    async function() {
             let mm = this
             await useTabulatorJs()
-            yz.mainVars.disableAutoSave     = true
+            yz.mainVars.disableAutoSave     = false
             mm.pane_home_selectedTable      = null
             await mm.switchTab({tabName: "home"})
         },
