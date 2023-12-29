5a3a7172030cb0652da8ee0690d7f20c7ac1ad0f
Zubair Quraishi <zubairq@gmail.com>
2023-12-29 21:38:24 +0000

    cleanup

--- public/visifile_drivers/all_system_components/appSqliteEditorComponent.js
+++ public/visifile_drivers/all_system_components/appSqliteEditorComponent.js
@@ -763,45 +763,41 @@ use_db("todo")
                             return menu;
                         };
 
-                        Vue.nextTick(async function () {
 
-                            mm.pane_home_tabulator = new Tabulator("#db_editor_grid_view",
-                                {
-                                    reactiveData:       true,
-                                    width:              "100px",
-                                    //height:           "70px",
-                                    rowHeight:          30,
-                                    tables:             [],
-                                    data:               mm.data_rows,
-                                    layout:             "fitDataStretch",
-                                    //responsiveLayout:   "hide",
-                                    responsiveLayout:   true,
-                                    tooltips:           true,
-                                    addRowPos:          "top",
-                                    history:            true,
-                                    pagination:         "local",
-                                    paginationSize:     7,
-                                    movableColumns:     true,
-                                    resizableColumns:   true,
-                                    resizableRows:      true,
-                                    layout:             [
-                                        //{display: "Fit Columns",    value: "fitColumns"},
-                                        //{display: "Fit Data",       value: "fitData"},
-                                        //{display: "Fit Data Fill",  value: "fitDataFill"}
-                                    ],
-                                    tableNames:         [],
-                                    initialSort:        [],
-                                    rowContextMenu:     rowMenu,
-                                    columns:            [
-                                        //{title:"Name",              field:"name",   width:150                               , headerMenu: headerMenu, headerFilter:"input"}
-                                    ]
-                                });
-                            window.dbEditorWindow = mm
-
+                        mm.pane_home_tabulator = new Tabulator("#db_editor_grid_view",
+                            {
+                                reactiveData:       true,
+                                width:              "100px",
+                                //height:           "70px",
+                                rowHeight:          30,
+                                tables:             [],
+                                data:               mm.data_rows,
+                                layout:             "fitDataStretch",
+                                //responsiveLayout:   "hide",
+                                responsiveLayout:   true,
+                                tooltips:           true,
+                                addRowPos:          "top",
+                                history:            true,
+                                pagination:         "local",
+                                paginationSize:     7,
+                                movableColumns:     true,
+                                resizableColumns:   true,
+                                resizableRows:      true,
+                                layout:             [
+                                    //{display: "Fit Columns",    value: "fitColumns"},
+                                    //{display: "Fit Data",       value: "fitData"},
+                                    //{display: "Fit Data Fill",  value: "fitDataFill"}
+                                ],
+                                tableNames:         [],
+                                initialSort:        [],
+                                rowContextMenu:     rowMenu,
+                                columns:            [
+                                    //{title:"Name",              field:"name",   width:150                               , headerMenu: headerMenu, headerFilter:"input"}
+                                ]
+                            });
+                        window.dbEditorWindow = mm
 
 
-
-                        })
                     })
                 }
                 setTimeout(async function ( ) {
