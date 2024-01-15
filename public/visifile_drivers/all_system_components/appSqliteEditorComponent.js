ab9351b76e6045ffa7bbf7daa041e59173113877
Zubair Quraishi <zubairq@gmail.com>
2024-01-15 14:39:23 +0100

    Adding a DB column editor

--- public/visifile_drivers/all_system_components/appSqliteEditorComponent.js
+++ public/visifile_drivers/all_system_components/appSqliteEditorComponent.js
@@ -5,7 +5,7 @@ component_type("SYSTEM")
 load_once_from_file(true)
 use_db("todo")
 */
-    var rowMenu = [
+    let rowMenu = [
         {
             label: "<i class='fas fa-user'></i> Change Name",
             action: function (e, row) {
@@ -36,47 +36,17 @@ use_db("todo")
                 },
             ]
         }]
-    var headerMenu = function () {
+    let mm = this
+    let headerMenu = function () {
         var menu = [];
-        var columns = this.getColumns();
 
-        for (let column of columns) {
-
-            //create checkbox element using font awesome icons
-            let icon = document.createElement("i");
-            icon.classList.add("fas");
-            icon.classList.add(column.isVisible() ? "fa-check-square" : "fa-square");
-
-            //build label
-            let label = document.createElement("span");
-            let title = document.createElement("span");
-
-            title.textContent = " " + column.getDefinition().title;
-
-            label.appendChild(icon);
-            label.appendChild(title);
-
-            //create menu item
-            menu.push({
-                label: label,
-                action: function (e) {
-                    //prevent menu closing
-                    e.stopPropagation();
-
-                    //toggle current column visibility
-                    column.toggle();
-
-                    //change menu item icon
-                    if (column.isVisible()) {
-                        icon.classList.remove("fa-square");
-                        icon.classList.add("fa-check-square");
-                    } else {
-                        icon.classList.remove("fa-check-square");
-                        icon.classList.add("fa-square");
-                    }
-                }
-            });
-        }
+        menu.push({
+            label: "Edit column",
+            action: function (e) {
+                e.stopPropagation();
+                alert("Del")
+            }
+        });
 
         return menu;
     };
