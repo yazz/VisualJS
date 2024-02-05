1cfb11407a5e2f370e1b05054d0ff3e98da58aa6
Zubair Quraishi <zubairq@gmail.com>
2024-02-05 14:50:56 +0000

    ...

--- public/visifile_drivers/controls/yazzSqlite.js
+++ public/visifile_drivers/controls/yazzSqlite.js
@@ -114,6 +114,14 @@ properties(
             default:    true,
             hidden:     true
         }
+        ,
+        {
+            id:         "show_driver_ui",
+            name:       "Show Driver UI?",
+            type:       "Boolean",
+            default:    true,
+            hidden:     true
+        }
 
 
     ]
@@ -126,18 +134,21 @@ logo_url("/driver_icons/sqlite.jpg")
         template:   ` 
 <div    v-bind:style='"white-space:normal;height:100%;width:100%; border: 0px;" +
         "background-color: "+    args["background_color"]  +  ";"'>
-    <div v-if="design_mode">
-        Internal Yazz DB
-        </br/>
-        {{tables.length}} tables
-    </div>
-    <div v-else>
-        Sqlite:
-        SQLITE LIVE
-        <br/>
-        Tables: 
-        {{tables}}
-    </div>
+        
+    <div  v-if='properties.show_driver_ui'>
+        <div v-if="design_mode">
+            Internal Yazz DB
+            </br/>
+            {{tables.length}} tables
+        </div>
+        <div v-else>
+            Sqlite:
+            SQLITE LIVE
+            <br/>
+            Tables: 
+            {{tables}}
+        </div>
+    </div>    
 </div>`,
         data:       function(  ) {
             return {
