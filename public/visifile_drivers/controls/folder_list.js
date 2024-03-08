68caa7dca928d9278a43fb6ed8775f1b01151045
yazz <zubairq@gmail.com>
2024-03-08 14:06:58 +0100

    Update folder_list.js

--- public/visifile_drivers/controls/folder_list.js
+++ public/visifile_drivers/controls/folder_list.js
@@ -64,10 +64,10 @@ logo_url("/driver_icons/folder_list.png")
 */
 
     Yazz.component({
-      props: ["args","design_mode",  "runEvent"]
+      props: ["control_properties_and_events","design_mode",  "runEvent"]
       ,
       template: `<div v-bind:style='"height:100%;width:100%; border: 0px;" +
-                                    "background-color: "+    args["background_color"]  +  ";"'>
+                                    "background-color: "+    control_properties_and_events["background_color"]  +  ";"'>
 
                                     <select
                                         v-on:change='changedFn();runEventHandler()'
@@ -101,8 +101,8 @@ logo_url("/driver_icons/folder_list.png")
                               }
                                   ,{ })
          if (result) {
-             if (!this.args.path) {
-                 this.args.path = result.value
+             if (!this.control_properties_and_events.path) {
+                 this.control_properties_and_events.path = result.value
              }
          }
        }
@@ -110,9 +110,9 @@ logo_url("/driver_icons/folder_list.png")
         watch: {
           // This would be called anytime the value of the input changes
           refresh: function(newValue, oldValue) {
-              //console.log("refresh: " + this.args.text)
-              if (isValidObject(this.args)) {
-                  this.value = this.args.value
+              //console.log("refresh: " + this.control_properties_and_events.text)
+              if (isValidObject(this.control_properties_and_events)) {
+                  this.value = this.control_properties_and_events.value
               }
           }
         }
@@ -123,14 +123,14 @@ logo_url("/driver_icons/folder_list.png")
 
 
                changedFn: function() {
-                   if (isValidObject(this.args)) {
-                       this.args.value = this.value
+                   if (isValidObject(this.control_properties_and_events)) {
+                       this.control_properties_and_events.value = this.value
                    }
                }
                ,
 
                runEventHandler: async function() {
-                   await this.runEvent({ display: "changed",   code: this.args.changed_event })
+                   await this.runEvent({ display: "changed",   code: this.control_properties_and_events.changed_event })
                }
                ,
                  load: async function() {
@@ -140,17 +140,17 @@ logo_url("/driver_icons/folder_list.png")
                                          {
                                              base_component_id: "serverFolderHierarchyList"
                                          }
-                                             ,{ path: mm.args.path })
+                                             ,{ path: mm.control_properties_and_events.path })
                      //alert(JSON.stringify(result,null,2))
 
                     if (result) {
                          this.drives = result
 
                     }
-                    if (isValidObject(this.args)) {
-                        this.items = this.args.items
-                        if (isValidObject(this.args.value)) {
-                           this.value = this.args.value
+                    if (isValidObject(this.control_properties_and_events)) {
+                        this.items = this.control_properties_and_events.items
+                        if (isValidObject(this.control_properties_and_events.value)) {
+                           this.value = this.control_properties_and_events.value
                         }
                     }
 
