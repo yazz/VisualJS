4828a4f7bccbaa8ee8397a6acce509cf262b4da2
Zubair Quraishi <zubairq@gmail.com>
2024-03-22 12:10:07 +0100

    cleanup chart control

--- public/visifile_drivers/controls/chart.js
+++ public/visifile_drivers/controls/chart.js
@@ -77,14 +77,10 @@ properties(
 )//properties
 logo_url("/driver_icons/chart.png")
 */
-
-
-
     Yazz.component({
-      props: ["meta","name","control_properties_and_events","refresh","design_mode","runEvent"]
-      ,
-      template:
-`<div   v-bind:style='"width:100%;overflow-y:auto;height:100%"
+        props:      [  "meta","name","control_properties_and_events","refresh","design_mode","runEvent"  ],
+        template:   `
+ <div   v-bind:style='"width:100%;overflow-y:auto;height:100%"
         v-bind:refresh='refresh'>
 
     <div v-bind:style='"height:100%;width:100%; border: 0px;color:black;"'
@@ -123,77 +119,68 @@ logo_url("/driver_icons/chart.png")
 
 
 
-</div>`
-      ,
-      data: function() {
-       return {
-         value:             null,
-         selected_index:    null,
-         items:             [],
-         new_value:         "",
-         new_text:          "",
-         canvasId:           uuidv4(),
-         myChart:           null,
-       }
-     }
-     ,
-     watch: {
-       // This would be called anytime the value of the input changes
-       refresh: function(newValue, oldValue) {
-           //console.log("refresh: " + this.control_properties_and_events.text)
-           if (isValidObject(this.control_properties_and_events)) {
-               this.value = this.control_properties_and_events.value
-               this.items = this.control_properties_and_events.items
-           }
-       }
-     }
-     ,
-     mounted: async function() {
-         await registerComponent(this)
-         await useChartsJs()
+</div>`,
+        data:       function(  ) {
+            return {
+                 value:             null,
+                 selected_index:    null,
+                 items:             [],
+                 new_value:         "",
+                 new_text:          "",
+                 canvasId:           uuidv4(),
+                 myChart:           null,
+            }
+        },
+        watch:      {
+            // This would be called anytime the value of the input changes
+            refresh: function(newValue, oldValue) {
+                //console.log("refresh: " + this.control_properties_and_events.text)
+                if (isValidObject(this.control_properties_and_events)) {
+                    this.value = this.control_properties_and_events.value
+                    this.items = this.control_properties_and_events.items
+                }
+            }
+        },
+        mounted:    async function(  ) {
+            await registerComponent(this)
+            await useChartsJs()
 
-         if (isValidObject(this.control_properties_and_events)) {
-             this.items = this.control_properties_and_events.items
-             if (isValidObject(this.control_properties_and_events.value)) {
-                this.value = this.control_properties_and_events.value
-             }
-         }
-         if (document.getElementById(this.canvasId)) {
-             var ctx = document.getElementById(this.canvasId).getContext('2d');
+            if (isValidObject(this.control_properties_and_events)) {
+                this.items = this.control_properties_and_events.items
+                if (isValidObject(this.control_properties_and_events.value)) {
+                    this.value = this.control_properties_and_events.value
+                }
+            }
+            if (document.getElementById(this.canvasId)) {
+                var ctx = document.getElementById(this.canvasId).getContext('2d');
 
-             if (this.control_properties_and_events.jsonConfig.length > 0 ) {
-                 this.myChart = new Chart(ctx,   eval("(" + this.control_properties_and_events.jsonConfig + ")"));
-             }
-         }
-      }
-      ,
-      methods: {
-            getData: function() {
+                if (this.control_properties_and_events.jsonConfig.length > 0 ) {
+                    this.myChart = new Chart(ctx,   eval("(" + this.control_properties_and_events.jsonConfig + ")"));
+                }
+            }
+        },
+        methods:    {
+            getData:            function        (  ) {
                 if (this.myChart) {
                     return this.myChart.data
                 } else {
                     return null
                 }
-            }
-            ,
-            update: async function() {
+            },
+            update:             async function  (  ) {
                 if (this.myChart) {
                     this.myChart.update()
                 }
-            }
-            ,
-            changedFn: function() {
+            },
+            changedFn:          function        (  ) {
                 if (isValidObject(this.control_properties_and_events)) {
                     this.control_properties_and_events.value = this.value
                     this.control_properties_and_events.items = this.items
                 }
-            }
-            ,
-
-            runEventHandler: async function() {
+            },
+            runEventHandler:    async function  (  ) {
                 await this.runEvent({ display: "changed",   code: this.control_properties_and_events.changed_event })
             }
-      }
-
+        }
     })
 }
